09a090f038e8e40dc9e5e99cf687fdf1911fc1cc
Amanda Bullington <albullington@gmail.com>
2021-09-13 16:41:59 -0700

    Update repo name to iNaturalistReactNative

--- android/app/src/main/java/com/inatmobilereactnative/MainActivity.java
+++ android/app/src/main/java/com/inatmobilereactnative/MainActivity.java
@@ -1,4 +1,4 @@
-package com.inatmobilereactnative;
+package com.inaturalistreactnative;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "INatMobileReactNative";
+    return "iNaturalistReactNative";
   }
 }
