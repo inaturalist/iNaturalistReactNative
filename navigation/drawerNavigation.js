2caf8b5ad7a7e2db2121972bd96df159e536799b
Amanda Bullington <albullington@gmail.com>
2021-11-12 11:51:13 -0800

    Include all plural cases for num of observations

--- navigation/drawerNavigation.js
+++ navigation/drawerNavigation.js
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useTranslation } from "react-i18next";
 
 import ObsList from "../components/Observations/ObsList";
 
@@ -10,10 +11,18 @@ const screenOptions = { headerLeft: ( ) => <></> };
 
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigator = ( ): React.Node => (
-  <Drawer.Navigator screenOptions={screenOptions}>
-    <Drawer.Screen name="my observations" component={ObsList} />
-  </Drawer.Navigator>
-);
+const DrawerNavigator = ( ): React.Node => {
+  const { t } = useTranslation( );
+
+  return (
+    <Drawer.Navigator screenOptions={screenOptions}>
+      <Drawer.Screen
+        name="my observations"
+        component={ObsList}
+        options={{ headerTitle: t( "my_observations_caps" ) }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 export default DrawerNavigator;
---
+++ translations/en.js
@@ -0,0 +1,9 @@
+module.exports = {
+  my_observations_caps: "MY OBSERVATIONS",
+  number_of_observations_zero: "{{count}} observations",
+  number_of_observations_one: "1 observation",
+  number_of_observations_two: "{{count}} observations",
+  number_of_observations_few: "{{count}} observations",
+  number_of_observations_many: "{{count}} observations",
+  number_of_observations_other: "{{count}} observations"
+};
