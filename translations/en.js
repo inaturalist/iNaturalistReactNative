ae429b57eecf6463e32a3ac14fdbdf5df81c4c3b
Amanda Bullington <albullington@gmail.com>
2021-11-12 16:30:27 -0800

    Test large numbers using international number format

--- translations/en.js
+++ translations/en.js
@@ -2,11 +2,6 @@ module.exports = {
   date_observed: "Date observed: {{date}}",
   date_uploaded: "Date uploaded: {{date}}",
   my_observations_caps: "MY OBSERVATIONS",
-  number_format: {
-    delimiter: ",",
-    precision: 3,
-    separator: "."
-  },
   total_species_seen: "{{val, number}} total species seen",
   x_observations_zero: "{{count}} observations",
   x_observations_one: "1 observation",
