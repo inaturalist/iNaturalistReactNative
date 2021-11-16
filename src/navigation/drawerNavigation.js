bec8c6b75a3fa22e3006c1770c5120e1ab06fe93
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2021-11-15 16:59:53 -0800

    Fill in side menu with placeholder screens (#16)
    * Fill in placeholders for side menu
    
    * Tweak text

--- src/navigation/drawerNavigation.js
+++ src/navigation/drawerNavigation.js
@@ -4,6 +4,7 @@ import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import ObsList from "../components/Observations/ObsList";
+import PlaceholderComponent from "../components/PlaceholderComponent";
 
 // this removes the default hamburger menu from header
 const screenOptions = { headerLeft: ( ) => <></> };
@@ -13,6 +14,17 @@ const Drawer = createDrawerNavigator();
 const DrawerNavigator = ( ): React.Node => (
   <Drawer.Navigator screenOptions={screenOptions}>
     <Drawer.Screen name="my observations" component={ObsList} />
+    <Drawer.Screen name="missions/seen nearby" component={PlaceholderComponent} />
+    <Drawer.Screen name="search" component={PlaceholderComponent} />
+    <Drawer.Screen name="identify" component={PlaceholderComponent} />
+    <Drawer.Screen name="following (dashboard)" component={PlaceholderComponent} />
+    <Drawer.Screen name="impact" component={PlaceholderComponent} />
+    <Drawer.Screen name="projects" component={PlaceholderComponent} />
+    <Drawer.Screen name="guides" component={PlaceholderComponent} />
+    <Drawer.Screen name="about" component={PlaceholderComponent} />
+    <Drawer.Screen name="help/tutorials" component={PlaceholderComponent} />
+    <Drawer.Screen name="settings" component={PlaceholderComponent} />
+    <Drawer.Screen name="logout" component={PlaceholderComponent} />
   </Drawer.Navigator>
 );
 
