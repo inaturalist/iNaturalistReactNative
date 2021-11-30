95d33027529bd14e9f3373ec1715fc3b3a768f15
Amanda Bullington <albullington@gmail.com>
2021-11-29 17:25:20 -0800

    Add basic styling, html renderer, Wikipedia link

--- src/components/TaxonDetails/TaxonDetails.js
+++ src/components/TaxonDetails/TaxonDetails.js
@@ -1,36 +1,46 @@
 // @flow
 
 import * as React from "react";
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, useWindowDimensions, Linking } from "react-native";
+import HTML from "react-native-render-html";
 
 import ViewWithFooter from "../SharedComponents/ViewWithFooter";
 import PhotoScroll from "../ObsDetails/PhotoScroll";
-import { viewStyles } from "../../styles/obsDetails";
+import { viewStyles, textStyles } from "../../styles/taxonDetails";
 import useFetchTaxonDetails from "./hooks/fetchTaxonDetails";
 
 const TaxonDetails = ( ): React.Node => {
   const taxon = useFetchTaxonDetails( 3 );
+  const { width } = useWindowDimensions( );
 
   if ( !taxon ) { return null; }
+
+  const openWikipedia = ( ) => Linking.openURL( taxon.wikipedia_url );
+
   return (
     <ViewWithFooter>
       <View style={viewStyles.photoContainer}>
         <PhotoScroll photos={taxon.taxonPhotos} />
       </View>
+      <View style={viewStyles.textContainer}>
       <Text>{taxon.rank}</Text>
       <Text>{taxon.preferred_common_name}</Text>
       <Text>{taxon.name}</Text>
-      <Text>ABOUT</Text>
-      <Text>{taxon.wikipedia_summary}</Text>
-      <Pressable>
-        <Text>Read more on Wikipedia</Text>
+      <Text style={textStyles.header}>ABOUT</Text>
+      <HTML
+        contentWidth={width}
+        source={{ html: taxon.wikipedia_summary }}
+      />
+      <Pressable onPress={openWikipedia}>
+        <Text style={textStyles.header}>Read more on Wikipedia</Text>
       </Pressable>
-      <Text>TAXONOMY</Text>
+      <Text style={textStyles.header}>TAXONOMY</Text>
       {taxon.ancestors.map( ( ancestor, i ) => {
-        const currentTaxon = `${taxon.preferred_common_name} (${taxon.rank} ${taxon.name})`;
+        const addIndent = index => index * 5;
+        const currentTaxon = `${taxon.preferred_common_name} (${taxon.name})`;
         const formattedAncestor = `${ancestor.preferred_common_name} (${ancestor.rank} ${ancestor.name})`;
-        const displayAncestor = <Text key={ancestor.id}>{formattedAncestor}</Text>;
-        const displayTaxon = <Text key={taxon.id}>{currentTaxon}</Text>;
+        const displayAncestor = <Text key={ancestor.id} style={{ marginLeft: addIndent( i ) }}>{formattedAncestor}</Text>;
+        const displayTaxon = <Text key={taxon.id} style={{ marginLeft: addIndent( i + 1 ) }}>{currentTaxon}</Text>;
 
         return (
           <>
@@ -39,6 +49,7 @@ const TaxonDetails = ( ): React.Node => {
           </>
         );
       } )}
+      </View>
     </ViewWithFooter>
   );
 };
---
+++ src/styles/taxonDetails.js
@@ -0,0 +1,28 @@
+// @flow strict-local
+
+import { StyleSheet } from "react-native";
+
+import type { ViewStyleProp, TextStyleProp } from "react-native/Libraries/StyleSheet/StyleSheet";
+import { colors } from "./global";
+
+const viewStyles: { [string]: ViewStyleProp } = StyleSheet.create( {
+  photoContainer: {
+    backgroundColor: colors.black,
+    height: 200
+  },
+  textContainer: {
+    marginHorizontal: 25,
+    marginTop: 10
+  }
+} );
+
+const textStyles: { [string]: TextStyleProp } = StyleSheet.create( {
+  header: {
+    marginVertical: 10
+  }
+} );
+
+export {
+  viewStyles,
+  textStyles
+};
