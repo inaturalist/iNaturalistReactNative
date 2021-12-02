5211fa744542912b3b0f1f31629097acb80115e8
Amanda Bullington <albullington@gmail.com>
2021-12-02 15:13:11 -0800

    Add input fields

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,20 +1,40 @@
 // @flow
 
-import * as React from "react";
+import React, { useState } from "react";
 import { Text, Pressable } from "react-native";
+import type { Node } from "react";
+
 import { textStyles } from "../../styles/explore/explore";
+import InputField from "../SharedComponents/InputField";
 import ViewWithFooter from "../SharedComponents/ViewWithFooter";
 
-const Explore = ( ): React.Node => (
-  <ViewWithFooter>
-    <Text style={textStyles.explanation}>search for species and taxa seen anywhere in the world</Text>
-    <Text style={textStyles.explanation}>try searching for insects near your location...</Text>
-    <Text>search bar one</Text>
-    <Text>my location</Text>
-    <Pressable>
-      <Text>Explore organisms</Text>
-    </Pressable>
-  </ViewWithFooter>
-);
+const Explore = ( ): Node => {
+  const [searchTerm, setSearchTerm] = useState( "" );
+  const [location, setLocation] = useState( "" );
+  const search = input => setSearchTerm( input );
+  const updateLocation = input => setLocation( input );
+
+  return (
+    <ViewWithFooter>
+      <Text style={textStyles.explanation}>search for species and taxa seen anywhere in the world</Text>
+      <Text style={textStyles.explanation}>try searching for insects near your location...</Text>
+      <InputField
+        handleTextChange={search}
+        placeholder="search bar"
+        text={searchTerm}
+        type="none"
+      />
+      <InputField
+        handleTextChange={updateLocation}
+        placeholder="location bar"
+        text={location}
+        type="none"
+      />
+      <Pressable>
+        <Text>Explore organisms</Text>
+      </Pressable>
+    </ViewWithFooter>
+  );
+};
 
 export default Explore;
---
+++ src/styles/sharedComponents/inputField.js
@@ -0,0 +1,22 @@
+// @flow
+
+import { StyleSheet } from "react-native";
+import { colors } from "../global";
+
+import type { TextStyleProp } from "react-native/Libraries/StyleSheet/StyleSheet";
+
+const textStyles: { [string]: TextStyleProp } = StyleSheet.create( {
+  inputField: {
+    backgroundColor: colors.white,
+    borderColor: colors.gray,
+    borderRadius: 40,
+    borderWidth: 0.5,
+    color: colors.black,
+    height: 37,
+    paddingLeft: 15,
+    marginHorizontal: 50,
+    marginVertical: 20
+  }
+} );
+
+export default textStyles;
