2055de29f06fc8b86c1f3dd1e353db7a8211b962
Amanda Bullington <albullington@gmail.com>
2022-02-03 17:52:10 -0800

    Fix warning eslint

--- tests/unit/components/PhotoLibrary/PhotoGallery.test.js
+++ tests/unit/components/PhotoLibrary/PhotoGallery.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react-native";
+import { render } from "@testing-library/react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import factory from "../../../factory";
