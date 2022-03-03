1738dbcb48cd960b581cece40bed6c4e7de6370b
Amanda Bullington <albullington@gmail.com>
2022-03-02 19:14:52 -0800

    Switch to computervision score_image endpoint for suggestions

--- src/components/ObsEdit/hooks/useTaxaSuggestions.js
+++ src/components/ObsEdit/hooks/useTaxaSuggestions.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { useEffect, useState } from "react";
-import inatjs from "inaturalistjs";
+import inatjs, { FileUpload } from "inaturalistjs";
 import { getJWTToken } from "../../LoginSignUp/AuthenticationService";
 
 const TAXON_FIELDS = {
@@ -29,25 +29,54 @@ const useTaxaSuggestions = ( currentObs: Object ): Object => {
     const latitude = currentObs.latitude;
     const longitude = currentObs.longitude;
 
+    // const uploadParams = await flattenUploadParameters( image );
+    //   const token = createJwtToken( );
+    //   const options = { api_token: token, user_agent: createUserAgent() };
+
+    //   try {
+    //     const r = await inatjs.computervision.score_image( uploadParams, options );
+    //     if ( r.results.length === 0 ) {
+    //       updateObs( { } );
+    //       return;
+
     let isCurrent = true;
     const fetchTaxaSuggestions = async ( ): Promise<Object> => {
       try {
         const apiToken = await getJWTToken( false );
+
+        // works with API v1, not v2
         const params = {
-          limit: 10,
-          // image_url: uri
-          // source: "visual",
-          lat: latitude,
-          lng: longitude,
-          fields: FIELDS
+          image: new FileUpload( {
+            uri,
+            name: "photo.jpeg",
+            type: "image/jpeg"
+          } ),
+          // observed_on: new Date( time * 1000 ).toISOString(),
+          latitude,
+          longitude
         };
+        // const params = {
+        //   limit: 10,
+        //   image_url: new FileUpload( {
+        //     uri: uri,
+        //     name: "photo.jpeg",
+        //     type: "image/jpeg"
+        //   } ),
+        //   source: "visual",
+        //   // lat: latitude,
+        //   // lng: longitude,
+        //   fields: FIELDS
+        // };
         console.log( "fetch suggestions with params: ", params );
         const options = {
           api_token: apiToken
         };
 
-        const response = await inatjs.taxa.suggest( params, options );
-        setSuggestions( response.results );
+        const r = await inatjs.computervision.score_image( params, options );
+
+        // const response = await inatjs.taxa.suggest( params, options );
+        console.log( r.results, "results computer vision" );
+        setSuggestions( r.results );
         if ( !isCurrent ) { return; }
       } catch ( e ) {
         console.log( JSON.stringify( e.response ), "couldn't fetch taxa suggestions" );
