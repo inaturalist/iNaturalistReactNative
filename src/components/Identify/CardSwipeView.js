ce1b9d4afee1656703fb7995570bc0a334cd7b0a
Amanda Bullington <albullington@gmail.com>
2022-03-09 16:10:48 -0800

    Add mark as reviewed; swipe right to agree on identifications for species rank

--- src/components/Identify/CardSwipeView.js
+++ src/components/Identify/CardSwipeView.js
@@ -1,12 +1,14 @@
 // @flow
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, Image } from "react-native";
 import type { Node } from "react";
 import TinderCard from "react-tinder-card";
 
 import { viewStyles, imageStyles, textStyles } from "../../styles/identify/identify";
 import Observation from "../../models/Observation";
+import markAsReviewed from "./helpers/markAsReviewed";
+import createIdentification from "./helpers/createIdentification";
 
 type Props = {
   loading: boolean,
@@ -15,12 +17,20 @@ type Props = {
 }
 
 const CardSwipeView = ( { loading, observationList, testID }: Props ): Node => {
-  const onSwipe = ( direction ) => {
+  const [totalSwiped, setTotalSwiped] = useState( 0 );
+  const onSwipe = async ( direction, id, isSpecies, agreeParams ) => {
+    if ( direction === "left" ) {
+      markAsReviewed( id );
+    } else if ( direction === "right" && isSpecies ) {
+      const agreed = await createIdentification( agreeParams );
+      console.log( agreed, "agreed in card swipe" );
+    }
     console.log( "You swiped: " + direction );
   };
 
-  const onCardLeftScreen = ( myIdentifier ) => {
-    console.log( myIdentifier + " left the screen" );
+  const onCardLeftScreen = ( ) => {
+    setTotalSwiped( totalSwiped + 1 );
+    // TODO: when total swiped is 30, fetch next page of observations
   };
 
   if ( observationList.length === 0 ) {
@@ -29,16 +39,25 @@ const CardSwipeView = ( { loading, observationList, testID }: Props ): Node => {
 
   return (
     <View style={viewStyles.cardContainer}>
+      <Text>Swipe left to mark as reviewed.</Text>
+      <Text>Swipe right to agree.</Text>
       {observationList.map( obs => {
         const commonName = obs.taxon && obs.taxon.preferred_common_name;
         const name = obs.taxon ? obs.taxon.name : "unknown";
+        const isSpecies = obs.taxon && obs.taxon.rank === "species";
         const imageUri = Observation.mediumUri( obs );
+        const preventSwipeDirections = ["up", "down"];
+        if ( !isSpecies ) {
+          preventSwipeDirections.push( "right" );
+        }
+        const agreeParams = { observation_id: obs.id, taxon_id: obs.taxon.id };
+
         return (
           <TinderCard
             key={obs.id}
-            onSwipe={onSwipe}
-            onCardLeftScreen={() => onCardLeftScreen( "fooBar" )}
-            preventSwipe={["right", "left"]}
+            onSwipe={( dir ) => onSwipe( dir, obs.uuid, isSpecies, agreeParams )}
+            onCardLeftScreen={onCardLeftScreen}
+            preventSwipe={preventSwipeDirections}
           >
             <View style={viewStyles.card}>
               <Image style={imageStyles.cardImage} source={imageUri} />
