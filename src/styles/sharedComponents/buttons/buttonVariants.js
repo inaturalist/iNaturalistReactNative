2b3bfe207216208a82cbdd3773c6b82b78db66ce
Merhawit Gubsa <merhawitgubsa@Merhawits-MacBook-Pro.local>
2022-08-19 15:45:57 -0700

    Add style for custom component

---
+++ src/styles/sharedComponents/buttons/buttonVariants.js
@@ -0,0 +1,49 @@
+// @flow strict-local
+
+import { StyleSheet } from "react-native";
+import type { TextStyleProp, ViewStyleProp } from "react-native/Libraries/StyleSheet/StyleSheet";
+
+import colors from "../../colors";
+
+const PRIMARY = "#5D8017";
+const WARNING = "#9B1111";
+const NEUTRAL = "#979797";
+
+const viewStyles: { [string]: ViewStyleProp } = StyleSheet.create( {
+  containerDefault: {
+    alignItems: "center",
+    paddingVertical: 10,
+    borderRadius: 40,
+    marginVertical: 15
+  },
+  containerPrimary: {
+    backgroundColor: PRIMARY,
+    borderColor: PRIMARY
+  },
+  containerWarning: {
+    backgroundColor: WARNING,
+    borderColor: WARNING
+  },
+  containerNeutral: {
+    backgroundColor: NEUTRAL,
+    borderColor: NEUTRAL
+  },
+  containerDisabled: {
+    opacity: 0.65
+  }
+
+} );
+
+const textStyles: { [string]: TextStyleProp } = StyleSheet.create( {
+  textDefault: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: colors.white
+  }
+
+} );
+
+export {
+  textStyles,
+  viewStyles
+};
