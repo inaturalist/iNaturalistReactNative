c4ae96e0883833f396789494299f24689cff11ba
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-08-25 16:39:42 -0700

    Fixed messages test; changed useQuery mocking approach
    In component unit tests we really don't need to know that useQuery works, or
    that our API wrappers work, we just need to know that the component renders
    what it's supposed to given data that it receives. So instead of making a
    fake hook to set the React Query client cache and waiting for a mocked
    network response to complete, I'm just mocking useQuery to return what we
    want.
    
    If we do want to test that all these things work together, we should write an
    integration test. Setup is more complicated, but that's why you don't write
    as many integration tests.

--- tests/unit/components/Messages/Messages.test.js
+++ tests/unit/components/Messages/Messages.test.js
@@ -26,13 +26,47 @@ const renderMessages = ( ) => render(
   </NavigationContainer>
 );
 
-it( "displays activity indicator when loading", ( ) => {
-  const { getByTestId } = renderMessages( );
-  expect( getByTestId( "Messages.activityIndicator" ) ).toBeTruthy( );
+// We need to do some weird stuff to test results that vary based on useQuery
+// return values. We can't use jest.mock to totally change the mock
+// definition within a test, so instead we mock the module once and tell it
+// to return an object literal attribute in response to useQuery. That would
+// be a serious problem if tests get run in parallel because you can't really
+// control what that value is for that specific test. To address this, we
+// take advantage of the fact that Jest seems to run describe blocks
+// sequentially, or at least it doesn't seem to allow tests within a describe
+// block to access data from other describe blocks. Whatever's happening,
+// using a single mocked return value for useQuery within a describe block
+// seems to work.
+const MockData = { useQueryResponse: {} };
+jest.mock( "@tanstack/react-query", ( ) => ( {
+  useQuery: ( ) => ( MockData.useQueryResponse )
+} ) );
+
+describe( "when loading", ( ) => {
+  beforeAll( ( ) => {
+    MockData.useQueryResponse = {
+      data: [],
+      isLoading: true,
+      isError: false
+    };
+  } );
+  it( "displays activity indicator when loading", ( ) => {
+    const { getByTestId } = renderMessages( );
+    expect( getByTestId( "Messages.activityIndicator" ) ).toBeTruthy( );
+  } );
 } );
 
-it( "displays message subject and not activity indicator when loading complete", ( ) => {
-  const { getByText, queryByTestId } = renderMessages( );
-  expect( getByText( mockMessage.subject ) ).toBeTruthy( );
-  expect( queryByTestId( "Messages.activityIndicator" ) ).toBeNull( );
+describe( "when loading complete", ( ) => {
+  beforeAll( ( ) => {
+    MockData.useQueryResponse = {
+      data: [mockMessage],
+      isLoading: false,
+      isError: false
+    };
+  } );
+  it( "displays message subject and not activity indicator when loading complete", ( ) => {
+    const { getByText, queryByTestId } = renderMessages( );
+    expect( getByText( mockMessage.subject ) ).toBeTruthy( );
+    expect( queryByTestId( "Messages.activityIndicator" ) ).toBeNull( );
+  } );
 } );
--- tests/unit/components/TaxonDetails/TaxonDetails.test.js
+++ tests/unit/components/TaxonDetails/TaxonDetails.test.js
@@ -1,19 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
-import { renderHook } from "@testing-library/react-hooks";
 import { fireEvent, render } from "@testing-library/react-native";
-import inatjs from "inaturalistjs";
 import React from "react";
 import { Linking } from "react-native";
 
-import fetchTaxa from "../../../../src/api/taxa";
 import TaxonDetails from "../../../../src/components/TaxonDetails/TaxonDetails";
-import factory, { makeResponse } from "../../../factory";
+import factory from "../../../factory";
 // Mock inaturalistjs so we can make some fake responses
 jest.mock( "inaturalistjs" );
 
-const testTaxon = factory( "RemoteTaxon" );
-const mockExpected = testTaxon;
+const mockTaxon = factory( "RemoteTaxon" );
 
 jest.mock( "@react-navigation/native", ( ) => {
   const actualNav = jest.requireActual( "@react-navigation/native" );
@@ -21,74 +16,57 @@ jest.mock( "@react-navigation/native", ( ) => {
     ...actualNav,
     useRoute: ( ) => ( {
       params: {
-        id: mockExpected.id
+        id: mockTaxon.id
       }
     } )
   };
 } );
 
-// set cacheTime to infinity to avoid 'Jest did not exit...' error message
-// https://tanstack.com/query/v4/docs/guides/testing#set-cachetime-to-infinity-with-jest
-const queryClient = new QueryClient( {
-  defaultOptions: {
-    queries: {
-      cacheTime: Infinity
-    }
-  }
-} );
-
-const wrapper = ( { children } ) => (
-  <QueryClientProvider client={queryClient}>
-    {children}
-  </QueryClientProvider>
+const renderTaxonDetails = ( ) => render(
+  <NavigationContainer>
+    <TaxonDetails />
+  </NavigationContainer>
 );
 
-const renderTaxonDetails = ( ) => render(
-  <QueryClientProvider client={queryClient}>
+// Mock useQuery results
+jest.mock( "@tanstack/react-query", ( ) => ( {
+  useQuery: ( queryKey, _queryFunction ) => {
+    if ( queryKey[0] === "taxaFetch" ) {
+      return {
+        data: mockTaxon,
+        isLoading: false,
+        isError: false
+      };
+    }
+    return {
+      data: null,
+      isLoading: false,
+      isError: false
+    };
+  }
+} ) );
+
+test( "renders taxon details from API call", async ( ) => {
+  const { getByTestId, getByText } = renderTaxonDetails( );
+  expect( getByTestId( `TaxonDetails.${mockTaxon.id}` ) ).toBeTruthy( );
+  expect( getByTestId( "PhotoScroll.photo" ).props.source )
+    .toStrictEqual( { uri: mockTaxon.taxonPhotos[0].photo.url } );
+  expect( getByText( mockTaxon.preferred_common_name ) ).toBeTruthy( );
+  expect( getByText( mockTaxon.wikipedia_summary ) ).toBeTruthy( );
+} );
+
+test( "should not have accessibility errors", ( ) => {
+  const taxonDetails = (
     <NavigationContainer>
       <TaxonDetails />
     </NavigationContainer>
-  </QueryClientProvider>
-);
-
-function useFetchData() {
-  return useQuery( ["taxaFetch"], ( ) => fetchTaxa( 1 ) );
-}
-
-test( "renders taxon details from API call", async ( ) => {
-  inatjs.taxa.fetch.mockResolvedValue( makeResponse( [mockExpected] ) );
-  const { result, waitFor } = renderHook( () => useFetchData(), { wrapper } );
-  await waitFor( () => result.current.isSuccess );
-  const { getByTestId, getByText } = renderTaxonDetails( );
-
-  expect( getByTestId( `TaxonDetails.${testTaxon.id}` ) ).toBeTruthy( );
-  expect( getByTestId( "PhotoScroll.photo" ).props.source )
-    .toStrictEqual( { uri: testTaxon.taxonPhotos[0].photo.url } );
-  expect( getByText( testTaxon.preferred_common_name ) ).toBeTruthy( );
-  expect( getByText( testTaxon.wikipedia_summary ) ).toBeTruthy( );
+  );
+  expect( taxonDetails ).toBeAccessible( );
 } );
 
-// right now this is failing on react-native-modal, since there's a TouchableWithFeedback
-// that allows the user to tap the backdrop and exit the modal
-test.todo( "should not have accessibility errors" );
-// test( "should not have accessibility errors", ( ) => {
-//   const taxonDetails = (
-//     <NavigationContainer>
-//       <TaxonDetails />
-//     </NavigationContainer>
-//   );
-//   expect( taxonDetails ).toBeAccessible( );
-// } );
-
 test( "navigates to Wikipedia on button press", async ( ) => {
-  inatjs.taxa.fetch.mockResolvedValue( makeResponse( [mockExpected] ) );
-  const { result, waitFor } = renderHook( () => useFetchData(), { wrapper } );
-  await waitFor( () => result.current.isSuccess );
-
-  expect( result.current.data.wikipedia_url ).toEqual( testTaxon.wikipedia_url );
   const { getByTestId } = renderTaxonDetails( );
-
   fireEvent.press( getByTestId( "TaxonDetails.wikipedia" ) );
   expect( Linking.openURL ).toHaveBeenCalledTimes( 1 );
-  expect( Linking.openURL ).toHaveBeenCalledWith( testTaxon.wikipedia_url );
+  expect( Linking.openURL ).toHaveBeenCalledWith( mockTaxon.wikipedia_url );
 } );
