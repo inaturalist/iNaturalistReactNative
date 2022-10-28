cad8d8544dc82cc8df1c9e8009690b370f467c6c
Angie Ta <angie@inaturalist.org>
2022-10-28 12:52:17 -0700

    Replacing styles/colors.js with tailwind theme colors. Added tailwind-colors.js. Closes #196.

---
+++ tailwind-colors.js
@@ -0,0 +1,10 @@
+// @flow
+
+import resolveConfig from "tailwindcss/resolveConfig";
+
+import tailwindConfig from "./tailwind.config";
+
+// $FlowIgnore
+const fullConfig = resolveConfig( tailwindConfig );
+
+export default fullConfig.theme.colors;
