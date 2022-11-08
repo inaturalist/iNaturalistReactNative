a48dbc91829c3e909c31f10d79a332f341970129
Angie Ta <angie@inaturalist.org>
2022-11-07 17:42:04 -0800

    Guard against icon_url being null or blank

--- src/components/Observations/UserCard.js
+++ src/components/Observations/UserCard.js
@@ -29,14 +29,14 @@ const UserCard = ( ): Node => {
   if ( !user ) { return <View className="flex-row mx-5 items-center" />; }
   const navToUserProfile = ( ) => navigation.navigate( "UserProfile", { userId: user.id } );
 
-  const productionIcon = remoteUser?.icon_url.replace( "staticdev", "static" );
+  const productionIcon = remoteUser?.icon_url?.replace( "staticdev", "static" );
   const icon = {
     uri: productionIcon
   };
 
   return (
     <View className="flex-row mx-5 items-center">
-      {remoteUser && <UserIcon uri={icon} />}
+      {remoteUser && productionIcon && <UserIcon uri={icon} />}
       <View className="ml-3">
         <Text className="color-white my-1">{User.userHandle( user )}</Text>
         {remoteUser && (
