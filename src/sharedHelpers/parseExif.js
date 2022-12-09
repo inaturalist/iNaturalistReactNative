c1eb685f150cdf0a62a1047de83dfb7ab6259041
Amanda Bullington <albullington@gmail.com>
2022-12-09 12:54:36 -0800

    Fix exif test

--- src/sharedHelpers/parseExif.js
+++ src/sharedHelpers/parseExif.js
@@ -1,7 +1,6 @@
 // @flow
 
 import { parse } from "date-fns";
-import { useEffect, useState } from "react";
 import { readExif } from "react-native-exif-reader";
 
 class UsePhotoExifDateFormatError extends Error {}
@@ -23,30 +22,13 @@ export const parseExifDateTime = ( datetime: string ): ?Date => {
   return parsedDate;
 };
 
-export const usePhotoExif = ( photoUri: ?string ): Object => {
-  const [exif, setExif] = useState( null );
-
-  useEffect( ( ) => {
-    let isCurrent = true;
-
-    const parseExif = async ( ): Promise<Object> => {
-      try {
-        const rawExif = await readExif( photoUri );
-
-        if ( !isCurrent ) { return; }
-        setExif( rawExif );
-      } catch ( e ) {
-        console.error( e, "Couldn't parse EXIF" );
-      }
-    };
-
-    if ( photoUri ) {
-      parseExif();
-    }
-    return ( ) => {
-      isCurrent = false;
-    };
-  }, [photoUri] );
-
-  return exif;
+// Parses EXIF date time into a date object
+export const parseExif = async ( photoUri: ?string ): Promise<Object> => {
+  try {
+    const rawExif = await readExif( photoUri );
+    return rawExif;
+  } catch ( e ) {
+    console.error( e, "Couldn't parse EXIF" );
+    return null;
+  }
 };
