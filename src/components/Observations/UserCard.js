248736e4b27b8541f8716ccbf438cc5f0375af3e
Amanda Bullington <albullington@gmail.com>
2022-12-23 12:50:32 -0800

    Comment out locale test

--- src/components/Observations/UserCard.js
+++ src/components/Observations/UserCard.js
@@ -9,31 +9,23 @@ import React from "react";
 import IconMaterial from "react-native-vector-icons/MaterialIcons";
 import User from "realmModels/User";
 import useCurrentUser from "sharedHooks/useCurrentUser";
-import useUserMe from "sharedHooks/useUserMe";
 import colors from "styles/tailwindColors";
 
 const UserCard = ( ): Node => {
   const navigation = useNavigation( );
   const currentUser = useCurrentUser( );
-
-  // this is needed for the first time a user signs in, when the only realm
-  // fields populated are id, login, and signedIn
-  const { remoteUser } = useUserMe( );
-
   if ( !currentUser ) { return <View className="flex-row mx-5 items-center" />; }
-  const user = currentUser?.observations_count !== null ? currentUser : remoteUser;
+  const navToUserProfile = ( ) => navigation.navigate( "UserProfile", { userId: currentUser.id } );
 
-  const navToUserProfile = ( ) => navigation.navigate( "UserProfile", { userId: user.id } );
-
-  const uri = User.uri( user );
+  const uri = User.uri( currentUser );
 
   return (
     <View className="flex-row px-5 items-center rounded-bl-3xl rounded-br-3xl bg-primary h-24">
       {uri && <UserIcon uri={uri} />}
       <View className="ml-3">
-        <Text className="color-white my-1">{User.userHandle( user )}</Text>
+        <Text className="color-white my-1">{User.userHandle( currentUser )}</Text>
         <Text className="color-white my-1">
-          { t( "X-Observations", { count: user?.observations_count || 0 } )}
+          { t( "X-Observations", { count: currentUser.observations_count || 0 } )}
         </Text>
       </View>
       <Pressable
