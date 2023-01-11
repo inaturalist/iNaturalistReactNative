8c13f0ba1cb5f41e932cdab5bc6d8d865442730c
Chris <chrischuckgithub@gmail.com>
2023-01-10 19:59:32 -0600

    Fix ObsListBottomSheet subsequent upload bug (#344)
    Closes #323
    
    Also,
    
    * Move upload status under the useUploadObservations hook for a single source
      of truth
    * Move useUploadObservations hook to ObsListBottomSheet component so children
      can be simpler components
    * Create a start upload function rather than relying on component mounting to
      trigger uploads
    * Add necessary cleanup: cleanup state in useUploadObservations and pull
      ObsListBottomSheet from the DOM to release and saved state/references

--- src/components/Observations/ObservationViews.js
+++ src/components/Observations/ObservationViews.js
@@ -37,11 +37,11 @@ const ObservationViews = ( ): Node => {
   const [view, setView] = useState( "list" );
   const navigation = useNavigation( );
   const currentUser = useCurrentUser( );
-  const { observationList } = localObservations;
+  const { observationList, unuploadedObsList } = localObservations;
   const [hasScrolled, setHasScrolled] = useState( false );
   const [idBelow, setIdBelow] = useState( null );
   const isLoading = useInfiniteScroll( idBelow );
-
+  const hasUnuploadedObservations = ( unuploadedObsList?.length || 0 ) > 0;
   // basing collapsible sticky header code off the example in this article
   // https://medium.com/swlh/making-a-collapsible-sticky-header-animations-with-react-native-6ad7763875c3
   const scrollY = useRef( new Animated.Value( 0 ) );
@@ -105,8 +105,6 @@ const ObservationViews = ( ): Node => {
     );
   };
 
-  const renderBottomSheet = ( ) => <ObsListBottomSheet hasScrolled={hasScrolled} />;
-
   const renderItemSeparator = ( ) => <View className="border border-border" />;
 
   const onEndReached = ( ) => {
@@ -149,7 +147,7 @@ const ObservationViews = ( ): Node => {
             onEndReached={onEndReached}
             onEndReachedThreshold={0.1}
           />
-          {renderBottomSheet( )}
+          {hasUnuploadedObservations && <ObsListBottomSheet hasScrolled={hasScrolled} /> }
         </Animated.View>
       </View>
     </ViewWithFooter>
