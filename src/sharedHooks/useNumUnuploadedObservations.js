bf1910a94bf25cdb73513afb05f828b305d3b594
Amanda Bullington <albullington@gmail.com>
2023-01-11 10:29:22 -0800

    Remove optional chaining; update expected return value from hook

--- src/sharedHooks/useNumUnuploadedObservations.js
+++ src/sharedHooks/useNumUnuploadedObservations.js
@@ -5,10 +5,10 @@ import Observation from "realmModels/Observation";
 
 const { useRealm } = RealmContext;
 
-const useNumUnuploadedObservations = ( ): Object => {
+const useNumUnuploadedObservations = ( ): number => {
   const realm = useRealm( );
   const unsyncedObs = Observation.filterUnsyncedObservations( realm );
-  return unsyncedObs?.length;
+  return unsyncedObs.length;
 };
 
 export default useNumUnuploadedObservations;
