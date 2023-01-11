0b822674b14b75576575d64032b16c6d6302ff6c
Johannes Klein <johannes.t.klein@gmail.com>
2023-01-11 16:57:24 +0100

    Add a11y matcher to Search

---
+++ tests/unit/components/Search/Search.test.js
@@ -0,0 +1,15 @@
+import { screen } from "@testing-library/react-native";
+import Search from "components/Search/Search";
+import React from "react";
+
+import { renderComponent } from "../../../helpers/render";
+
+describe( "Search", () => {
+  test( "should not have accessibility errors", async () => {
+    renderComponent( <Search /> );
+    const search = await screen.findByTestId( "Search" );
+    expect( search ).toBeAccessible( );
+
+    screen.unmount( );
+  } );
+} );
