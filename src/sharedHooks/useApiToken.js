41bba23eada613897fc84a100408f05b70ca47e7
Johannes Klein <johannes.t.klein@gmail.com>
2023-01-18 14:26:39 +0100

    Fetch token on every render in useApiToken hook

--- src/sharedHooks/useApiToken.js
+++ src/sharedHooks/useApiToken.js
@@ -6,9 +6,10 @@ import useCurrentUser from "sharedHooks/useCurrentUser";
 
 const useApiToken = ( ): string | null => {
   const [apiToken, setApiToken] = useState( null );
-  const [shouldFetchToken, setShouldFetchToken] = useState( true );
   const currentUser = useCurrentUser( );
 
+  // Without the dependency array, this hook will run on every render, then every time
+  // a new token is fetched, and if expired fetched from API (in getJWT).
   useEffect( ( ) => {
     const fetchApiToken = async ( ) => {
       if ( !currentUser ) {
@@ -21,13 +22,12 @@ const useApiToken = ( ): string | null => {
         if ( token !== apiToken ) {
           setApiToken( token );
         }
-        setShouldFetchToken( false );
       } else {
         console.error( "Failed to get API token even though user is logged in: ", currentUser );
       }
     };
-    if ( shouldFetchToken ) fetchApiToken( );
-  }, [shouldFetchToken, currentUser, apiToken] );
+    fetchApiToken( );
+  } );
 
   return apiToken;
 };
