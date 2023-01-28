4d6b5b10ef139bd1139f104c80608a135663f96c
Johannes Klein <johannes.t.klein@gmail.com>
2023-01-28 23:33:02 +0100

    Revert "Remove useEffect from useApiToken"
    This reverts commit 9cf1e0c0bc655e742cfa61c7b913898c3e1dafb6.

--- src/sharedHooks/useApiToken.js
+++ src/sharedHooks/useApiToken.js
@@ -1,32 +1,32 @@
 // @flow
 
 import { getJWT } from "components/LoginSignUp/AuthenticationService";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useCurrentUser from "sharedHooks/useCurrentUser";
 
 const useApiToken = ( ): string | null => {
   const [apiToken, setApiToken] = useState( null );
   const currentUser = useCurrentUser( );
 
-  const fetchApiToken = async () => {
-    if ( !currentUser ) {
-      setApiToken( null );
-      return;
-    }
-    const token = await getJWT();
-    if ( token ) {
-      if ( token !== apiToken ) {
-        setApiToken( token );
+  // Without the dependency array, this hook will run on every render, then every time
+  // a new token is fetched, and if expired fetched from API (in getJWT).
+  useEffect( ( ) => {
+    const fetchApiToken = async ( ) => {
+      if ( !currentUser ) {
+        setApiToken( null );
+        return;
       }
-    } else {
-      console.error(
-        "Failed to get API token even though user is logged in: ",
-        currentUser
-      );
-    }
-  };
-
-  fetchApiToken();
+      const token = await getJWT( );
+      if ( token ) {
+        if ( token !== apiToken ) {
+          setApiToken( token );
+        }
+      } else {
+        console.error( "Failed to get API token even though user is logged in: ", currentUser );
+      }
+    };
+    fetchApiToken( );
+  } );
 
   return apiToken;
 };
