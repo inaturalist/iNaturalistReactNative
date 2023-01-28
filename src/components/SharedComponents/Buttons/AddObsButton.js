6a61e5e06856eb96c416b52366e8b8286fa2d51f
Chris Chuck <chrischuckgithub@gmail.com>
2023-01-28 09:49:47 -0600

    Incremental changes to navbar component

--- src/components/SharedComponents/Buttons/AddObsButton.js
+++ src/components/SharedComponents/Buttons/AddObsButton.js
@@ -5,14 +5,15 @@ import Modal from "components/SharedComponents/Modal";
 import * as React from "react";
 import { Pressable } from "react-native";
 import IconMaterial from "react-native-vector-icons/MaterialIcons";
+import colors from "styles/tailwindColors";
 
-const AddObsButton = ( ): React.Node => {
+const AddObsButton = (): React.Node => {
   const [showModal, setModal] = React.useState( false );
 
-  const openModal = React.useCallback( ( ) => setModal( true ), [] );
-  const closeModal = React.useCallback( ( ) => setModal( false ), [] );
+  const openModal = React.useCallback( () => setModal( true ), [] );
+  const closeModal = React.useCallback( () => setModal( false ), [] );
 
-  const navToAddObs = ( ) => openModal( );
+  const navToAddObs = () => openModal();
 
   return (
     <>
@@ -21,8 +22,17 @@ const AddObsButton = ( ): React.Node => {
         closeModal={closeModal}
         modal={<AddObsModal closeModal={closeModal} />}
       />
-      <Pressable testID="camera-options-button" onPress={navToAddObs} accessibilityRole="link">
-        <IconMaterial name="add-circle" size={30} />
+      <Pressable
+        testID="camera-options-button"
+        onPress={navToAddObs}
+        accessibilityRole="link"
+      >
+        <IconMaterial
+          className="shadow-lg"
+          name="add-circle"
+          size={60}
+          color={colors.primary}
+        />
       </Pressable>
     </>
   );
--- src/components/SharedComponents/ViewWithFooter.js
+++ src/components/SharedComponents/ViewWithFooter.js
@@ -4,7 +4,7 @@ import { SafeAreaView } from "components/styledComponents";
 import * as React from "react";
 import { StatusBar } from "react-native";
 
-import Footer from "./Footer";
+import NavBar from "./NavBar/NavBar";
 
 type Props = {
   children: React.Node,
@@ -15,7 +15,7 @@ const ViewWithFooter = ( { children, testID }: Props ): React.Node => (
   <SafeAreaView className="flex-1 bg-white" testID={testID}>
     <StatusBar barStyle="dark-content" />
     {children}
-    <Footer />
+    <NavBar />
   </SafeAreaView>
 );
 
