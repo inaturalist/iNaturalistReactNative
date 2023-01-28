e00996ed318bfb91904591c0aa5e4a11f909af31
Chris Chuck <chrischuckgithub@gmail.com>
2023-01-28 07:01:25 -0600

    Make minimal required changes

--- src/sharedHooks/useUserLocation.js
+++ src/sharedHooks/useUserLocation.js
@@ -1,6 +1,7 @@
 // @flow
+
 import Geolocation from "@react-native-community/geolocation";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Platform } from "react-native";
 import { PERMISSIONS, request } from "react-native-permissions";
 import fetchPlaceName from "sharedHelpers/fetchPlaceName";
@@ -9,75 +10,64 @@ type Props = {
   skipPlaceGuess?: bool
 }
 
-type LatLng = {
-  place_guess: string | null | typeof undefined,
-  latitude: number,
-  longitude: number,
-  positional_accuracy: any
-}
-
 const useUserLocation = ( { skipPlaceGuess = false }: Props ): Object => {
-  const [latLng, setLatLng] = useState<LatLng | null>( null );
+  const [latLng, setLatLng] = useState( null );
   const [isLoading, setIsLoading] = useState( true );
 
   // TODO: wrap this in PermissionsGate so permissions aren't requested at odd times
-  const requestiOSPermissions = ( ): string | null => {
+  const requestiOSPermissions = async ( ): Promise<?string> => {
     // TODO: test this on a real device
     try {
-      return request( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE );
+      const permission = await request( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE );
+      return permission;
     } catch ( e ) {
       console.warn( e, ": error requesting iOS permissions" );
     }
     return null;
   };
 
-  // TODO: set geolocation fetch error
-  const failure = error => {
-    console.warn( error.code, error.message );
-    setIsLoading( false );
-  };
-
-  const success = ( { coords } ) => {
-    setLatLng( {
-      place_guess: null,
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-      positional_accuracy: coords.accuracy
-    } );
-  };
-
-  const fetchLocation = useCallback( async ( ) => {
-    if ( Platform.OS === "ios" ) {
-      const permissions = await requestiOSPermissions( );
-      // TODO: handle case where iOS permissions are not granted
-      if ( permissions !== "granted" ) { return; }
-    }
-
-    const options = { enableHighAccuracy: true, maximumAge: 0 };
-
-    Geolocation.getCurrentPosition( success, failure, options );
-  }, [] );
-
-  const fetchPlaceGuess = useCallback( async ( ) => {
-    if ( latLng ) {
-      const placeGuess = await fetchPlaceName( latLng.latitude, latLng.longitude );
-      setLatLng( currentLatLng => currentLatLng && {
-        ...currentLatLng, place_guess: placeGuess
-      } );
-    }
-  }, [latLng] );
-
-  useEffect( () => {
-    fetchLocation();
-  }, [fetchLocation] );
-
   useEffect( ( ) => {
-    setIsLoading( true );
-    if ( !skipPlaceGuess ) {
-      fetchPlaceGuess();
-    }
-    setIsLoading( false );
-  }, [fetchPlaceGuess, skipPlaceGuess] );
+    let isCurrent = true;
+
+    const fetchLocation = async ( ) => {
+      setIsLoading( true );
+      if ( Platform.OS === "ios" ) {
+        const permissions = await requestiOSPermissions( );
+        // TODO: handle case where iOS permissions are not granted
+        if ( permissions !== "granted" ) { return; }
+      }
+
+      const success = async ( { coords } ) => {
+        if ( !isCurrent ) { return; }
+        let placeGuess = null;
+        if ( !skipPlaceGuess ) {
+          placeGuess = await fetchPlaceName( coords.latitude, coords.longitude );
+        }
+        setLatLng( {
+          place_guess: placeGuess,
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          positional_accuracy: coords.accuracy
+        } );
+        setIsLoading( false );
+      };
+
+      // TODO: set geolocation fetch error
+      const failure = error => {
+        console.warn( error.code, error.message );
+        setIsLoading( false );
+      };
+
+      const options = { enableHighAccuracy: true, maximumAge: 0 };
+
+      Geolocation.getCurrentPosition( success, failure, options );
+    };
+    fetchLocation( );
+
+    return ( ) => {
+      isCurrent = false;
+    };
+  }, [skipPlaceGuess] );
 
   return {
     latLng,
