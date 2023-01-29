f8c403b9b14a0bd306b028fcd5237ee257c2c35b
Johannes Klein <johannes.t.klein@gmail.com>
2023-01-29 10:09:50 +0100

    353 retry with jwt (#382)
    * Rename some functions and variables
    
    * Rename function
    
    * Revert to original timeout for tokens
    
    * Get JWT for a 401 or 403 http errors in the react-query QueryClient retry handler
    
    * Log API error to new logger
    
    * Fetch token on every render in useApiToken hook
    
    * Remove commented code
    
    * Add comment
    
    * Remove useEffect from useApiToken
    
    * Revert "Remove useEffect from useApiToken"
    
    This reverts commit 9cf1e0c0bc655e742cfa61c7b913898c3e1dafb6.

--- src/sharedHooks/useApiToken.js
+++ src/sharedHooks/useApiToken.js
@@ -1,33 +1,32 @@
 // @flow
 
-import { getJWTToken } from "components/LoginSignUp/AuthenticationService";
+import { getJWT } from "components/LoginSignUp/AuthenticationService";
 import { useEffect, useState } from "react";
 import useCurrentUser from "sharedHooks/useCurrentUser";
 
 const useApiToken = ( ): string | null => {
   const [apiToken, setApiToken] = useState( null );
-  const [shouldFetchToken, setShouldFetchToken] = useState( true );
   const currentUser = useCurrentUser( );
 
+  // Without the dependency array, this hook will run on every render, then every time
+  // a new token is fetched, and if expired fetched from API (in getJWT).
   useEffect( ( ) => {
     const fetchApiToken = async ( ) => {
       if ( !currentUser ) {
         setApiToken( null );
-        // setShouldFetchToken( false );
         return;
       }
-      const token = await getJWTToken( );
+      const token = await getJWT( );
       if ( token ) {
         if ( token !== apiToken ) {
           setApiToken( token );
         }
-        setShouldFetchToken( false );
       } else {
         console.error( "Failed to get API token even though user is logged in: ", currentUser );
       }
     };
-    if ( shouldFetchToken ) fetchApiToken( );
-  }, [shouldFetchToken, currentUser, apiToken] );
+    fetchApiToken( );
+  } );
 
   return apiToken;
 };
