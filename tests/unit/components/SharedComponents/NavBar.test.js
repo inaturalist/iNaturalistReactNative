fe17f980d3f9ee6afdad79a4bb7f42cd5939556d
Chris Chuck <chrischuckgithub@gmail.com>
2023-01-30 15:59:04 -0600

    Add more comprehensive test

--- tests/unit/components/SharedComponents/NavBar.test.js
+++ tests/unit/components/SharedComponents/NavBar.test.js
@@ -1,5 +1,6 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { useRoute } from "@react-navigation/native";
+import { screen } from "@testing-library/react-native";
 import NavBar from "components/SharedComponents/NavBar/NavBar";
 import React from "react";
 import { Text } from "react-native";
@@ -11,22 +12,36 @@ const Drawer = createDrawerNavigator( );
 /* eslint-disable i18next/no-literal-string */
 const TestObsList = ( ): React.Node => (
   <>
-    <Text>ObsList</Text>
+    <Text>Obs List</Text>
     <NavBar />
   </>
 );
+
+const TestMessages = ( ): React.Node => (
+  <>
+    <Text>Messages</Text>
+    <NavBar />
+  </>
+);
+
 describe( "NavBar", ( ) => {
   it( "handles active pages correctly", async ( ) => {
     useRoute.mockReturnValueOnce( { name: "ObsList" } );
-    const { getByTestId } = renderComponent(
+    renderComponent(
       <Drawer.Navigator>
         <Drawer.Screen name="ObsList" component={TestObsList} />
+        <Drawer.Screen name="Messages" component={TestMessages} />
       </Drawer.Navigator>
     );
 
-    const navToObsList = getByTestId( "ObsList" );
+    const navToObsList = screen.getByTestId( "ObsList" );
     expect( navToObsList ).toHaveAccessibilityState(
       { selected: true, expanded: true, disabled: true }
     );
+    const navToMessages = screen.getByTestId( "Messages" );
+    expect( navToMessages ).toHaveAccessibilityState(
+      { selected: false, expanded: false, disabled: false }
+    );
+    expect( screen.getByText( "Obs List" ) ).toBeTruthy();
   } );
 } );
