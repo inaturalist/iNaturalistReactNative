cba8e7dbff5d94be8540a554e5ac006d893f1498
Johannes Klein <johannes.t.klein@gmail.com>
2023-01-31 17:55:34 +0100

    Refactor Inline to use UserIcon and add no internet icon

--- src/components/SharedComponents/InlineUser.js
+++ src/components/SharedComponents/InlineUser.js
@@ -2,7 +2,7 @@
 
 import { useNavigation } from "@react-navigation/native";
 import {
-  Image, Pressable, Text, View
+  Pressable, Text, View
 } from "components/styledComponents";
 import { t } from "i18next";
 import type { Node } from "react";
@@ -11,6 +11,8 @@ import IconMaterial from "react-native-vector-icons/MaterialIcons";
 import User from "realmModels/User";
 import useIsConnected from "sharedHooks/useIsConnected";
 import colors from "styles/tailwindColors";
+
+import UserIcon from "./UserIcon";
 
 type Props = {
   user: any
@@ -21,6 +23,31 @@ const InlineUser = ( { user }: Props ): Node => {
   const isOnline = useIsConnected( );
   const userImgUri = User.uri( user );
   const userHandle = User.userHandle( user );
+
+  const renderUserIcon = () => {
+    if ( !isOnline ) {
+      return (
+        <IconMaterial
+          testID="InlineUser.NoInternetPicture"
+          name="wifi-off"
+          size={22}
+          color={colors.logInGray}
+        />
+      );
+    }
+    if ( !userImgUri ) {
+      return (
+        <IconMaterial
+          testID="InlineUser.FallbackPicture"
+          name="person"
+          size={22}
+          color={colors.logInGray}
+        />
+      );
+    }
+    return <UserIcon uri={userImgUri} small />;
+  };
+
   return (
     <Pressable
       testID="InlineUser"
@@ -32,22 +59,9 @@ const InlineUser = ( { user }: Props ): Node => {
         navigation.navigate( "UserProfile", { userId: user.id } );
       }}
     >
-      {userImgUri && isOnline ? (
-        <Image
-          testID="InlineUser.ProfilePicture"
-          className="w-[22px] h-[22px] rounded-full mr-[7px]"
-          source={userImgUri}
-        />
-      ) : (
-        <View className="mr-[7px]">
-          <IconMaterial
-            testID="InlineUser.FallbackPicture"
-            name="person"
-            size={22}
-            color={colors.logInGray}
-          />
-        </View>
-      )}
+      <View className="mr-[7px]">
+        {renderUserIcon()}
+      </View>
       <Text>{userHandle}</Text>
     </Pressable>
   );
