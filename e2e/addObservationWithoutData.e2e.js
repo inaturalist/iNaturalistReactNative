b30bb955c8699d52872feed3e46f134ff4e7d8d4
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-02-01 22:02:14 -0800

    Re-arrange deck chairs; enable manual trigger for e2e workflows

--- e2e/addObservationWithoutData.e2e.js
+++ e2e/addObservationWithoutData.e2e.js
@@ -29,20 +29,18 @@ describe( "Add observation without evidence", () => {
 
   it( "should open app with the observation list screen", async () => {
     const loginText = element( by.id( "log-in-to-iNaturalist-text" ) );
-    await waitFor( loginText ).toBeVisible().withTimeout( 10000 );
-    await expect( loginText ).toBeVisible();
+    await waitFor( loginText ).toBeVisible( ).withTimeout( 10000 );
+    await expect( loginText ).toBeVisible( );
   } );
 
   it( "should navigate to observation add screen on add evidence button pressed", async () => {
-    await waitFor( element( by.id( "add-obs-button" ) ) )
-      .toBeVisible()
-      .withTimeout( 10000 );
-    await element( by.id( "add-obs-button" ) ).tap();
-    await expect( element( by.id( "evidence-text" ) ) ).toBeVisible();
-    await expect(
-      element( by.id( "observe-without-evidence-button" ) )
-    ).toBeVisible();
-    await element( by.id( "observe-without-evidence-button" ) ).tap();
-    await waitFor( element( by.id( "new-observation-text" ) ) ).toBeVisible().withTimeout( 10000 );
+    const addObsButton = element( by.id( "add-obs-button" ) );
+    await waitFor( addObsButton ).toBeVisible( ).withTimeout( 10000 );
+    await addObsButton.tap( );
+    await expect( element( by.id( "evidence-text" ) ) ).toBeVisible( );
+    const obsWithoutEvidenceButton = element( by.id( "observe-without-evidence-button" ) );
+    await expect( obsWithoutEvidenceButton ).toBeVisible( );
+    await obsWithoutEvidenceButton.tap( );
+    await waitFor( element( by.id( "new-observation-text" ) ) ).toBeVisible( ).withTimeout( 10000 );
   } );
 } );
