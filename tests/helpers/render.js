21dcab64b0ddba0c6176b304abab722bfaf91cfc
Johannes Klein <johannes.t.klein@gmail.com>
2023-02-03 10:56:41 +0100

    Remove not needed function

--- tests/helpers/render.js
+++ tests/helpers/render.js
@@ -1,4 +1,3 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import {
   QueryClient,
@@ -7,8 +6,6 @@ import {
 import { render } from "@testing-library/react-native";
 import App from "components/App";
 import React from "react";
-
-const Drawer = createDrawerNavigator( );
 
 const queryClient = new QueryClient( {
   defaultOptions: {
@@ -33,21 +30,11 @@ function renderComponent( component, update = null ) {
   );
 }
 
-function renderScreen( component, name, update ) {
-  return renderComponent(
-    <Drawer.Navigator>
-      <Drawer.Screen component={component} name={name} />
-    </Drawer.Navigator>,
-    update
-  );
-}
-
 function renderAppWithComponent( component, update = null ) {
   return renderComponent( <App>{ component }</App>, update );
 }
 
 export {
   renderAppWithComponent,
-  renderComponent,
-  renderScreen
+  renderComponent
 };
