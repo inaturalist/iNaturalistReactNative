cd2a34becf7deb8929f4e2231927f517bd6a477d
Chris Chuck <chrischuckgithub@gmail.com>
2023-02-06 04:41:31 -0600

    Change document order for a11y

--- src/components/DisplayTaxonName.js
+++ src/components/DisplayTaxonName.js
@@ -13,53 +13,54 @@ type Props = {
   layout?: string,
 };
 
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
   commonName: {
-    maxWidth: "100%"
+    maxWidth: "100%",
   },
   scientificName: {
-    fontStyle: "italic"
-  }
-} );
+    fontStyle: "italic",
+  },
+});
 
-const DisplayTaxonName = ( {
+const DisplayTaxonName = ({
   layout = "list",
-  item: { user, taxon }
-}: Props ): Node => {
+  item: { user, taxon },
+}: Props): Node => {
   const { t } = useTranslation();
 
-  if ( !taxon ) {
-    return <Body1 numberOfLines={1}>{t( "unknown" )}</Body1>;
+  if (!taxon) {
+    return <Body1 numberOfLines={1}>{t("unknown")}</Body1>;
   }
 
-  const taxonData = generateTaxonPieces( taxon );
+  const taxonData = generateTaxonPieces(taxon);
 
   const isListView = layout === "list";
   const scientificNameFirst = user?.prefers_scientific_name_first;
 
+  const renderCommonName = () =>
+    taxonData.commonName && (
+      <Body1 numberOfLines={3} style={styles.commonName}>
+        {`${taxonData.commonName}${
+          !scientificNameFirst && isListView ? " " : ""
+        }`}
+      </Body1>
+    );
+
   return (
     <View
       testID="display-taxon-name"
-      className={classnames( "flex", {
-        "flex-row-reverse flex-wrap-reverse justify-end items-start":
-          scientificNameFirst && isListView,
-        "flex-row items-end flex-wrap": isListView && !scientificNameFirst,
-        "w-11/12": isListView,
-        "flex-col-reverse": !isListView && scientificNameFirst
-      } )}
+      className={classnames("flex", {
+        "flex-row items-end flex-wrap w-111/12": isListView,
+        "flex-col-reverse": !isListView && scientificNameFirst,
+      })}
     >
-      {taxonData.commonName && (
-        <Body1 numberOfLines={3} style={styles.commonName}>
-          {`${taxonData.commonName}${
-            !scientificNameFirst && isListView ? " " : ""
-          }`}
-        </Body1>
-      )}
+      {!scientificNameFirst && renderCommonName()}
       <Body3 style={styles.scientificName}>
         {`${taxonData.scientificName}${
           scientificNameFirst && isListView ? " " : ""
         }`}
       </Body3>
+      {scientificNameFirst && renderCommonName()}
     </View>
   );
 };
