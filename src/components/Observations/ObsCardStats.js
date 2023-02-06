daa020200cef180f607e2ef497f29f9b16d9cce4
Chris Chuck <chrischuckgithub@gmail.com>
2023-02-05 18:22:05 -0600

    Add item id count

--- src/components/Observations/ObsCardStats.js
+++ src/components/Observations/ObsCardStats.js
@@ -2,8 +2,8 @@
 
 import classnames from "classnames";
 import checkCamelAndSnakeCase from "components/ObsDetails/helpers/checkCamelAndSnakeCase";
-import { ActivityCount, QualityGradeStatus } from "components/SharedComponents";
-import { View } from "components/styledComponents";
+import { ActivityCount, Body3, QualityGradeStatus } from "components/SharedComponents";
+import { Image, View } from "components/styledComponents";
 import type { Node } from "react";
 import React from "react";
 import { useTranslation } from "react-i18next";
@@ -13,7 +13,7 @@ type Props = {
   item: Object,
   layout?: string
 }
-
+/* eslint-disable react-native/no-inline-styles */
 const ObsCardStats = ( { item, layout }: Props ): Node => {
   const { t } = useTranslation( );
   const theme = useTheme( );
@@ -29,6 +29,7 @@ const ObsCardStats = ( { item, layout }: Props ): Node => {
     return theme.colors.primary;
   };
 
+  const iconColor = getIconColor( );
   const margin = layout === "list" ? "mb-2.5" : "mr-2.5";
   const flexDirection = layout === "list" ? "flex-column" : "flex-row";
   return (
@@ -37,21 +38,33 @@ const ObsCardStats = ( { item, layout }: Props ): Node => {
       flexDirection
     )}
     >
-      <ActivityCount
-        marginClass={margin}
-        count={item.identifications?.length}
-        color={getIconColor( )}
-        accessibilityLabel={t( "Number-of-identifications" )}
-        testID="ActivityCount.identificationCount"
-      />
+      {
+        /*
+        @TODO add ic_id icon
+        <ActivityCount
+          marginClass={margin}
+          count={item.identifications?.length}
+          color={iconColor}
+          accessibilityLabel={t( "Number-of-identifications" )}
+          testID="ActivityCount.identificationCount"
+        />
+        */
+      }
+      <View className={classnames( margin, "flex flex-row items-center" )}>
+        <Image
+          style={{ height: 15, width: 15, tintColor: iconColor }}
+          source={require( "images/ic_id.png" )}
+        />
+        <Body3 className="ml-1" style={{ color: iconColor }}>{item.identifications.length}</Body3>
+      </View>
       <ActivityCount
         marginClass={margin}
         count={item.comments?.length}
-        color={getIconColor( )}
+        color={iconColor}
         accessibilityLabel={t( "Number-of-comments" )}
         testID="ActivityCount.commentCount"
       />
-      <QualityGradeStatus qualityGrade={qualityGrade} color={getIconColor( )} />
+      <QualityGradeStatus qualityGrade={qualityGrade} color={iconColor} />
     </View>
   );
 };
