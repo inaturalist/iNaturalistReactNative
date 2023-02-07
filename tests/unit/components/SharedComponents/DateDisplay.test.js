401736ab3aac2b2fdbd1da43089a6b5d8b71b657
Chris Chuck <chrischuckgithub@gmail.com>
2023-02-06 19:43:38 -0600

    Fix tests

--- tests/unit/components/SharedComponents/DateDisplay.test.js
+++ tests/unit/components/SharedComponents/DateDisplay.test.js
@@ -11,12 +11,12 @@ describe( "DateDisplay", () => {
 
   it( "should format datetime correctly from date string", () => {
     renderComponent( <DateDisplay dateTime="2023-12-14T21:07:41-09:30" /> );
-    expect( screen.getByText( "12/15/23 12:37 AM" ) ).toBeTruthy( );
+    expect( screen.getByText( "12/15/23 6:37 AM" ) ).toBeTruthy( );
   } );
 
   it( "should format datetime correctly from date string", () => {
     renderComponent( <DateDisplay dateTime="2023-01-02T21:09:41-23:30" /> );
-    expect( screen.getByText( "1/3/23 2:39 PM" ) ).toBeTruthy( );
+    expect( screen.getByText( "1/3/23 8:39 PM" ) ).toBeTruthy( );
   } );
 
   it( "should display placeholder if no dateTime", () => {
