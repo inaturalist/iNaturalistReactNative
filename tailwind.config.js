d0aebdd8a34a40284f5de294eeb535ab4d518d4a
Johannes Klein <johannes.t.klein@gmail.com>
2023-02-08 21:59:59 +0100

    Add rounded-lg

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,7 +63,8 @@ module.exports = {
       },
       borderRadius: {
         // tried using rem value here, but it wouldn't load on iOS or Android
-        DEFAULT: "7px"
+        DEFAULT: "7px",
+        lg: "8px"
       }
     },
     colors: {
