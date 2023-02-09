2c156bbbb774a8ad2f784fb5e3d2339261820d59
Chris Chuck <chrischuckgithub@gmail.com>
2023-02-09 17:15:56 -0600

    Space parens

--- tests/unit/components/Observations/ObsList.test.js
+++ tests/unit/components/Observations/ObsList.test.js
@@ -5,7 +5,7 @@ import React from "react";
 import factory from "../../../factory";
 import { renderComponent } from "../../../helpers/render";
 
-jest.useFakeTimers();
+jest.useFakeTimers( );
 
 const mockObservations = [
   factory( "LocalObservation", {
@@ -24,24 +24,24 @@ jest.mock( "sharedHooks/useApiToken" );
 
 const mockUser = factory( "LocalUser" );
 
-jest.mock( "sharedHooks/useCurrentUser", () => ( {
+jest.mock( "sharedHooks/useCurrentUser", ( ) => ( {
   __esModule: true,
-  default: () => mockUser
+  default: ( ) => mockUser
 } ) );
 
-jest.mock( "sharedHooks/useLocalObservations", () => ( {
+jest.mock( "sharedHooks/useLocalObservations", ( ) => ( {
   __esModule: true,
-  default: () => ( {
+  default: ( ) => ( {
     observationList: mockObservations,
     allObsToUpload: []
   } )
 } ) );
 
-jest.mock( "@react-navigation/native", () => {
+jest.mock( "@react-navigation/native", ( ) => {
   const actualNav = jest.requireActual( "@react-navigation/native" );
   return {
     ...actualNav,
-    useRoute: () => ( {
+    useRoute: ( ) => ( {
       params: {
         id: mockObservations[0].uuid
       }
@@ -49,7 +49,7 @@ jest.mock( "@react-navigation/native", () => {
   };
 } );
 
-it( "renders an observation", async () => {
+it( "renders an observation", async ( ) => {
   renderComponent( <ObsList /> );
   const obs = mockObservations[0];
 
@@ -58,30 +58,30 @@ it( "renders an observation", async () => {
   expect( list.props.data.length ).toEqual( mockObservations.length );
   // Test that a card got rendered for the our test obs
   const card = await screen.findByTestId( `ObsList.obsListItem.${obs.uuid}` );
-  expect( card ).toBeTruthy();
+  expect( card ).toBeTruthy( );
   // Test that the card has the correct comment count
   const commentCount = within( card ).getByTestId( "ActivityCount.commentCount" );
   // TODO: I disabled node eslint rule here because we will soon have to refactor this
   // test into it's own unit test, because the comment count will be a component
   // after the refactor we should change this line to be in compliance with the eslint rule
   // eslint-disable-next-line testing-library/no-node-access
-  expect( commentCount.children[0] ).toEqual( obs.comments.length.toString() );
+  expect( commentCount.children[0] ).toEqual( obs.comments.length.toString( ) );
 } );
 
-it( "renders multiple observations", async () => {
+it( "renders multiple observations", async ( ) => {
   renderComponent( <ObsList /> );
   // Awaiting the first observation because using await in the forEach errors out
   const firstObs = mockObservations[0];
   await screen.findByTestId( `ObsList.obsListItem.${firstObs.uuid}` );
   mockObservations.forEach( obs => {
-    expect( screen.getByTestId( `ObsList.obsListItem.${obs.uuid}` ) ).toBeTruthy();
+    expect( screen.getByTestId( `ObsList.obsListItem.${obs.uuid}` ) ).toBeTruthy( );
   } );
   // TODO: some things are still happening in the background so I unmount here,
   // better probably to mock away those things happening in the background for this test
-  screen.unmount();
+  screen.unmount( );
 } );
 
-it( "renders grid view on button press", async () => {
+it( "renders grid view on button press", async ( ) => {
   renderComponent( <ObsList /> );
   const button = await screen.findByTestId( "ObsList.toggleGridView" );
   fireEvent.press( button );
@@ -89,14 +89,14 @@ it( "renders grid view on button press", async () => {
   const firstObs = mockObservations[0];
   await screen.findByTestId( `ObsList.gridItem.${firstObs.uuid}` );
   mockObservations.forEach( obs => {
-    expect( screen.getByTestId( `ObsList.gridItem.${obs.uuid}` ) ).toBeTruthy();
+    expect( screen.getByTestId( `ObsList.gridItem.${obs.uuid}` ) ).toBeTruthy( );
   } );
 } );
 
-describe( "ObsList", () => {
-  test( "should not have accessibility errors", () => {
+describe( "ObsList", ( ) => {
+  test( "should not have accessibility errors", ( ) => {
     renderComponent( <ObsList /> );
     const obsList = screen.getByTestId( "ObservationViews.myObservations" );
-    expect( obsList ).toBeAccessible();
+    expect( obsList ).toBeAccessible( );
   } );
 } );
