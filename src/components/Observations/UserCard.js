9883745b88d9fa5bb63b98e375a243f4b6ffa94b
Chris Chuck <chrischuckgithub@gmail.com>
2023-02-11 09:16:43 -0600

    User trans component for complex interpolations

--- src/components/Observations/UserCard.js
+++ src/components/Observations/UserCard.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { Body1 } from "components/SharedComponents";
-import { View } from "components/styledComponents";
+import {View } from "components/styledComponents";
 import type { Node } from "react";
 import React from "react";
 import { Trans } from "react-i18next";
@@ -15,8 +15,8 @@ const UserCard = ( ): Node => {
     <View className="flex-row px-5 items-center bg-white h-24 ml-3">
       <Trans
         i18nKey="Welcome-user"
-        values={{ userHandle: User.userHandle( currentUser ) }}
         parent={View}
+        values={{ userHandle: User.userHandle( currentUser ) }}
         components={[
           <Body1 className="mt-1 text-3xl" />,
           <Body1 className="mb-1 text-3xl font-semibold" />
