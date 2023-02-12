7d37ea1cbbf27df7c6f78bd17424e572c9ccdd9a
Chris Chuck <chrischuckgithub@gmail.com>
2023-02-12 11:54:49 -0600

    Integrate reusable components into obs details

--- src/components/ObsDetails/DataTab.js
+++ src/components/ObsDetails/DataTab.js
@@ -1,14 +1,16 @@
 // @flow
 
-import Map from "components/SharedComponents/Map";
+import {
+  DateDisplay,
+  Map,
+  ObservationLocation
+} from "components/SharedComponents";
 import { Text, View } from "components/styledComponents";
-import { format, parseISO } from "date-fns";
 import { t } from "i18next";
 import type { Node } from "react";
 import React from "react";
 import IconMaterial from "react-native-vector-icons/MaterialIcons";
 import useIsConnected from "sharedHooks/useIsConnected";
-import colors from "styles/tailwindColors";
 
 import Attribution from "./Attribution";
 import checkCamelAndSnakeCase from "./helpers/checkCamelAndSnakeCase";
@@ -20,11 +22,6 @@ type Props = {
 const DataTab = ( { observation }: Props ): Node => {
   const isOnline = useIsConnected( );
   const application = observation?.application?.name;
-
-  const displayTime = datetime => {
-    const time = checkCamelAndSnakeCase( observation, datetime );
-    return time ? format( parseISO( time ), "M/d/yy HH:mm a" ) : "";
-  };
 
   return (
     <View>
@@ -55,25 +52,18 @@ const DataTab = ( { observation }: Props ): Node => {
           </View>
         )}
       <View className="px-5">
-        <View className="flex-row my-3">
-          <IconMaterial name="location-pin" size={15} color={colors.logInGray} />
-          <Text className="ml-2">
-            {checkCamelAndSnakeCase( observation, "placeGuess" )}
-          </Text>
+        <View className="my-3">
+          <ObservationLocation observation={observation} />
         </View>
         <Text className="text-lg my-3">{t( "Date" )}</Text>
-        <View className="flex-row">
-          <IconMaterial name="schedule" size={15} color={colors.logInGray} />
-          <Text className="ml-2">
-            {`${t( "Date-observed-colon" )} ${displayTime( "timeObservedAt" )}`}
-          </Text>
-        </View>
-        <View className="flex-row">
-          <IconMaterial name="schedule" size={15} color={colors.logInGray} />
-          <Text className="ml-2">
-            {`${t( "Date-uploaded-colon" )} ${displayTime( "createdAt" )}`}
-          </Text>
-        </View>
+        <DateDisplay
+          pretext={t( "Date-observed-colon" )}
+          dateTime={checkCamelAndSnakeCase( observation, "timeObservedAt" )}
+        />
+        <DateDisplay
+          pretext={t( "Date-observed-colon" )}
+          dateTime={checkCamelAndSnakeCase( observation, "createdAt" )}
+        />
         <Text className="text-lg my-3">{t( "Other-Data" )}</Text>
         <Attribution observation={observation} />
         {application && (
--- src/components/SharedComponents/DateDisplay.js
+++ src/components/SharedComponents/DateDisplay.js
@@ -8,10 +8,12 @@ import IconMaterial from "react-native-vector-icons/MaterialIcons";
 import { formatObsListTime } from "sharedHelpers/dateAndTime";
 
 type Props = {
-  dateTime: string | typeof undefined
+  dateTime: string | typeof undefined,
+  pretext?: string
 };
 
 const DateDisplay = ( {
+  pretext,
   dateTime
 }: Props ): React.Node => {
   const { t } = useTranslation( );
@@ -25,6 +27,7 @@ const DateDisplay = ( {
   return (
     <View className="flex flex-row items-center">
       <IconMaterial name="watch-later" size={15} />
+      {pretext && <Body4 className="ml-[5px]">{ pretext }</Body4>}
       <Body4 className="text-darkGray ml-[5px]">
         {displayTime( )}
       </Body4>
