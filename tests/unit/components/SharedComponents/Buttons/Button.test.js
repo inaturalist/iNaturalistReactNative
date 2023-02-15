3f0c7a193a93fc71833715e4412468f25634072c
Johannes Klein <johannes.t.klein@gmail.com>
2023-02-15 10:55:25 +0100

    Use .each in Button test

--- tests/unit/components/SharedComponents/Buttons/Button.test.js
+++ tests/unit/components/SharedComponents/Buttons/Button.test.js
@@ -2,120 +2,36 @@ import { render, screen } from "@testing-library/react-native";
 import { Button } from "components/SharedComponents";
 import React from "react";
 
-describe( "Button", () => {
-  describe( "primary", () => {
-    it( "renders correctly", async () => {
-      render( <Button level="primary" text="PRIMARY BUTTON" /> );
+// Create the above tests with a descirbe.each loop
+describe.each( [["primary"], ["warning"], ["focus"], ["neutral"]] )(
+  "Button %s",
+  level => {
+    it( "should render correctly", async () => {
+      render( <Button level={level} text={`${level.toUpperCase()} BUTTON`} /> );
       // Snapshot test
       expect( screen ).toMatchSnapshot();
     } );
 
     it( "has no accessibility errors", async () => {
-      const button = <Button level="primary" text="PRIMARY BUTTON" />;
+      const button = <Button level={level} text={`${level.toUpperCase()} BUTTON`} />;
       // Snapshot test
       expect( button ).toBeAccessible();
     } );
-  } );
 
-  describe( "neutral", () => {
-    it( "renders correctly", async () => {
-      render( <Button text="NEUTRAL BUTTON" /> );
-      // Snapshot test
-      expect( screen ).toMatchSnapshot();
-    } );
+    describe( "when disabled", () => {
+      it( "should render correctly", async () => {
+        render( <Button level={level} text={`${level.toUpperCase()} DISABLED`} disabled /> );
+        // Snapshot test
+        expect( screen ).toMatchSnapshot();
+      } );
 
-    it( "has no accessibility errors", async () => {
-      const button = <Button text="NEUTRAL BUTTON" />;
-      // Snapshot test
-      expect( button ).toBeAccessible();
+      it( "has no accessibility errors", async () => {
+        const button = (
+          <Button level={level} text={`${level.toUpperCase()} DISABLED`} disabled />
+        );
+          // Snapshot test
+        expect( button ).toBeAccessible();
+      } );
     } );
-  } );
-
-  describe( "focus", () => {
-    it( "renders correctly", async () => {
-      render( <Button level="focus" text="FOCUS BUTTON" /> );
-      // Snapshot test
-      expect( screen ).toMatchSnapshot();
-    } );
-
-    it( "has no accessibility errors", async () => {
-      const button = <Button level="focus" text="FOCUS BUTTON" />;
-      // Snapshot test
-      expect( button ).toBeAccessible();
-    } );
-  } );
-
-  describe( "warning", () => {
-    it( "renders correctly", async () => {
-      render( <Button level="warning" text="WARNING BUTTON" /> );
-      // Snapshot test
-      expect( screen ).toMatchSnapshot();
-    } );
-
-    it( "has no accessibility errors", async () => {
-      const button = <Button level="warning" text="WARNING BUTTON" />;
-      // Snapshot test
-      expect( button ).toBeAccessible();
-    } );
-  } );
-
-  describe( "primary disabled", () => {
-    it( "renders correctly", async () => {
-      render( <Button level="primary" text="PRIMARY DISABLED" disabled /> );
-      // Snapshot test
-      expect( screen ).toMatchSnapshot();
-    } );
-
-    it( "has no accessibility errors", async () => {
-      const button = (
-        <Button level="primary" text="PRIMARY DISABLED" disabled />
-      );
-      // Snapshot test
-      expect( button ).toBeAccessible();
-    } );
-  } );
-
-  describe( "neutral disabled", () => {
-    it( "renders correctly", async () => {
-      render( <Button text="NEUTRAL DISABLED" disabled /> );
-      // Snapshot test
-      expect( screen ).toMatchSnapshot();
-    } );
-
-    it( "has no accessibility errors", async () => {
-      const button = <Button text="NEUTRAL DISABLED" disabled />;
-      // Snapshot test
-      expect( button ).toBeAccessible();
-    } );
-  } );
-
-  describe( "focus disabled", () => {
-    it( "renders correctly", async () => {
-      render( <Button level="focus" text="FOCUS DISABLED" disabled /> );
-      // Snapshot test
-      expect( screen ).toMatchSnapshot();
-    } );
-
-    it( "has no accessibility errors", async () => {
-      const button = <Button level="focus" text="FOCUS DISABLED" disabled />;
-      // Snapshot test
-      expect( button ).toBeAccessible();
-    } );
-  } );
-
-  describe( "warning disabled", () => {
-    it( "renders correctly", async () => {
-      render( <Button level="warning" text="WARNING DISABLED" disabled /> );
-      // Snapshot test
-      expect( screen ).toMatchSnapshot();
-    } );
-
-    it( "has no accessibility errors", async () => {
-      const button = (
-        <Button level="warning" text="WARNING DISABLED" disabled />
-      );
-      // Snapshot test
-      expect( button ).toBeAccessible();
-    } );
-  } );
-} );
+  }
+);
