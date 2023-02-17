6a1ef013994dfeae517b95d3980aba6e13d1a46f
Chris Chuck <chrischuckgithub@gmail.com>
2023-02-16 18:07:10 -0600

    Use it.each

--- tests/unit/components/SharedComponents/ObservationLocation.test.js
+++ tests/unit/components/SharedComponents/ObservationLocation.test.js
@@ -5,6 +5,48 @@ import React from "react";
 
 import factory from "../../../factory";
 
+const latitude = 30.18183
+const longitude = -85.760449
+
+const testData = [
+  [
+    "should format location correctly from place_guess",
+    {
+      latitude,
+      longitude,
+      place_guess: "Panama City Beach, Florida"
+    },
+    "Panama City Beach, Florida"
+  ],
+  [
+    "should format location correctly from latitude/longitude",
+    {
+      latitude,
+      longitude,
+      place_guess: null
+    },
+    `${latitude}, ${longitude}`
+  ],
+  [
+    "should handle latitude/longitude w/ zero",
+    {
+      latitude: 0,
+      longitude: 0,
+      place_guess: null
+    },
+    "0, 0"
+  ],
+  [
+    "should show no location if unknown",
+    {
+      latitude: null,
+      longitude: null,
+      place_guess: null
+    },
+    "Missing Location"
+  ]
+]
+
 describe( "ObservationLocation", () => {
   beforeAll( async ( ) => {
     await initI18next( );
@@ -17,61 +59,12 @@ describe( "ObservationLocation", () => {
     ).toBeAccessible();
   } );
 
-  it( "should format location correctly from place_guess", async ( ) => {
-    const mockObservation = factory( "RemoteObservation", {
-      latitude: 30.18183,
-      longitude: -85.760449,
-      place_guess: "Panama City Beach, Florida"
-    } );
+  it.each(testData)('%s', async (a, obsData, expectedResult) => {
+    const mockObservation = factory( "RemoteObservation", obsData );
 
     render(
       <ObservationLocation observation={mockObservation} />
     );
-    expect( await screen.findByText( mockObservation.place_guess ) ).toBeTruthy();
-  } );
-
-  it( "should format location correctly from latitude/longitude", async ( ) => {
-    const mockObservation = factory( "RemoteObservation", {
-      latitude: 30.18183,
-      longitude: -85.760449,
-      place_guess: null
-    } );
-
-    render(
-      <ObservationLocation observation={mockObservation} />
-    );
-    expect( await screen.findByText(
-      `${mockObservation.latitude}, ${mockObservation.longitude}`
-    ) ).toBeTruthy();
-  } );
-
-  it( "should handle latitude/longitude w/ zero", async ( ) => {
-    const mockObservation = factory( "RemoteObservation", {
-      latitude: 0,
-      longitude: 0,
-      place_guess: null
-    } );
-
-    render(
-      <ObservationLocation observation={mockObservation} />
-    );
-    expect( await screen.findByText(
-      `${0}, ${0}`
-    ) ).toBeTruthy();
-  } );
-
-  it( "should show no location if unknown", async ( ) => {
-    const mockObservation = factory( "RemoteObservation", {
-      latitude: null,
-      longitude: null,
-      place_guess: null
-    } );
-
-    render(
-      <ObservationLocation observation={mockObservation} />
-    );
-    expect( await screen.findByText(
-      "Missing Location"
-    ) ).toBeTruthy();
-  } );
-} );
+    expect( await screen.findByText( expectedResult ) ).toBeTruthy();
+  });
+});
