afc440f6bb5b2434dfb7fd2b8b02d137f9b15811
Johannes Klein <johannes.t.klein@gmail.com>
2023-02-21 16:34:12 +0100

    Add snapshot test for ActivityCount

---
+++ tests/unit/components/SharedComponents/ActivityCount/ActivityCount.test.js
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react-native";
+import { ActivityCount } from "components/SharedComponents";
+import initI18next from "i18n/initI18next";
+import React from "react";
+
+const count = 0;
+const icon = "comments-filled-in";
+const testID = "some_id";
+
+describe( "ActivityCount", () => {
+  beforeAll( async () => {
+    await initI18next();
+  } );
+
+  it( "renders reliably", () => {
+    // Snapshot test
+    render(
+      <ActivityCount
+        count={1}
+        icon={icon}
+        testID={testID}
+      />
+    );
+    expect( screen ).toMatchSnapshot();
+  } );
+
+  // a11y test
+  it( "should not have accessibility errors", () => {
+    const activityCount = (
+      <ActivityCount
+        count={count}
+        icon={icon}
+        testID={testID}
+      />
+    );
+    expect( activityCount ).toBeAccessible();
+  } );
+} );
