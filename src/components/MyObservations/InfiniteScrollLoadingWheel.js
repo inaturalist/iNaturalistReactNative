c0db3bbf04da400e117ae27c9664f84416fb02e1
Amanda Bullington <albullington@gmail.com>
2023-03-03 15:55:57 -0800

    Update infinite scroll to 50 obs at a time; make loading wheel show faster

--- src/components/MyObservations/InfiniteScrollLoadingWheel.js
+++ src/components/MyObservations/InfiniteScrollLoadingWheel.js
@@ -12,14 +12,14 @@ type Props = {
 
 const InfiniteScrollLoadingWheel = ( { isLoading, currentUser }: Props ): Node => {
   const className = "h-64 border-t border-border py-16";
-  if ( isLoading && currentUser ) {
-    return (
-      <View className={className}>
-        <ActivityIndicator />
-      </View>
-    );
+  if ( !isLoading || !currentUser ) {
+    return <View className="h-64 py-16" />;
   }
-  return <View className="h-64 py-16" />;
+  return (
+    <View className={className}>
+      <ActivityIndicator />
+    </View>
+  );
 };
 
 export default InfiniteScrollLoadingWheel;
