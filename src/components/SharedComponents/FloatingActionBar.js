6006e485966620253c984f9f3f85e824ac982e49
Chris Chuck <chrischuckgithub@gmail.com>
2023-03-12 09:40:59 -0500

    Fix shadow on fab android

--- src/components/SharedComponents/FloatingActionBar.js
+++ src/components/SharedComponents/FloatingActionBar.js
@@ -7,14 +7,15 @@ import { Animated, Keyboard } from "react-native";
 import { useTheme } from "react-native-paper";
 import { getShadowStyle } from "styles/global";
 
-const getShadow = shadowColor => getShadowStyle( {
-  shadowColor,
-  offsetWidth: 0,
-  offsetHeight: 4,
-  shadowOpacity: 0.4,
-  shadowRadius: 4,
-  elevation: 5
-} );
+const getShadow = (shadowColor) =>
+  getShadowStyle({
+    shadowColor,
+    offsetWidth: 0,
+    offsetHeight: 4,
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 5,
+  });
 
 type Props = {
   position: "topStart" | "topEnd" | "bottomStart" | "bottomEnd",
@@ -26,87 +27,86 @@ type Props = {
 
 // Ensure this component is placed outside of scroll views
 
-const FloatingActionBar = ( {
+const FloatingActionBar = ({
   position = "bottomEnd",
   endY = null,
   containerClass,
   children,
-  show
-}: Props ): React.Node => {
+  show,
+}: Props): React.Node => {
   const theme = useTheme();
-  const [keyboardHeight, setKeyboardHeight] = useState( 0 );
-  const [keyboardOpen, setKeyboardOpen] = useState( false );
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+  const [keyboardOpen, setKeyboardOpen] = useState(false);
   const isBottom = position === "bottomEnd" || position === "bottomStart";
   const start = isBottom ? 100 : -100;
-  const animateIn = useMemo( () => new Animated.Value( start ), [start] );
-  const animateOut = useMemo( () => new Animated.Value( 0 ), [] );
+  const animate = useMemo(
+    () => new Animated.Value(show ? start : 0),
+    [start, show]
+  );
 
-  useEffect( () => {
+  useEffect(() => {
     const showSubscription = Keyboard.addListener(
       "keyboardDidShow",
-      event => {
-        setKeyboardHeight( event.endCoordinates.height );
-        setKeyboardOpen( true );
+      (event) => {
+        setKeyboardHeight(event.endCoordinates.height);
+        setKeyboardOpen(true);
       }
     );
-    const hideSubscription = Keyboard.addListener( "keyboardDidHide", () => {
-      setKeyboardOpen( false );
-    } );
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardOpen(false);
+    });
 
     return () => {
       showSubscription.remove();
       hideSubscription.remove();
     };
-  }, [] );
+  }, []);
 
-  useEffect( () => {
+  useEffect(() => {
     const sharedParams = {
       velocity: 1,
       tension: 2,
       friction: 8,
-      useNativeDriver: true
+      useNativeDriver: true,
     };
 
-    if ( show ) {
-      Animated.spring( animateIn, {
-        ...sharedParams,
-        toValue: 0
-      } ).start();
-    } else {
-      Animated.spring( animateOut, {
-        ...sharedParams,
-        toValue: start
-      } ).start();
-    }
-  }, [
-    keyboardOpen,
-    keyboardHeight,
-    position,
-    show,
-    animateIn,
-    animateOut,
-    start
-  ] );
+    const toValue = show ? 0 : start;
+
+    Animated.spring(animate, {
+      ...sharedParams,
+      toValue,
+    }).start();
+  }, [keyboardOpen, keyboardHeight, position, show, animate, start]);
 
   const effectiveKeyboardHeight = keyboardOpen ? keyboardHeight : 0;
+
+  const positionStyle = {};
+
+  if (position.includes("bottom")) {
+    positionStyle.bottom = (endY ?? 0) + effectiveKeyboardHeight;
+  }
+
+  if (position.includes("End")) {
+    positionStyle.right = 0;
+  }
 
   return (
     <Animated.View
       /* eslint-disable-next-line react-native/no-inline-styles */
       style={{
+        overflow: "visible",
         position: "absolute",
         zIndex: 50,
-        transform: [{ translateY: show ? animateIn : animateOut }],
-        ...( position === "bottomEnd" || position === "bottomStart"
-          ? { bottom: ( endY ?? 0 ) + effectiveKeyboardHeight }
-          : {} ),
-        ...( position === "bottomEnd" || position === "topEnd"
-          ? { right: 0 }
-          : {} ),
-        ...getShadow( theme.colors.primary )
+        transform: [{ translateY: animate }],
+        ...positionStyle,
       }}
     >
-      <View className={classNames( "bg-white", containerClass )}>{children}</View>
+      <View
+        className={classNames("bg-white", containerClass)}
+        style={getShadow(theme.colors.primary)}
+      >
+        {children}
+      </View>
     </Animated.View>
   );
 };
