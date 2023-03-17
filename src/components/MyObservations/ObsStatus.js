1bb0957313724aafe205241ba7892922160b5940
Johannes Klein <johannes.t.klein@gmail.com>
2023-03-17 10:12:57 +0100

    Remove components from ObsStatus

--- src/components/MyObservations/ObsStatus.js
+++ src/components/MyObservations/ObsStatus.js
@@ -2,14 +2,11 @@
 
 import classNames from "classnames";
 import checkCamelAndSnakeCase from "components/ObsDetails/helpers/checkCamelAndSnakeCase";
-import {
-  CommentsCount,
-  IdentificationsCount,
-  QualityGradeStatus
-} from "components/SharedComponents";
+import { ActivityCount, QualityGradeStatus } from "components/SharedComponents";
 import { View } from "components/styledComponents";
 import type { Node } from "react";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { useTheme } from "react-native-paper";
 import Observation from "realmModels/Observation";
 
@@ -26,6 +23,7 @@ const ObsStatus = ( {
   layout = "vertical",
   classNameMargin
 }: Props ): Node => {
+  const { t } = useTranslation();
   const theme = useTheme();
   const qualityGrade = checkCamelAndSnakeCase( observation, "qualityGrade" );
   const margin = layout === "vertical" ? "mb-1" : "mr-1";
@@ -34,19 +32,34 @@ const ObsStatus = ( {
   const numIdents = observation.identifications?.length || 0;
   const numComments = observation.comments?.length || 0;
 
+  // 03072023 amanda - applying chris' bandaid fix from PR #515: https://github.com/inaturalist/iNaturalistReactNative/pull/515
+  // to make sure android devices don't crash on start
+  let identificationA11yLabel = "";
+  let commentA11yLabel = "";
+  try {
+    // not exactly sure why this causes a consistent error every time you run android
+    // for the first time...
+    identificationA11yLabel = t( "x-identifications", { count: numIdents } );
+    commentA11yLabel = t( "x-comments", { count: numComments } );
+  } catch ( e ) {
+    console.warn( e );
+  }
+
   return (
     <View className={classNames( "flex", flexDirection, classNameMargin )}>
-      <IdentificationsCount
-        classNameMargin={margin}
+      <ActivityCount
+        icon="label"
+        margin={margin}
         count={numIdents}
-        white={white}
-        testID="ObsStatus.identificationsCount"
+        color={iconColor}
+        accessibilityLabel={identificationA11yLabel}
       />
-      <CommentsCount
-        classNameMargin={margin}
+      <ActivityCount
+        icon="comments"
+        margin={margin}
         count={numComments}
-        white={white}
-        testID="ObsStatus.commentsCount"
+        color={iconColor}
+        accessibilityLabel={commentA11yLabel}
       />
       <QualityGradeStatus qualityGrade={qualityGrade} color={iconColor} />
     </View>
