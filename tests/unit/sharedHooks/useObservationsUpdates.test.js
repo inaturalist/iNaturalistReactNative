97dac8efcef6f15c98573756e6f1c3f51787eabf
Johannes Klein <johannes.t.klein@gmail.com>
2023-04-18 11:07:55 +0200

    Unit tests for different paths for updating the realm observation when using the hook

--- tests/unit/sharedHooks/useObservationsUpdates.test.js
+++ tests/unit/sharedHooks/useObservationsUpdates.test.js
@@ -1,5 +1,4 @@
 import { renderHook } from "@testing-library/react-native";
-import { fetchObservationUpdates } from "api/observations";
 import useAuthenticatedQuery from "sharedHooks/useAuthenticatedQuery";
 import useObservationsUpdates from "sharedHooks/useObservationsUpdates";
 
@@ -18,14 +17,7 @@ const mockIdentificationUpdate = factory( "RemoteUpdate", {
 } );
 const mockData = [mockCommentUpdate, mockIdentificationUpdate];
 
-const mockObservation = factory( "LocalObservation", {
-  viewed_comments: false,
-  viewed_identifications: false
-} );
-const mockObservation2 = factory( "LocalObservation", {
-  viewed_comments: true,
-  viewed_identifications: true
-} );
+const mockObservation = factory( "LocalObservation" );
 const mockRealm = {
   objectForPrimaryKey: jest.fn( ).mockReturnValue( mockObservation ),
   write: jest.fn( )
@@ -45,7 +37,11 @@ jest.mock( "providers/contexts", ( ) => {
 describe( "useObservationsUpdates", ( ) => {
   beforeEach( ( ) => {
     useAuthenticatedQuery.mockReturnValue( { data: mockData, isLoading: false } );
-    fetchObservationUpdates.mockReturnValue( { } );
+  } );
+
+  afterEach( () => {
+    // Clear mocks after each test to handle different cases of realm.write being called or not
+    jest.clearAllMocks();
   } );
 
   it( "should return an object", ( ) => {
@@ -53,7 +49,7 @@ describe( "useObservationsUpdates", ( ) => {
     expect( result.current ).toEqual( { isLoading: false } );
   } );
 
-  it( "should call useAuthenticatedQuery not enabled without param given", ( ) => {
+  it( "should call disabled useAuthenticatedQuery without current user param", ( ) => {
     renderHook( ( ) => useObservationsUpdates( undefined ) );
     expect( useAuthenticatedQuery ).toHaveBeenCalledWith(
       ["fetchObservationUpdates"],
@@ -62,7 +58,7 @@ describe( "useObservationsUpdates", ( ) => {
     );
   } );
 
-  it( "should call useAuthenticatedQuery enabled with param given", ( ) => {
+  it( "should call enabled useAuthenticatedQuery with current user param", ( ) => {
     renderHook( ( ) => useObservationsUpdates( mockUser ) );
     expect( useAuthenticatedQuery ).toHaveBeenCalledWith(
       ["fetchObservationUpdates"],
@@ -71,25 +67,47 @@ describe( "useObservationsUpdates", ( ) => {
     );
   } );
 
-  describe( "when the local observation is not viewed", ( ) => {
-    it( "should not call realm.write", () => {
-      renderHook( () => useObservationsUpdates( mockUser ) );
-      expect( mockRealm.write ).not.toHaveBeenCalled();
-    } );
-  } );
-
-  describe( "when the local observation is viewed", ( ) => {
+  describe.each( [
+    ["comment", mockCommentUpdate],
+    ["identification", mockIdentificationUpdate]
+  ] )( "when the update is a %s", ( a, update ) => {
     beforeEach( ( ) => {
-      mockRealm.objectForPrimaryKey.mockReturnValue( mockObservation2 );
+      useAuthenticatedQuery.mockReturnValue( { data: [update], isLoading: false } );
     } );
 
-    it( "should call realm.write", ( ) => {
-      renderHook( ( ) => useObservationsUpdates( mockUser ) );
-      expect( mockRealm.write ).toHaveBeenCalled();
-    } );
+    describe.each( [
+      ["viewed comments and viewed identifications", true, true],
+      ["viewed comments and not viewed identifications", true, false],
+      ["not viewed comments and viewed identifications", false, true],
+      ["not viewed comments and not viewed identifications", false, false]
+    ] )( "when the local observation has %s", ( a1, viewedComments, viewedIdentifications ) => {
+      beforeEach( ( ) => {
+        mockRealm.objectForPrimaryKey.mockReturnValue( {
+          ...mockObservation,
+          viewed_comments: viewedComments,
+          viewed_identifications: viewedIdentifications
+        } );
+      } );
 
-    afterEach( ( ) => {
-      mockRealm.objectForPrimaryKey.mockReturnValue( mockObservation );
+      // Testing if realm.write is correctly called or not,
+      // this is not testing the actual change in the local observation
+      // Depending on whether the update is a comment or an identification
+      // and whether the local observation has viewed comments or identifications
+      // the realm.write function should be called or not
+      const shouldCallRealmWrite = ( ( viewedComments && a === "comment" )
+          || ( viewedIdentifications && a === "identification" ) );
+      it( "should call realm.write", ( ) => {
+        renderHook( ( ) => useObservationsUpdates( mockUser ) );
+        if ( shouldCallRealmWrite ) {
+          expect( mockRealm.write ).toHaveBeenCalled();
+        }
+      } );
+      it( "should not call realm.write ", ( ) => {
+        renderHook( ( ) => useObservationsUpdates( mockUser ) );
+        if ( !shouldCallRealmWrite ) {
+          expect( mockRealm.write ).not.toHaveBeenCalled();
+        }
+      } );
     } );
   } );
 } );
