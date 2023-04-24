d1966282f51c2a1774571f8b999fe628f63435ea
Amanda Bullington <albullington@gmail.com>
2023-04-24 12:21:07 -0700

    Use debouncing to avoid map jitter when typing in location

--- src/components/LocationPicker/LocationSearch.js
+++ src/components/LocationPicker/LocationSearch.js
@@ -6,6 +6,7 @@ import {
 import type { Node } from "react";
 import React from "react";
 import fetchCoordinates from "sharedHelpers/fetchCoordinates";
+import { useDebouncedCallback } from "use-debounce";
 
 type Props = {
   region: Object,
@@ -15,7 +16,7 @@ type Props = {
 
 const LocationSearch = ( { region, setRegion, locationName }: Props ): Node => (
   <SearchBar
-    handleTextChange={async locationText => {
+    handleTextChange={useDebouncedCallback( async locationText => {
       const newCoords = await fetchCoordinates( locationText );
       if ( !newCoords ) { return; }
       setRegion( {
@@ -23,7 +24,7 @@ const LocationSearch = ( { region, setRegion, locationName }: Props ): Node => (
         latitude: newCoords.latitude,
         longitude: newCoords.longitude
       } );
-    }}
+    }, 500 )}
     value={locationName}
     testID="LocationPicker.locationSearch"
     containerClass="absolute top-[20px] right-[26px] left-[26px]"
