808d7f0178571796b20a26b00d96e65c53f28cf7
Johannes Klein <johannes.t.klein@gmail.com>
2023-04-24 23:48:58 +0200

    Updates to files because of upgrade

--- ios/iNaturalistReactNative/AppDelegate.mm
+++ ios/iNaturalistReactNative/AppDelegate.mm
@@ -7,10 +7,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"iNaturalistReactNative";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
-
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
