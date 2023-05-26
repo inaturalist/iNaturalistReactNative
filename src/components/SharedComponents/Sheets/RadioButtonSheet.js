5d083d556da4c89a8bfa4fe637656c469650d104
Amanda Bullington <albullington@gmail.com>
2023-05-25 18:22:04 -0700

    Update radio buttons to make text pressable

--- src/components/SharedComponents/Sheets/RadioButtonSheet.js
+++ src/components/SharedComponents/Sheets/RadioButtonSheet.js
@@ -1,12 +1,13 @@
 // @flow
 
 import {
-  Body1, BottomSheet, Button, List2
+  BottomSheet, Button, List2
 } from "components/SharedComponents";
 import { View } from "components/styledComponents";
 import type { Node } from "react";
 import React, { useState } from "react";
-import { RadioButton } from "react-native-paper";
+import { Platform } from "react-native";
+import { RadioButton, useTheme } from "react-native-paper";
 import useTranslation from "sharedHooks/useTranslation";
 
 type Props = {
@@ -24,27 +25,37 @@ const RadioButtonSheet = ( {
   headerText,
   radioValues
 }: Props ): Node => {
+  const theme = useTheme( );
   const { t } = useTranslation( );
   const [checked, setChecked] = useState( "none" );
 
   const radioButtonRow = radioRow => (
-    <View className="flex-row mb-4" key={radioRow}>
-      {/* adding a View here makes the onPress ripple surround only
-      the button and not use extra padding */}
-      <View>
-        <RadioButton.Android
-          value={radioValues[radioRow]}
-          status={( checked === radioValues[radioRow].value )
-            ? "checked"
-            : "unchecked"}
-          onPress={( ) => setChecked( radioValues[radioRow].value )}
-        />
-      </View>
-      <View className="ml-1 mt-2 flex-1">
-        <Body1>{radioValues[radioRow].label}</Body1>
-        <List2 className="flex-wrap">{radioValues[radioRow].text}</List2>
-      </View>
-    </View>
+    <RadioButton.Group>
+      <RadioButton.Item
+        value={radioValues[radioRow]}
+        status={( checked === radioValues[radioRow].value )
+          ? "checked"
+          : "unchecked"}
+        onPress={( ) => setChecked( radioValues[radioRow].value )}
+        mode="android"
+        label={radioValues[radioRow].label}
+        position="leading"
+        // eslint-disable-next-line react-native/no-inline-styles
+        labelStyle={{
+          fontSize: 18,
+          lineHeight: 22,
+          fontFamily: `Whitney-Light${Platform.OS === "ios"
+            ? ""
+            : "-Pro"}`,
+          color: theme.colors.primary,
+          fontWeight: "500",
+          textAlign: "left",
+          textAlignVertical: "center"
+        }}
+        className="p-0"
+      />
+      <List2 className="ml-[37px] mr-[33px] py-1">{radioValues[radioRow].text}</List2>
+    </RadioButton.Group>
   );
 
   return (
@@ -58,12 +69,13 @@ const RadioButtonSheet = ( {
         }
       }}
     >
-      <View className="p-6">
+      <View className="px-4 pt-[20px]">
         {Object.keys( radioValues ).map( radioRow => radioButtonRow( radioRow ) )}
         <Button
           level="primary"
           text={t( "CONFIRM" )}
           onPress={( ) => confirm( checked )}
+          className="mt-[15px]"
         />
       </View>
     </BottomSheet>
