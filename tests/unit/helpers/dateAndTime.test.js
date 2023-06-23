fa30355e070e8e6b8ccd6ecedafca5d8e0729952
Amanda Bullington <albullington@gmail.com>
2023-06-23 11:08:43 -0700

    Make minimum viable RemoteComment

--- tests/unit/helpers/dateAndTime.test.js
+++ tests/unit/helpers/dateAndTime.test.js
@@ -5,7 +5,10 @@ import { formatApiDatetime } from "sharedHelpers/dateAndTime";
 
 const remoteObservation = factory( "RemoteObservation" );
 const remoteIdentification = factory( "RemoteIdentification" );
-const remoteComment = factory( "RemoteComment" );
+const remoteComment = factory( "RemoteComment", {
+  created_at: "2015-02-13T05:15:38+00:00",
+  updated_at: "2015-02-12T20:41:10-08:00"
+} );
 
 describe( "formatApiDatetime", ( ) => {
   describe( "in default locale", ( ) => {
