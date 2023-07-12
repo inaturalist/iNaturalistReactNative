42b02ae2996c284a670856a1bc8c3f585055e1d3
Johannes Klein <johannes.t.klein@gmail.com>
2023-07-12 22:42:59 +0200

    Copy over example model file if not included

--- src/sharedHelpers/cvModel.js
+++ src/sharedHelpers/cvModel.js
@@ -36,13 +36,19 @@ const addCameraFilesAndroid = () => {
   RNFS.readDirAssets( "camera" ).then( results => {
     const model = modelFiles.ANDROIDMODEL;
     const taxonomy = modelFiles.ANDROIDTAXONOMY;
+    const sampleModel = "small_inception_tf1.tflite";
+    const sampleTaxonomy = "small_export_tax.csv";
 
     const hasModel = results.find( r => r.name === model );
+    const hasSampleModel = results.find( r => r.name === sampleModel );
 
     // Android writes over existing files
     if ( hasModel !== undefined ) {
       copyFilesAndroid( `camera/${model}`, dirModel );
       copyFilesAndroid( `camera/${taxonomy}`, dirTaxonomy );
+    } else if ( hasSampleModel !== undefined ) {
+      copyFilesAndroid( `camera/${sampleModel}`, dirModel );
+      copyFilesAndroid( `camera/${sampleTaxonomy}`, dirTaxonomy );
     }
   } );
 };
