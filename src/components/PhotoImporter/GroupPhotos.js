642f46679e8f5fcfcfaaec57c46812b38b6caa30
Johannes Klein <johannes.t.klein@gmail.com>
2023-09-14 13:17:57 +0200

    Calculate number of columns and item width based on screen width

--- src/components/PhotoImporter/GroupPhotos.js
+++ src/components/PhotoImporter/GroupPhotos.js
@@ -10,6 +10,8 @@ import { t } from "i18next";
 import type { Node } from "react";
 import React from "react";
 import { useTheme } from "react-native-paper";
+import { BREAKPOINTS } from "sharedHelpers/breakpoint";
+import { useDeviceOrientation } from "sharedHooks";
 
 import GroupPhotoImage from "./GroupPhotoImage";
 
@@ -33,23 +35,61 @@ const GroupPhotos = ( {
   separatePhotos
 }: Props ): Node => {
   const theme = useTheme();
-  const renderImage = ( { item } ) => (
-    <GroupPhotoImage
-      item={item}
-      selectedObservations={selectedObservations}
-      selectObservationPhotos={selectObservationPhotos}
-    />
-  );
-
+  const {
+    isLandscapeMode,
+    isTablet,
+    screenWidth,
+    screenHeight
+  } = useDeviceOrientation();
   const extractKey = ( item, index ) => `${item.photos[0].uri}${index}`;
 
   const noObsSelected = selectedObservations.length === 0;
   const oneObsSelected = selectedObservations.length === 1;
   const obsWithMultiplePhotosSelected = selectedObservations?.[0]?.photos?.length > 1;
 
+  const calculateNumColumns = () => {
+    if ( screenWidth <= BREAKPOINTS.sm ) {
+      return 1;
+    }
+    if ( !isTablet ) return 2;
+    if ( isLandscapeMode ) return 6;
+    if ( screenWidth <= BREAKPOINTS.xl ) return 2;
+    return 4;
+  };
+  const numColumns = calculateNumColumns();
+
+  const GUTTER = 15;
+  const calculateGridItemWidth = () => {
+    const combinedGutter = ( numColumns + 1 ) * GUTTER;
+    const gridWidth = isTablet
+      ? screenWidth
+      : Math.min( screenWidth, screenHeight );
+    return Math.floor( ( gridWidth - combinedGutter ) / numColumns );
+  };
+  const itemWidth = calculateGridItemWidth();
+
+  const itemStyle = {
+    height: itemWidth,
+    width: itemWidth,
+    margin: GUTTER / 2
+  };
+
+  const renderImage = ( { item } ) => (
+    <GroupPhotoImage
+      item={item}
+      selectedObservations={selectedObservations}
+      selectObservationPhotos={selectObservationPhotos}
+      style={itemStyle}
+    />
+  );
+
   return (
     <ViewWrapper>
       <FlashList
+        contentContainerStyle={{
+          paddingLeft: GUTTER / 2,
+          paddingRight: GUTTER / 2
+        }}
         ListHeaderComponent={(
           <View className="m-5">
             <Body2>{t( "Group-photos-onboarding" )}</Body2>
@@ -58,11 +98,15 @@ const GroupPhotos = ( {
         data={groupedPhotos}
         initialNumToRender={4}
         keyExtractor={extractKey}
-        numColumns={2}
+        numColumns={numColumns}
+        key={numColumns}
         renderItem={renderImage}
         testID="GroupPhotos.list"
-        estimatedItemSize={178}
-        extraData={selectedObservations}
+        extraData={{
+          selectedObservations,
+          itemWidth
+        }}
+        estimatedItemSize={itemWidth + GUTTER}
       />
       <FloatingActionBar
         show={selectedObservations.length > 0}
