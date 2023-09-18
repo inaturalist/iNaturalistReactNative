d2f063af06ae8d82af1b9dae23d9531dff7e1000
Amanda Bullington <albullington@gmail.com>
2023-09-18 15:05:15 -0700

    Handle app links in MainActivity.java and update intent s

--- android/app/src/main/java/com/inaturalistreactnative/MainActivity.java
+++ android/app/src/main/java/com/inaturalistreactnative/MainActivity.java
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
@@ -14,6 +17,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
+    // ATTENTION: This was auto-generated to handle app links.
+    Intent appLinkIntent = getIntent();
+    String appLinkAction = appLinkIntent.getAction();
+    Uri appLinkData = appLinkIntent.getData();
   }
 
   /**
