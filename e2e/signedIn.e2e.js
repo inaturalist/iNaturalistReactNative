1cf8832941d74717c17e7508c0bcb111816ed8f3
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-10-04 12:09:37 -0700

    Couple changes to e2e testing
    * Removed sign in test since the obs creation test already signs in
    * Renamed two remaining test files to signedIn and signedOut to reflect the
      different conditions that they test
    * Tried running with 2 workers in CI

--- e2e/signedIn.e2e.js
+++ e2e/signedIn.e2e.js
@@ -3,26 +3,11 @@ import {
 } from "detox";
 import Config from "react-native-config-node";
 
-describe( "Sign in and create an observation", () => {
-  beforeAll( async () => {
-    await device.launchApp( {
-      newInstance: true,
-      permissions: { location: "always" }
-    } );
-  } );
+import { iNatE2eBeforeAll, iNatE2eBeforeEach } from "./helpers";
 
-  beforeEach( async () => {
-    // device.launchApp would be preferred for an app of our complexity. It does work locally
-    // for both, but on CI for Android it does not work. So we use reloadReactNative for Android.
-    if ( device.getPlatform() === "android" ) {
-      await device.reloadReactNative();
-    } else {
-      await device.launchApp( {
-        newInstance: true,
-        permissions: { location: "always" }
-      } );
-    }
-  } );
+describe( "Signed in user", () => {
+  beforeAll( async ( ) => iNatE2eBeforeAll( device ) );
+  beforeEach( async ( ) => iNatE2eBeforeEach( device ) );
 
   it( "should create an observation, add a comment, and delete the observation", async () => {
     /*
