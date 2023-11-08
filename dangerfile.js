b52e3e5a2e1d74fb147ee32d0422ce1c4ad2fa1e
Amanda Bullington <albullington@gmail.com>
2023-11-07 16:43:55 -0800

    Comment out performance test CI

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line
 import { danger, message } from "danger";
 
 const modifiedMD = danger.git.modified_files.join( "- " );
