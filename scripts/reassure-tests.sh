5d45b21570325a4bc80464c8b734317e5437a993
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2023-11-09 09:50:22 -0800

    Performance testing with Reassure (#867)
    * Performance test with reassure, danger/CI integration set up
    
    * Remove log
    
    * Fix script location in github actions test
    
    * Run performance tests before regular tests
    
    * Move script
    
    * Make script findable and executable
    
    * Find scripts directory
    
    * Find scripts directory
    
    * Find scripts directory
    
    * Give permission
    
    * Comment out performance test CI
    
    * Remove danger/CI and update naming
    
    * Remove danger from package.json

---
+++ scripts/reassure-tests.sh
@@ -0,0 +1,16 @@
+set -e
+
+BASELINE_BRANCH=main
+
+# Required for `git switch` on CI
+git fetch origin
+
+# Gather baseline perf measurements
+git switch "$BASELINE_BRANCH"
+npm install --force
+npx reassure --baseline
+
+# Gather current perf measurements & compare results
+git switch --detach -
+npm install --force
+npm run reassure
