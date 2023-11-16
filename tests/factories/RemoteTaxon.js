f6592c768d5581799a8e375886a12b0b99fd1f79
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-11-15 17:45:00 -0800

    Added Explore test to look for correct status

--- tests/factories/RemoteTaxon.js
+++ tests/factories/RemoteTaxon.js
@@ -3,4 +3,9 @@ import { define } from "factoria";
 export default define( "RemoteTaxon", faker => ( {
   id: faker.datatype.number( ),
   name: faker.name.fullName( )
-} ) );
+} ), {
+  genus: faker => ( {
+    rank: "genus",
+    name: faker.name.firstName
+  } )
+} );
