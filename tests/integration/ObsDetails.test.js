44ce914bd88392a2e2e0a5727f481e0e6b9fefa0
Amanda Bullington <albullington@gmail.com>
2023-11-16 12:24:38 -0800

    Use waitFor to fix ObsDetails integration test

--- tests/integration/ObsDetails.test.js
+++ tests/integration/ObsDetails.test.js
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { screen } from "@testing-library/react-native";
+import { screen, waitFor } from "@testing-library/react-native";
 import ObsDetailsContainer from "components/ObsDetails/ObsDetailsContainer";
 import initI18next from "i18n/initI18next";
 import inatjs from "inaturalistjs";
@@ -74,7 +74,9 @@ describe( "ObsDetails", () => {
       ).toBeTruthy();
       expect( inatjs.observations.viewedUpdates ).toHaveBeenCalledTimes( 1 );
       // Expect the observation in realm to have been updated with comments_viewed = true
-      expect( observation.comments_viewed ).toBe( true );
+      await waitFor( ( ) => {
+        expect( observation.comments_viewed ).toBe( true );
+      } );
     } );
   } );
 } );
