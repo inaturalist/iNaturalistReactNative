1b415b6727fae420dbba2e702bf61e0590ec91f8
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-11-20 16:24:50 -0800

    Bugfix: iPad landscape wasn't showing all pics in StandardCamera delete mode

--- src/components/Camera/StandardCamera/PhotoCarousel.js
+++ src/components/Camera/StandardCamera/PhotoCarousel.js
@@ -233,12 +233,13 @@ const PhotoCarousel = ( {
   // the measure() method to store the position of the container element in
   // state, and use that to position another container inside the modal in
   // exactly the same place
+
   const containerRef = useRef( );
   const [containerPos, setContainerPos] = useState( { x: null, y: null } );
   const containerStyle = {
     height: isTablet && isLandscapeMode
-      ? containerPos.h
-      : photoDim + photoGutter * 2,
+      ? photoUris.length * ( photoDim + photoGutter ) + photoGutter
+      : photoDim + ( photoGutter * 2 ),
     padding: photoGutter / 2
   };
 
@@ -274,8 +275,7 @@ const PhotoCarousel = ( {
               backdropOpacity={0}
               // We want this to take over the whole screen
               // eslint-disable-next-line react-native/no-inline-styles
-              style={{ margin: 0 }}
-              statusBarTranslucent
+              style={{ margin: 0, position: "relative" }}
             >
               <View
                 // These layout values need to be dynamic relative to the
