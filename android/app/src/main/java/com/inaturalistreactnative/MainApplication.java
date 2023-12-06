00b3bb5d110cfb5794658fa8b8505e1750d53bc5
Amanda Bullington <albullington@gmail.com>
2023-12-06 15:37:49 -0800

    Add Android implementation for CodePush

--- android/app/src/main/java/com/inaturalistreactnative/MainApplication.java
+++ android/app/src/main/java/com/inaturalistreactnative/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -41,6 +42,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected Boolean isHermesEnabled() {
           return BuildConfig.IS_HERMES_ENABLED;
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+          return CodePush.getJSBundleFile();
         }
       };
 
