fc420059bef4bd9775ef623c28f8d4490755d90f
Amanda Bullington <albullington@gmail.com>
2023-12-06 21:14:52 -0800

    Set code push env variable directly in build.gradle, not strings.xml

--- src/components/SharedComponents/Buttons/Button.js
+++ src/components/SharedComponents/Buttons/Button.js
@@ -73,7 +73,7 @@ const setStyles = ( {
     } else {
       buttonClasses.push( disabled
         ? "bg-inatGreenDisabled"
-        : "bg-inatGreen" );
+        : "bg-red" );
     }
   } else {
     buttonClasses.push( "border border-[3px]" );
