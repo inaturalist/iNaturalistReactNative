740ba794d17d439ef80ca3fc3d930c85c1faffda
Amanda Bullington <albullington@gmail.com>
2023-12-06 19:51:46 -0800

    Change button back to normal inatGreen

--- src/components/SharedComponents/Buttons/Button.js
+++ src/components/SharedComponents/Buttons/Button.js
@@ -73,7 +73,7 @@ const setStyles = ( {
     } else {
       buttonClasses.push( disabled
         ? "bg-inatGreenDisabled"
-        : "bg-blue" );
+        : "bg-inatGreen" );
     }
   } else {
     buttonClasses.push( "border border-[3px]" );
