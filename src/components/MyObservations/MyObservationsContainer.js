4b316f84c3c8f91e5c0083e0e841de2c064057d2
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-12-08 16:36:15 -0800

    Nocked auth token requests in an attempt to fix tests

--- src/components/MyObservations/MyObservationsContainer.js
+++ src/components/MyObservations/MyObservationsContainer.js
@@ -125,7 +125,10 @@ const MyObservationsContainer = ( ): Node => {
   const apiToken = useApiToken( );
   const [state, dispatch] = useReducer( uploadReducer, INITIAL_UPLOAD_STATE );
   const { observationList: observations, allObsToUpload } = useLocalObservations( );
-  const { getItem, setItem } = useAsyncStorage( "myObservationsLayout" );
+  const {
+    getItem: getStoredLayout,
+    setItem: setStoredLayout
+  } = useAsyncStorage( "myObservationsLayout" );
   const [layout, setLayout] = useState( null );
   const isOnline = useIsConnected( );
 
@@ -155,28 +158,25 @@ const MyObservationsContainer = ( ): Node => {
 
   const [showLoginSheet, setShowLoginSheet] = useState( false );
 
-  const writeItemToStorage = useCallback( async newValue => {
-    await setItem( newValue );
+  const writeLayoutToStorage = useCallback( async newValue => {
+    await setStoredLayout( newValue );
     setLayout( newValue );
-  }, [setItem] );
+  }, [setStoredLayout] );
 
   useEffect( ( ) => {
-    const readItemFromStorage = async ( ) => {
-      const item = await getItem( );
-      if ( !item ) {
-        await writeItemToStorage( "list" );
-      }
-      setLayout( item || "list" );
+    const readLayoutFromStorage = async ( ) => {
+      const storedLayout = await getStoredLayout( );
+      setLayout( storedLayout || "list" );
     };
 
-    readItemFromStorage( );
-  }, [getItem, writeItemToStorage] );
+    readLayoutFromStorage( );
+  }, [getStoredLayout, writeLayoutToStorage] );
 
   const toggleLayout = ( ) => {
     if ( layout === "grid" ) {
-      writeItemToStorage( "list" );
+      writeLayoutToStorage( "list" );
     } else {
-      writeItemToStorage( "grid" );
+      writeLayoutToStorage( "grid" );
     }
   };
 
