9d83e34e08efedac2e91173974c4396167ff1de3
Amanda Bullington <albullington@gmail.com>
2023-12-13 12:07:24 -0800

    Add tests to Checkbox component

--- src/components/SharedComponents/Checkbox.js
+++ src/components/SharedComponents/Checkbox.js
@@ -6,6 +6,7 @@ import React, { useMemo, useState } from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { useTheme } from "react-native-paper";
+import { useTranslation } from "sharedHooks";
 
 type Props = {
   text: string
@@ -13,9 +14,14 @@ type Props = {
 
 const Checkbox = ( { text }: Props ): Node => {
   const theme = useTheme( );
+  const { t } = useTranslation( );
   const [isChecked, setIsChecked] = useState( false );
 
-  const renderCheckboxText = useMemo( ( ) => <Body2 className="ml-3">{text}</Body2>, [text] );
+  const renderCheckboxText = useMemo( ( ) => {
+    if ( !text ) { return null; }
+    return <Body2 className="ml-3">{text}</Body2>;
+  }, [text] );
+
   const renderIcon = useMemo( ( ) => (
     <INatIcon
       name="checkmark"
@@ -41,6 +47,8 @@ const Checkbox = ( { text }: Props ): Node => {
       onPress={( ) => setIsChecked( !isChecked )}
       textComponent={renderCheckboxText}
       innerIconStyle={innerIconStyle}
+      accessibilityRole="radio"
+      accessibilityLabel={t( "Checkmark" )}
     />
   );
 };
