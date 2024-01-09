759857fe45a9eb5f5d1f34b09cdcf065cab7c7dc
Amanda Bullington <albullington@gmail.com>
2024-01-08 16:19:33 -0800

    Only show current ids in IdentificationCount; closes #995

---
+++ tests/unit/components/SharedComponents/ObservationsFlashList/ObsStatus.test.js
@@ -0,0 +1,50 @@
+import {
+  screen
+} from "@testing-library/react-native";
+import ObsStatus from "components/SharedComponents/ObservationsFlashList/ObsStatus";
+import initI18next from "i18n/initI18next";
+import React from "react";
+import factory from "tests/factory";
+import { renderComponent } from "tests/helpers/render";
+
+const mockObservation = factory( "LocalObservation", {
+  identifications: [
+    factory( "LocalIdentification", {
+      current: true
+    } ),
+    factory( "LocalIdentification", {
+      current: false
+    } )
+  ],
+  comments: []
+} );
+
+describe( "ObsStatus", () => {
+  beforeAll( async () => {
+    await initI18next();
+  } );
+
+  it( "displays count for current ids, not withdrawn ids", () => {
+    renderComponent(
+      <ObsStatus
+        observation={mockObservation}
+      />
+    );
+
+    const idCount = screen.getByText( /1/ );
+
+    expect( idCount ).toBeVisible( );
+  } );
+
+  it( "displays comment count", () => {
+    renderComponent(
+      <ObsStatus
+        observation={mockObservation}
+      />
+    );
+
+    const commentCount = screen.getByText( /0/ );
+
+    expect( commentCount ).toBeVisible( );
+  } );
+} );
