840b2a82d98ba9b94af3181c7f6e981176f5be43
Amanda Bullington <albullington@gmail.com>
2024-01-09 16:13:09 -0800

    Make entire dropdown for notes tappable; closes #860

--- src/components/ObsEdit/OtherDataSection.js
+++ src/components/ObsEdit/OtherDataSection.js
@@ -1,13 +1,14 @@
 // @flow
 
 import {
-  Body3, Heading4, INatIcon, TextInputSheet
+  Heading4, TextInputSheet
 } from "components/SharedComponents";
-import { Pressable, View } from "components/styledComponents";
+import { View } from "components/styledComponents";
 import type { Node } from "react";
 import React, { useState } from "react";
 import useTranslation from "sharedHooks/useTranslation";
 
+import DropdownItem from "./DropdownItem";
 import GeoprivacySheet from "./Sheets/GeoprivacySheet";
 import WildStatusSheet from "./Sheets/WildStatusSheet";
 
@@ -49,13 +50,6 @@ const OtherDataSection = ( {
       value: true
     }];
 
-  const updateGeoprivacyStatus = value => updateObservationKeys( {
-    geoprivacy: value
-  } );
-  const updateCaptiveStatus = value => updateObservationKeys( {
-    captive_flag: value
-  } );
-
   const currentGeoprivacyStatus = geoprivacyOptions
     .find( e => e.value === currentObservation?.geoprivacy );
   const currentCaptiveStatus = captiveOptions
@@ -67,83 +61,52 @@ const OtherDataSection = ( {
         <GeoprivacySheet
           selectedValue={currentObservation?.geoprivacy}
           handleClose={( ) => setShowGeoprivacySheet( false )}
-          updateGeoprivacyStatus={updateGeoprivacyStatus}
+          updateGeoprivacyStatus={value => updateObservationKeys( {
+            geoprivacy: value
+          } )}
         />
       )}
       {showWildStatusSheet && (
         <WildStatusSheet
           selectedValue={currentObservation?.captive_flag}
           handleClose={( ) => setShowWildStatusSheet( false )}
-          updateCaptiveStatus={updateCaptiveStatus}
+          updateCaptiveStatus={value => updateObservationKeys( {
+            captive_flag: value
+          } )}
+        />
+      )}
+      {showNotesSheet && (
+        <TextInputSheet
+          handleClose={( ) => setShowNotesSheet( false )}
+          headerText={t( "NOTES" )}
+          placeholder={t( "Add-optional-notes" )}
+          initialInput={currentObservation?.description}
+          confirm={textInput => updateObservationKeys( {
+            description: textInput
+          } )}
         />
       )}
       <Heading4>{t( "OTHER-DATA" )}</Heading4>
-      <Pressable
-        className="flex-row flex-nowrap items-center ml-1 mt-5"
-        onPress={( ) => setShowGeoprivacySheet( true )}
-        accessibilityRole="button"
+      <DropdownItem
         accessibilityLabel={t( "Select-geoprivacy-status" )}
-      >
-        <INatIcon
-          name="globe-outline"
-          size={14}
-        />
-        <Body3 className="mx-3">
-          {t( "Geoprivacy" )}
-          {" "}
-          {currentGeoprivacyStatus?.label || geoprivacyOptions[0].label}
-        </Body3>
-        <INatIcon
-          name="caret"
-          size={10}
-        />
-      </Pressable>
-      <Pressable
-        className="flex-row flex-nowrap items-center ml-1 mt-5"
-        onPress={( ) => setShowWildStatusSheet( true )}
-        accessibilityRole="button"
+        handlePress={( ) => setShowGeoprivacySheet( true )}
+        iconName="globe-outline"
+        text={t( "Geoprivacy-status", {
+          status: currentGeoprivacyStatus?.label || geoprivacyOptions[0].label
+        } )}
+      />
+      <DropdownItem
         accessibilityLabel={t( "Select-captive-or-cultivated-status" )}
-      >
-        <INatIcon
-          name="pot-outline"
-          size={14}
-        />
-        <Body3 className="mx-3">
-          {currentCaptiveStatus?.label || captiveOptions[0].label}
-        </Body3>
-        <INatIcon
-          name="caret"
-          size={10}
-        />
-      </Pressable>
-      <View className="flex-row flex-nowrap items-center ml-1 mt-2.5">
-        {showNotesSheet && (
-          <TextInputSheet
-            handleClose={( ) => setShowNotesSheet( false )}
-            headerText={t( "NOTES" )}
-            placeholder={t( "Add-optional-notes" )}
-            initialInput={currentObservation?.description}
-            confirm={textInput => updateObservationKeys( {
-              description: textInput
-            } )}
-          />
-        )}
-        <INatIcon
-          size={14}
-          name="pencil-outline"
-        />
-        <Body3
-          onPress={( ) => setShowNotesSheet( true )}
-          className="mx-3 py-3"
-          accessibilityRole="link"
-        >
-          {currentObservation?.description || t( "Add-optional-notes" )}
-        </Body3>
-        <INatIcon
-          name="caret"
-          size={10}
-        />
-      </View>
+        handlePress={( ) => setShowWildStatusSheet( true )}
+        iconName="pot-outline"
+        text={currentCaptiveStatus?.label || captiveOptions[0].label}
+      />
+      <DropdownItem
+        accessibilityLabel={t( "Add-optional-notes" )}
+        handlePress={( ) => setShowNotesSheet( true )}
+        iconName="pencil-outline"
+        text={currentObservation?.description || t( "Add-optional-notes" )}
+      />
     </View>
   );
 };
