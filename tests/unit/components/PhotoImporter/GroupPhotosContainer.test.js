f1ca545de473cef52fb28fb5c0d0601043b8df95
Amanda Bullington <albullington@gmail.com>
2024-01-11 13:53:29 -0800

    Add test for separating photos

--- tests/unit/components/PhotoImporter/GroupPhotosContainer.test.js
+++ tests/unit/components/PhotoImporter/GroupPhotosContainer.test.js
@@ -98,4 +98,36 @@ describe( "GroupPhotosContainer", ( ) => {
 
     expect( firstPhotoCombinedPressable ).toHaveTextContent( /3/ );
   } );
+
+  it( "separates combined photos", async ( ) => {
+    useStore.setState( { groupedPhotos: mockGroupedPhotos } );
+    renderComponent( <GroupPhotosContainer /> );
+
+    const firstPhotoPressable = screen.getByTestId(
+      `GroupPhotos.${mockGroupedPhotos[0].photos[0].image.uri}`
+    );
+    fireEvent.press( firstPhotoPressable );
+    const secondPhotoPressable = screen.getByTestId(
+      `GroupPhotos.${mockGroupedPhotos[1].photos[0].image.uri}`
+    );
+    fireEvent.press( secondPhotoPressable );
+
+    const combinePhotosButton = screen.getByLabelText( /Combine Photos/ );
+    fireEvent.press( combinePhotosButton );
+
+    const { groupedPhotos } = useStore.getState( );
+
+    const firstPhotoCombinedPressable = screen.getByTestId(
+      `GroupPhotos.${groupedPhotos[0].photos[0].image.uri}`
+    );
+    const secondPhotoCombinedPressable = screen.getByTestId(
+      `GroupPhotos.${groupedPhotos[1].photos[0].image.uri}`
+    );
+    fireEvent.press( firstPhotoCombinedPressable );
+    fireEvent.press( secondPhotoCombinedPressable );
+    const separatePhotosButton = screen.getByLabelText( /Separate Photos/ );
+    fireEvent.press( separatePhotosButton );
+
+    expect( firstPhotoCombinedPressable ).not.toHaveTextContent( );
+  } );
 } );
