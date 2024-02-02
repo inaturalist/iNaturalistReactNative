46ebea487ba08dcf1d7f4687af738bb552858154
Amanda Bullington <albullington@gmail.com>
2024-02-01 16:50:32 -0800

    Code cleanup

--- src/components/ObsDetails/ObsDetailsContainer.js
+++ src/components/ObsDetails/ObsDetailsContainer.js
@@ -299,7 +299,6 @@ const ObsDetailsContainer = ( ): Node => {
     ( idParams, optsWithAuth ) => createIdentification( idParams, optsWithAuth ),
     {
       onSuccess: data => {
-        console.log( data, "data in mutation" );
         if ( belongsToCurrentUser ) {
           realm?.write( ( ) => {
             const localIdentifications = localObservation?.identifications;
