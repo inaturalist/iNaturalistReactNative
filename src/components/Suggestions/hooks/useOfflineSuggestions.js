9f66b71160bcaa4107e1fac2440c9cfaa71447d1
Johannes Klein <johannes.t.klein@gmail.com>
2024-02-05 11:23:59 +0100

    Android and iOS have different return types

--- src/components/Suggestions/hooks/useOfflineSuggestions.js
+++ src/components/Suggestions/hooks/useOfflineSuggestions.js
@@ -4,6 +4,7 @@ import {
   useEffect,
   useState
 } from "react";
+import { Platform } from "react-native";
 import { predictImage } from "sharedHelpers/cvModel";
 import { log } from "sharedHelpers/logger";
 
@@ -27,7 +28,12 @@ const useOfflineSuggestions = (
       setLoadingOfflineSuggestions( true );
       let predictions = [];
       try {
-        predictions = await predictImage( selectedPhotoUri );
+        const result = await predictImage( selectedPhotoUri );
+        // Android returns an object with a predictions key, while iOS returns an array because
+        // currently Seek codebase as well expects different return types for each platform
+        predictions = Platform.OS === "android"
+          ? result.predictions
+          : result;
       } catch ( predictImageError ) {
         if ( predictImageError.message.match( /getWidth/ ) ) {
           // TODO fix this. There's a bug in the android side of vision-camera-plugin-inatvision
