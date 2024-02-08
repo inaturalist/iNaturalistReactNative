8dbb68d13cd5c38ec031be9359ed653f0f08963c
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-02-07 17:44:08 -0800

    ScrollableWithStickyHeader cleanup and docs
    * Refactored ScrollableWithStickyHeader so that it manages the height it
      sticks at and the scroll state
    * ScrollableWithStickyHeader explicitly takes two rendering functions as
      props, hopefully making what it does a little clearer
    * Added some comments attempting to explain what this component does and why
      we're not taking other approaches to sticky headers

--- src/components/SharedComponents/ScrollableWithStickyHeader.js
+++ src/components/SharedComponents/ScrollableWithStickyHeader.js
@@ -1,21 +1,51 @@
 // @flow
+
+// ScrollableWithStickyHeader renders a scrollable view (e.g. ScrollView or
+// FlashList) with a header component above it. The header component will
+// stick to the top of the screen when scrolled to a particular y value
+// (stickyAt)
+//
+// To use this, you need to give it two functions, one to render the header
+// and the other to render the scrollable. renderHeader takes a single
+// argument, the setStickyAt function, that sets the scroll offset at which
+// the header sticks (this is probably dependent on the height of the
+// rendered layout).
+//
+// renderScrollable takes a single argument, the onScroll callback, which
+// should be passed to the scrollable's onScroll prop, and/or get called with
+// the same event
+//
+// Some background: the easiest way to set a sticky header header with a
+// ScrollView is the stickyHeaderIndices prop, but that doesn't work quite as
+// expected with FlashList because the only children of the underlying
+// ScrollView in FlashList are the items themselves. You can render the
+// header as the first item and then make that stick, but you run into
+// problems when you try to show multiple columns and your header component
+// gets confined to the column width. The solution here uses an offset
+// transform to achive something similar. It also assumes it occupies full
+// height
+
+import { View } from "components/styledComponents";
 import type { Node } from "react";
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from "react";
 import { Animated } from "react-native";
 import { useDeviceOrientation } from "sharedHooks";
 
 const { diffClamp } = Animated;
 
 type Props = {
-  children: any,
-  scrollY: any,
-  heightAboveView: number
+  renderHeader: Function,
+  renderScrollable: Function
 };
 
 const ScrollableWithStickyHeader = ( {
-  children,
-  scrollY,
-  heightAboveView
+  renderHeader,
+  renderScrollable
 }: Props ): Node => {
   const {
     isTablet,
@@ -25,20 +55,39 @@ const ScrollableWithStickyHeader = ( {
   // eslint-disable-next-line no-unused-vars
   const [scrollPosition, setScrollPosition] = useState( 0 );
 
+  const [stickyAt, setStickyAt] = useState( 0 );
+
+  // basing collapsible sticky header code off the example in this article
+  // https://medium.com/swlh/making-a-collapsible-sticky-header-animations-with-react-native-6ad7763875c3
+  const scrollY = useRef( new Animated.Value( 0 ) );
+
+  const onScroll = Animated.event(
+    [
+      {
+        nativeEvent: {
+          contentOffset: { y: scrollY.current }
+        }
+      }
+    ],
+    {
+      useNativeDriver: true
+    }
+  );
+
   // On Android, the scroll view offset is a double (not an integer), and interpolation shouldn't be
   // one-to-one, which causes a jittery header while slow scrolling (see issue #634).
   // See here as well: https://stackoverflow.com/a/60898411/1233767
   const scrollYClamped = diffClamp(
     scrollY.current,
     0,
-    heightAboveView * 2
+    stickyAt * 2
   );
 
   // Same as comment above (see here: https://stackoverflow.com/a/60898411/1233767)
   const offsetForHeader = scrollYClamped.interpolate( {
-    inputRange: [0, heightAboveView * 2],
+    inputRange: [0, stickyAt * 2],
     // $FlowIgnore
-    outputRange: [0, -heightAboveView]
+    outputRange: [0, -stickyAt]
   } );
 
   useEffect( () => {
@@ -66,23 +115,35 @@ const ScrollableWithStickyHeader = ( {
     };
   }, [scrollY] );
 
-  const contentHeight = isTablet
-    ? screenHeight
-    : Math.max( screenWidth, screenHeight );
+  const contentHeight = useMemo(
+    ( ) => (
+      isTablet
+        ? screenHeight
+        : Math.max( screenWidth, screenHeight )
+    ),
+    [isTablet, screenHeight, screenWidth]
+  );
 
   return (
-    <Animated.View
-      style={[
-        {
-          transform: [{ translateY: offsetForHeader }],
-          // Set the height to flow off screen so that when we translate the
-          // view up, there's no gap at the bottom
-          height: contentHeight + heightAboveView
-        }
-      ]}
-    >
-      {children}
-    </Animated.View>
+    // Note that we want to occupy full height but hide the overflow because
+    // we are intentionally setting the height of the Animated.View to exceed
+    // the height of this parent view. We want the parent view to be laid out
+    // nicely with its peers, not flow off the screen.
+    <View className="overflow-hidden h-full">
+      <Animated.View
+        style={[
+          {
+            transform: [{ translateY: offsetForHeader }],
+            // Set the height to flow off screen so that when we translate the
+            // view up, there's no gap at the bottom
+            height: contentHeight + stickyAt
+          }
+        ]}
+      >
+        {renderHeader( setStickyAt )}
+        {renderScrollable( onScroll )}
+      </Animated.View>
+    </View>
   );
 };
 
