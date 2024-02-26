269f542fe327cc472afc4c3fd8088bbc8a41243c
Johannes Klein <johannes.t.klein@gmail.com>
2024-02-26 11:50:47 +0100

    Add UI for Help page

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -1,15 +1,101 @@
 // @flow
 
 import {
+  Body2,
+  Button,
+  Heading4,
   ScrollViewWrapper
 } from "components/SharedComponents";
 import { View } from "components/styledComponents";
+import { t } from "i18next";
 import type { Node } from "react";
 import React from "react";
+import { Alert, Linking } from "react-native";
+
 const Help = (): Node => {
+  const onHelpPressed = async () => {
+    const url = "";
+    const supported = await Linking.canOpenURL( url );
+    if ( supported ) {
+      await Linking.openURL( url );
+    } else {
+      Alert.alert( `Don't know how to open this URL: ${url}` );
+    }
+  };
+
+  const onContactPressed = async () => {
+    const url = "mailto:support@inaturalist.org";
+    const supported = await Linking.canOpenURL( url );
+    if ( supported ) {
+      await Linking.openURL( url );
+    } else {
+      Alert.alert( `Don't know how to open this URL: ${url}` );
+    }
+  };
+
+  const onTeacherPressed = async () => {
+    const url = "https://www.inaturalist.org/pages/teacher's+guide";
+    const supported = await Linking.canOpenURL( url );
+    if ( supported ) {
+      await Linking.openURL( url );
+    } else {
+      Alert.alert( `Don't know how to open this URL: ${url}` );
+    }
+  };
+
+  const onForumPressed = async () => {
+    const url = "https://forum.inaturalist.org";
+    const supported = await Linking.canOpenURL( url );
+    if ( supported ) {
+      await Linking.openURL( url );
+    } else {
+      Alert.alert( `Don't know how to open this URL: ${url}` );
+    }
+  };
+
   return (
     <ScrollViewWrapper>
       <View className="p-4">
+        <Heading4 className="mb-3">{t( "INATURALIST-HELP-PAGE" )}</Heading4>
+        <Body2 className="mb-5">
+          {t( "You-can-find-answers-on-our-help-page" )}
+        </Body2>
+        <Button
+          className="mb-8"
+          level="neutral"
+          text={t( "VIEW-INATURALIST-HELP" )}
+          onPress={() => onHelpPressed()}
+        />
+        <Heading4 className="mb-3">{t( "CONTACT-SUPPORT" )}</Heading4>
+        <Body2 className="mb-5">
+          {t( "Still-need-help" )}
+        </Body2>
+        <Button
+          className="mb-8"
+          level="neutral"
+          text={t( "CONTACT-SUPPORT" )}
+          onPress={() => onContactPressed()}
+        />
+        <Heading4 className="mb-3">{t( "TEACHERS" )}</Heading4>
+        <Body2 className="mb-5">
+          {t( "Are-you-a-teacher" )}
+        </Body2>
+        <Button
+          className="mb-8"
+          level="neutral"
+          text={t( "VIEW-TEACHERS-GUIDE" )}
+          onPress={() => onTeacherPressed()}
+        />
+        <Heading4 className="mb-3">{t( "INATURALIST-FORUM" )}</Heading4>
+        <Body2 className="mb-5">
+          {t( "Connect-with-other-naturalists" )}
+        </Body2>
+        <Button
+          className="mb-8"
+          level="neutral"
+          text={t( "INATURALIST-FORUM" )}
+          onPress={() => onForumPressed()}
+        />
       </View>
     </ScrollViewWrapper>
   );
