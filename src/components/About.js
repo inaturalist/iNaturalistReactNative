ca39d91c099093754bcbedf86af99669ac5bcf1d
Johannes Klein <johannes.t.klein@gmail.com>
2024-02-26 12:42:39 +0100

    Remove elements that are already on Developer screen as well

--- src/components/About.js
+++ src/components/About.js
@@ -6,39 +6,11 @@ import { t } from "i18next";
 import type { Node } from "react";
 import React from "react";
 import { Platform } from "react-native";
-import Config from "react-native-config";
-import {
-  getBuildNumber,
-  getVersion
-} from "react-native-device-info";
 import useLogs from "sharedHooks/useLogs";
 
 import ScrollViewWrapper from "./SharedComponents/ScrollViewWrapper";
 
-const modelFileName: string = Platform.select( {
-  ios: Config.IOS_MODEL_FILE_NAME,
-  android: Config.ANDROID_MODEL_FILE_NAME
-} );
-const taxonomyFileName = Platform.select( {
-  ios: Config.IOS_TAXONOMY_FILE_NAME,
-  android: Config.ANDROID_TAXONOMY_FILE_NAME
-} );
-
-type TextHeaderProps = {
-  level?: number,
-  children: any
-};
-
-const TextHeader = ( { level, children }: TextHeaderProps ) => {
-  let sizeClass = "text-2xl";
-  if ( level === 2 ) sizeClass = "text-xl";
-  if ( level === 3 ) sizeClass = "text-lg";
-  return <Text className={`${sizeClass} mt-1 mb-2`}>{children}</Text>;
-};
-
 const About = (): Node => {
-  const appVersion = getVersion();
-  const buildVersion = getBuildNumber();
   const { shareLogFile, emailLogFile } = useLogs( );
 
   /* eslint-disable i18next/no-literal-string */
@@ -49,17 +21,6 @@ const About = (): Node => {
           This is an app under development! Right now this page just shows some
           details for developers.
         </Text>
-        <TextHeader>Version</TextHeader>
-        <Text selectable>{`${appVersion} (${buildVersion})`}</Text>
-        <TextHeader>Computer Vision</TextHeader>
-        <View className="flex-row">
-          <Text className="font-bold">Model: </Text>
-          <Text selectable>{modelFileName}</Text>
-        </View>
-        <View className="flex-row mb-5">
-          <Text className="font-bold">Taxonomy: </Text>
-          <Text selectable>{taxonomyFileName}</Text>
-        </View>
         <Button
           level="focus"
           onPress={emailLogFile}
