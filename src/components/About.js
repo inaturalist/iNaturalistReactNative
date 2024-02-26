2ce3d7535c7a7c73e577c393f6a4ca7fe7b7e5c5
Johannes Klein <johannes.t.klein@gmail.com>
2024-02-26 14:23:01 +0100

    Add image

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ import {
   Tabs,
   ViewWrapper
 } from "components/SharedComponents";
-import { ScrollView, View } from "components/styledComponents";
+import { Image, View } from "components/styledComponents";
 import { t } from "i18next";
 import type { Node } from "react";
 import React, { useState } from "react";
@@ -44,41 +44,55 @@ const About = (): Node => {
         activeId={activeTab}
       />
 
-          <Heading4 className="mb-3">
-            {t( "INATURALIST-MISSION-VISION" )}
-          </Heading4>
-          <Body2 className="mb-5">
-            {t( "iNaturalist-mission-is-to-connect" )}
-          </Body2>
-          <Body2 className="mb-5">{t( "iNaturalist-vision-is-a-world" )}</Body2>
-          <Heading4 className="mb-3">{t( "WHAT-IS-INATURALIST" )}</Heading4>
-          <Body2 className="mb-5">{t( "iNaturalist-helps-you-identify" )}</Body2>
-          <Body2 className="mb-5">{t( "Whats-more-by-recording" )}</Body2>
-          <Body2 className="mb-5">{t( "iNaturalist-is-supported-by" )}</Body2>
-
-          <View className="mb-8 items-center justify-center">
-            <Body1>{`Version ${appVersion} (${buildVersion})`}</Body1>
       <ScrollViewWrapper>
         {activeTab === aboutID && (
           <View className="px-4 py-8">
+            <View className="mb-8 items-center justify-center">
+              <Image
+                className="w-[250px] h-[45px]"
+                resizeMode="contain"
+                source={require( "images/inaturalist-dark.png" )}
+                accessibilityIgnoresInvertColors
+              />
+            </View>
+            <Heading4 className="mb-3">
+              {t( "INATURALIST-MISSION-VISION" )}
+            </Heading4>
+            <Body2 className="mb-5">
+              {t( "iNaturalist-mission-is-to-connect" )}
+            </Body2>
+            <Body2 className="mb-5">{t( "iNaturalist-vision-is-a-world" )}</Body2>
+            <Heading4 className="mb-3">{t( "WHAT-IS-INATURALIST" )}</Heading4>
+            <Body2 className="mb-5">
+              {t( "iNaturalist-helps-you-identify" )}
+            </Body2>
+            <Body2 className="mb-5">{t( "Whats-more-by-recording" )}</Body2>
+            <Body2 className="mb-5">{t( "iNaturalist-is-supported-by" )}</Body2>
+            <Body2 className="underline font-bold">{t( "Terms-of-Use" )}</Body2>
+            <Body2 className="underline font-bold">{t( "Privacy-Policy" )}</Body2>
+            <Body2 className="underline font-bold">
+              {t( "Community-Guidelines" )}
+            </Body2>
+            <View className="items-center justify-center">
+              <Body1>{`Version ${appVersion} (${buildVersion})`}</Body1>
+            </View>
           </View>
         )}
         {activeTab === teamID && (
+          <View className="px-4 py-8">
+            <Heading4 className="mb-3">{t( "INATURALIST-TEAM" )}</Heading4>
+            <Body2 className="mb-5">
+              {t( "iNaturalists-apps-are-designed-and-developed" )}
+            </Body2>
+            <Heading4 className="mb-3">{t( "INATURALIST-COMMUNITY" )}</Heading4>
+            <Body2 className="mb-5">
+              {t( "iNaturalist-is-supported-by-community" )}
+            </Body2>
+            <Heading4 className="mb-3">{t( "INATURALIST-NETWORK" )}</Heading4>
+            <Body2 className="mb-5">{t( "The-iNaturalist-Network" )}</Body2>
+            <Heading4 className="mb-3">{t( "COLLABORATORS" )}</Heading4>
+            <Body2>{t( "The-iNaturalist-team-has-collaborated" )}</Body2>
           </View>
-          <Heading4 className="mb-3">{t( "INATURALIST-TEAM" )}</Heading4>
-          <Body2 className="mb-5">
-            {t( "iNaturalists-apps-are-designed-and-developed" )}
-          </Body2>
-          <Heading4 className="mb-3">{t( "INATURALIST-COMMUNITY" )}</Heading4>
-          <Body2 className="mb-5">
-            {t( "iNaturalist-is-supported-by-community" )}
-          </Body2>
-          <Heading4 className="mb-3">{t( "INATURALIST-NETWORK" )}</Heading4>
-          <Body2 className="mb-5">{t( "The-iNaturalist-Network" )}</Body2>
-          <Heading4 className="mb-3">{t( "COLLABORATORS" )}</Heading4>
-          <Body2 className="mb-5">
-            {t( "The-iNaturalist-team-has-collaborated" )}
-          </Body2>
         )}
       </ScrollViewWrapper>
     </ViewWrapper>
