b9d7d7950605e459de6b9e6c01896937bc06333d
Amanda Bullington <albullington@gmail.com>
2024-03-04 22:03:23 +1100

    Clean logs

--- src/api/search.js
+++ src/api/search.js
@@ -20,7 +20,6 @@ const fetchSearchResults = async ( params: Object = {}, opts: Object = {} ): Pro
   try {
     const response = await inatjs.search( { ...PARAMS, ...params }, opts );
     if ( !response ) { return null; }
-    console.log( response?.results?.[0], "first result in search" );
     const records = response?.results?.map( result => {
       const recordType = mappedRecords[params.sources];
       return result[recordType];
