969956c4876824bad0a9ebad07491f9a28b715d4
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-06 00:09:43 +0100

    Get the user jwt token as authorization for the log request

--- src/api/log.ts
+++ src/api/log.ts
@@ -1,16 +1,25 @@
 import { getUserAgent } from "api/userAgent";
 import { create } from "apisauce";
+import { getJWT } from "components/LoginSignUp/AuthenticationService";
 import Config from "react-native-config";
 import { transportFunctionType } from "react-native-logs";
+
 const API_HOST: string
     = Config.API_URL || process.env.API_URL || "https://api.inaturalist.org/v2";
 
 // Custom transport for posting to iNat API logging
 const iNatLogstashTransport: transportFunctionType = async (props) => {
+  const userToken = await getJWT(); // Removed the import causing the dependency cycle
+  if ( !userToken ) {
+    return;
+  }
   const api = create( {
     baseURL: API_HOST,
     headers: {
       "User-Agent": getUserAgent(),
+      Authorization: [
+        userToken,
+      ].join( ", " )
     }
   } );
   const message
