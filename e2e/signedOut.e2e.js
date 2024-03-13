4342f9157e2011d8f8f0438073252c5a5f83bf90
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-13 09:25:40 +0100

    Code format

--- e2e/signedOut.e2e.js
+++ e2e/signedOut.e2e.js
@@ -20,17 +20,12 @@ describe( "Signed out user", () => {
 
   it( "should add an observation without evidence", async () => {
     const addObsButton = element( by.id( "add-obs-button" ) );
-    await waitFor( addObsButton ).toBeVisible().withTimeout( 10000 );
-    await addObsButton.tap();
-    await expect( element( by.id( "evidence-text" ) ) ).toBeVisible();
-    const obsWithoutEvidenceButton = element(
-      by.id( "observe-without-evidence-button" )
-    );
-    await expect( obsWithoutEvidenceButton ).toBeVisible();
-    await obsWithoutEvidenceButton.tap();
-
-    await waitFor( element( by.id( "new-observation-text" ) ) )
-      .toBeVisible()
-      .withTimeout( 10000 );
+    await waitFor( addObsButton ).toBeVisible( ).withTimeout( 10000 );
+    await addObsButton.tap( );
+    await expect( element( by.id( "evidence-text" ) ) ).toBeVisible( );
+    const obsWithoutEvidenceButton = element( by.id( "observe-without-evidence-button" ) );
+    await expect( obsWithoutEvidenceButton ).toBeVisible( );
+    await obsWithoutEvidenceButton.tap( );
+    await waitFor( element( by.id( "new-observation-text" ) ) ).toBeVisible( ).withTimeout( 10000 );
   } );
 } );
