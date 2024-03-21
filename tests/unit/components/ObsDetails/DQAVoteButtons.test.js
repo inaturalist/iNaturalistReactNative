5fed43a20b7d9ee56b635607ac3aef0358c15f1d
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-21 12:57:50 +0100

    Add needs_id DQAVoteButtons unit test

--- tests/unit/components/ObsDetails/DQAVoteButtons.test.js
+++ tests/unit/components/ObsDetails/DQAVoteButtons.test.js
@@ -14,6 +14,14 @@ const mockQualityMetrics = [
   }
 ];
 
+const mockVotesNeedsID = [
+  factory( "RemoteVote", {
+    vote_scope: "needs_id",
+    user: mockUser,
+    user_id: mockUser.id
+  } )
+];
+
 // Mock useCurrentUser hook
 jest.mock( "sharedHooks/useCurrentUser", ( ) => ( {
   __esModule: true,
@@ -58,5 +66,38 @@ describe( "DQA Vote Buttons for wild metric", ( ) => {
   } );
 } );
 
+describe( "DQA Vote Buttons for needs_id metric", ( ) => {
+  test( "renders correct DQA user vote", async ( ) => {
+    renderComponent( <DQAVoteButtons
+      metric="needs_id"
+      votes={mockVotesNeedsID}
+      setVote={jest.fn()}
+      loadingAgree={jest.fn()}
+      loadingDisagree={jest.fn()}
+      loadingMetric={jest.fn()}
+      removeVote={jest.fn()}
+    /> );
+
+    const button = await screen.findByTestId(
+      "DQAVoteButton.UserAgree"
+    );
+    expect( button ).toBeTruthy( );
+  } );
+
+  test( "renders correct DQA user vote number", async ( ) => {
+    renderComponent( <DQAVoteButtons
+      metric="needs_id"
+      votes={mockVotesNeedsID}
+      setVote={jest.fn()}
+      loadingAgree={jest.fn()}
+      loadingDisagree={jest.fn()}
+      loadingMetric={jest.fn()}
+      removeVote={jest.fn()}
+    /> );
+
+    const voteNumber = await screen.findByText(
+      "1"
+    );
+    expect( voteNumber ).toBeTruthy( );
   } );
 } );
