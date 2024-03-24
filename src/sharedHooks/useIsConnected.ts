252eb231693cdc62068750c41c7e0e0310a65cb8
Yaron Budowski <budowski@gmail.com>
2024-03-24 16:01:36 +0200

    #1144 - when using useOnlineSuggestions - check for Internet connectivity

--- src/sharedHooks/useIsConnected.ts
+++ src/sharedHooks/useIsConnected.ts
@@ -2,7 +2,10 @@ import { useNetInfo } from "@react-native-community/netinfo";
 
 // Note that a return value of null means the state is unknown
 const useIsConnected = ( ): boolean | null => {
-  const { isInternetReachable } = useNetInfo( );
+  const { isInternetReachable } = useNetInfo({
+    reachabilityShortTimeout: 1000,
+    reachabilityRequestTimeout: 1000
+  } );
   return isInternetReachable;
 };
 
