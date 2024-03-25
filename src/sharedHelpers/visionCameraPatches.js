c265f454a3218a84af7798978a03acd3b0a05b74
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-25 22:21:14 +0100

    Remove log

--- src/sharedHelpers/visionCameraPatches.js
+++ src/sharedHelpers/visionCameraPatches.js
@@ -170,7 +170,6 @@ export const usePatchedRunAsync = ( ) => {
       "worklet";
 
       try {
-        logOnJs( "customRunOnAsyncContext - func call" );
         func( frame );
       } catch ( e ) {
         logOnJs( "customRunOnAsyncContext error", e );
