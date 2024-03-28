0e1dc984d7039de8aa6ff870fc616d90f492e8c0
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-28 11:12:31 +0100

    Replace password autofill workaround

--- e2e/helpers.js
+++ e2e/helpers.js
@@ -1,4 +1,22 @@
+import { execSync } from "child_process";
+
 export async function iNatE2eBeforeAll( device ) {
+  if ( device.getPlatform() === "ios" ) {
+    // disable password autofill
+    execSync(
+      // eslint-disable-next-line max-len
+      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles/UserSettings.plist`
+    );
+    execSync(
+      // eslint-disable-next-line max-len
+      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/EffectiveUserSettings.plist`
+    );
+    execSync(
+      // eslint-disable-next-line max-len
+      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/PublicInfo/PublicEffectiveUserSettings.plist`
+    );
+  }
+
   if ( device.getPlatform() === "android" ) {
     await device.launchApp( {
       newInstance: true,
