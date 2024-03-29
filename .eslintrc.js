5d022c25b7c109ce554a08ea934c467ce7c0f0c6
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-29 18:18:39 +0100

    Add comment to muted eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   extends: [
     "airbnb",
+    // This was added to the RN0.72 template, but it does not work with our current setup
     // "@react-native",
     "plugin:i18next/recommended",
     "plugin:@tanstack/eslint-plugin-query/recommended",
