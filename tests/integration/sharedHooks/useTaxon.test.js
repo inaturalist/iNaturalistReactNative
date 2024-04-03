219baa4656744321ad626ccb6f88478f75017e14
Amanda Bullington <albullington@gmail.com>
2024-04-02 19:51:43 -0700

    Fix jest upgrade test failures

--- tests/integration/sharedHooks/useTaxon.test.js
+++ tests/integration/sharedHooks/useTaxon.test.js
@@ -47,98 +47,87 @@ const mockOutdatedTaxon = factory( "LocalTaxon", {
   }
 } );
 
-describe( "useTaxon", ( ) => {
-  beforeAll( async () => {
-    jest.useFakeTimers( );
-  } );
+describe( "with local taxon", ( ) => {
   beforeEach( ( ) => {
     jest.restoreAllMocks( );
-    inatjs.taxa.fetch.mockResolvedValue( makeResponse( [mockRemoteTaxon] ) );
+    // Write mock taxon to realm
+    safeRealmWrite( global.mockRealms[mockRealmIdentifier], ( ) => {
+      global.mockRealms[mockRealmIdentifier].create( "Taxon", mockTaxon, "modified" );
+    }, "write mock taxon, useTaxon test" );
   } );
 
-  describe( "with local taxon", ( ) => {
-    beforeEach( ( ) => {
-      // Write mock taxon to realm
-      safeRealmWrite( global.mockRealms[mockRealmIdentifier], ( ) => {
-        global.mockRealms[mockRealmIdentifier].create( "Taxon", mockTaxon, "modified" );
-      }, "write mock taxon, useTaxon test" );
-    } );
+  it( "should return an object", ( ) => {
+    const { result } = renderHook( ( ) => useTaxon( mockTaxon ) );
+    expect( result.current ).toBeInstanceOf( Object );
+  } );
 
-    it( "should return an object", ( ) => {
+  describe( "when there is a local taxon with taxon id", ( ) => {
+    it( "should return local taxon with default photo", ( ) => {
       const { result } = renderHook( ( ) => useTaxon( mockTaxon ) );
-      expect( result.current ).toBeInstanceOf( Object );
+      const { taxon: resultTaxon } = result.current;
+      expect( resultTaxon ).toHaveProperty( "default_photo" );
+      expect( resultTaxon.default_photo.url ).toEqual( mockTaxon.default_photo.url );
     } );
 
-    describe( "when there is a local taxon with taxon id", ( ) => {
-      it( "should return local taxon with default photo", ( ) => {
-        const { result } = renderHook( ( ) => useTaxon( mockTaxon ) );
-        expect( inatjs.taxa.fetch ).not.toHaveBeenCalled( );
-        const { taxon: resultTaxon } = result.current;
-        expect( resultTaxon ).toHaveProperty( "default_photo" );
-        expect( resultTaxon.default_photo.url ).toEqual( mockTaxon.default_photo.url );
-      } );
+    it( "should request a taxon from the API if the local copy is out of date", async ( ) => {
+      inatjs.taxa.fetch.mockResolvedValue( makeResponse( [mockRemoteTaxon] ) );
+      safeRealmWrite( global.mockRealms[mockRealmIdentifier], ( ) => {
+        global.mockRealms[mockRealmIdentifier].create( "Taxon", mockOutdatedTaxon, "modified" );
+      }, "write mock outdated taxon, useTaxon test" );
+      renderHook( ( ) => useTaxon( mockOutdatedTaxon ) );
+      await waitFor( ( ) => expect( inatjs.taxa.fetch ).toHaveBeenCalled( ) );
+    } );
+  } );
+} );
 
-      it( "should request a taxon from the API if the local copy is out of date", async ( ) => {
-        safeRealmWrite( global.mockRealms[mockRealmIdentifier], ( ) => {
-          global.mockRealms[mockRealmIdentifier].create( "Taxon", mockOutdatedTaxon, "modified" );
-        }, "write mock outdated taxon, useTaxon test" );
-        renderHook( ( ) => useTaxon( mockOutdatedTaxon ) );
-        await waitFor( ( ) => expect( inatjs.taxa.fetch ).toHaveBeenCalled( ) );
-      } );
+describe( "when there is no local taxon with taxon id", ( ) => {
+  beforeAll( ( ) => {
+    safeRealmWrite( global.mockRealms[mockRealmIdentifier], ( ) => {
+      global.mockRealms[mockRealmIdentifier].deleteAll( );
+    }, "delete all realm, useTaxon test" );
+  } );
+
+  describe( "with fetchRemote: true", ( ) => {
+    it( "should request the taxon from the API", async ( ) => {
+      expect(
+        global.mockRealms[mockRealmIdentifier].objectForPrimaryKey( "Taxon", mockTaxon.id )
+      ).toBeNull( );
+      renderHook( ( ) => useTaxon( mockTaxon ) );
+      await waitFor( ( ) => expect( inatjs.taxa.fetch ).toHaveBeenCalled( ) );
+    } );
+
+    it( "should return the argument taxon if request fails", ( ) => {
+      // I don't love this. While it kind of mocks at the edge of the code
+      // we need to integrate, it doesn't test out API error handling code.
+      // I tried mocking inatjs to make it throw, but that always seems to
+      // result in a failure in the test, even though useQuery should catch
+      // those errors. ~~~kueda20240305
+      jest.mock( "@tanstack/react-query", () => ( {
+        useQuery: jest.fn( ( ) => ( {
+          error: { }
+        } ) )
+      } ) );
+      const partialTaxon = { id: faker.number.int( ), foo: "bar" };
+      const { result } = renderHook( ( ) => useTaxon( partialTaxon ) );
+      expect( result.current.taxon.foo ).toEqual( "bar" );
+      jest.unmock( "@tanstack/react-query" );
+    } );
+
+    it( "should return a taxon like a local taxon record if the request succeeds", async ( ) => {
+      const { result } = renderHook( ( ) => useTaxon( { id: mockTaxon.id } ) );
+      expect( result.current.taxon ).toHaveProperty( "default_photo" );
     } );
   } );
 
-  describe( "when there is no local taxon with taxon id", ( ) => {
-    beforeEach( async ( ) => {
-      safeRealmWrite( global.mockRealms[mockRealmIdentifier], ( ) => {
-        global.mockRealms[mockRealmIdentifier].deleteAll( );
-      }, "delete all realm, useTaxon test" );
-    } );
-
-    describe( "with fetchRemote: true", ( ) => {
-      it( "should request the taxon from the API", async ( ) => {
-        expect(
-          global.mockRealms[mockRealmIdentifier].objectForPrimaryKey( "Taxon", mockTaxon.id )
-        ).toBeNull( );
-        renderHook( ( ) => useTaxon( mockTaxon ) );
-        await waitFor( ( ) => expect( inatjs.taxa.fetch ).toHaveBeenCalled( ) );
-      } );
-
-      it( "should return the argument taxon if request fails", ( ) => {
-        // I don't love this. While it kind of mocks at the edge of the code
-        // we need to integrate, it doesn't test out API error handling code.
-        // I tried mocking inatjs to make it throw, but that always seems to
-        // result in a failure in the test, even though useQuery should catch
-        // those errors. ~~~kueda20240305
-        jest.mock( "@tanstack/react-query", () => ( {
-          useQuery: jest.fn( ( ) => ( {
-            error: { }
-          } ) )
-        } ) );
-        const partialTaxon = { id: faker.number.int( ), foo: "bar" };
-        const { result } = renderHook( ( ) => useTaxon( partialTaxon ) );
-        expect( result.current.taxon.foo ).toEqual( "bar" );
-        jest.unmock( "@tanstack/react-query" );
-      } );
-
-      it( "should return a taxon like a local taxon record if the request succeeds", async ( ) => {
-        const { result } = renderHook( ( ) => useTaxon( { id: mockTaxon.id } ) );
-        await waitFor( ( ) => expect( inatjs.taxa.fetch ).toHaveBeenCalled( ) );
-        expect( result.current.taxon ).toHaveProperty( "default_photo" );
-      } );
-    } );
-
-    describe( "with fetchRemote: false", ( ) => {
-      it( "should not call the API and return passed in taxon", ( ) => {
-        const taxonId = faker.number.int( );
-        expect(
-          global.mockRealms[mockRealmIdentifier].objectForPrimaryKey( "Taxon", taxonId )
-        ).toBeNull( );
-        const { result } = renderHook( ( ) => useTaxon( { id: taxonId, foo: "bar" }, false ) );
-        expect( inatjs.taxa.fetch ).not.toHaveBeenCalled( );
-        expect( result.current.taxon ).not.toHaveProperty( "default_photo" );
-        expect( result.current.taxon.foo ).toEqual( "bar" );
-      } );
+  describe( "with fetchRemote: false", ( ) => {
+    it( "should not call the API and return passed in taxon", ( ) => {
+      const taxonId = faker.number.int( );
+      expect(
+        global.mockRealms[mockRealmIdentifier].objectForPrimaryKey( "Taxon", taxonId )
+      ).toBeNull( );
+      const { result } = renderHook( ( ) => useTaxon( { id: taxonId, foo: "bar" }, false ) );
+      expect( result.current.taxon ).not.toHaveProperty( "default_photo" );
+      expect( result.current.taxon.foo ).toEqual( "bar" );
     } );
   } );
 } );
