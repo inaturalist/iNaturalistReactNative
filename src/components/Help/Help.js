57877001c018a1a99c235e3c5a1a1bd3a2705138
Amanda Bullington <albullington@gmail.com>
2024-04-09 16:26:04 -0700

    Allow users to send support emails via Gmail; closes #1356

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -12,10 +12,11 @@ import { t } from "i18next";
 import type { Node } from "react";
 import React from "react";
 import { Alert, Linking } from "react-native";
+import { openComposer, getEmailClients } from "react-native-email-link";
 
-const Help = (): Node => {
-  const navigation = useNavigation();
-  const onHelpPressed = async () => {
+const Help = ( ): Node => {
+  const navigation = useNavigation( );
+  const onHelpPressed = async ( ) => {
     const url = "https://help.inaturalist.org/";
     navigation.navigate( "FullPageWebView", {
       title: t( "iNaturalist-Help" ),
@@ -26,29 +27,14 @@ const Help = (): Node => {
     } );
   };
 
-  const onContactPressed = async () => {
-    // TODO: Should it use react-native-mail or Linking?
-    const url = "mailto:support@inaturalist.org";
-    try {
-      const supported = await Linking.canOpenURL( url );
-      if ( supported ) {
-        await Linking.openURL( url );
-      } else {
-        Alert.alert( `Don't know how to open this URL: ${url}` );
-      }
-    } catch ( canOpenURLError ) {
-      if ( canOpenURLError.message.match( /Info.plist/ ) ) {
-        Alert.alert(
-          "No email in the Simulator",
-          "This way of sending email doesn't work in the Simulator. Try it on a physical device."
-        );
-      } else {
-        throw canOpenURLError;
-      }
-    }
+  const onContactPressed = ( ) => {
+    openComposer( {
+      to: "support@inaturalist.org",
+      subject: "Help with iNaturalist Beta"
+    } );
   };
 
-  const onTeacherPressed = async () => {
+  const onTeacherPressed = async ( ) => {
     const url = "https://www.inaturalist.org/pages/teacher's+guide";
     navigation.navigate( "FullPageWebView", {
       title: t( "Teachers-Guide" ),
@@ -58,7 +44,7 @@ const Help = (): Node => {
     } );
   };
 
-  const onForumPressed = async () => {
+  const onForumPressed = async ( ) => {
     const url = "https://forum.inaturalist.org";
     const supported = await Linking.canOpenURL( url );
     if ( supported ) {
@@ -79,7 +65,7 @@ const Help = (): Node => {
           className="mb-8"
           level="neutral"
           text={t( "VIEW-INATURALIST-HELP" )}
-          onPress={() => onHelpPressed()}
+          onPress={onHelpPressed}
         />
         <Heading4 className="mb-3">{t( "CONTACT-SUPPORT" )}</Heading4>
         <Body2 className="mb-5">
@@ -89,7 +75,7 @@ const Help = (): Node => {
           className="mb-8"
           level="neutral"
           text={t( "CONTACT-SUPPORT" )}
-          onPress={() => onContactPressed()}
+          onPress={onContactPressed}
         />
         <Heading4 className="mb-3">{t( "TEACHERS" )}</Heading4>
         <Body2 className="mb-5">
@@ -99,7 +85,7 @@ const Help = (): Node => {
           className="mb-8"
           level="neutral"
           text={t( "VIEW-TEACHERS-GUIDE" )}
-          onPress={() => onTeacherPressed()}
+          onPress={onTeacherPressed}
         />
         <Heading4 className="mb-3">{t( "INATURALIST-FORUM" )}</Heading4>
         <Body2 className="mb-5">
@@ -109,7 +95,7 @@ const Help = (): Node => {
           className="mb-8"
           level="neutral"
           text={t( "INATURALIST-FORUM" )}
-          onPress={() => onForumPressed()}
+          onPress={onForumPressed}
         />
       </View>
     </ScrollViewWrapper>
