ad4bb6f05b992c30c49303573a4a71e36e112c2c
Johannes Klein <johannes.t.klein@gmail.com>
2024-04-09 22:10:28 +0200

    Chang in prop usage

--- src/components/Camera/hooks/useTakePhoto.js
+++ src/components/Camera/hooks/useTakePhoto.js
@@ -18,7 +18,6 @@ const useTakePhoto = ( camera: Object, addEvidence: ?boolean, device: Object ): 
   const hasFlash = device?.hasFlash;
   const initialPhotoOptions = {
     enableShutterSound: true,
-    qualityPrioritization: "quality",
     ...( hasFlash && { flash: "off" } )
   };
   const [takePhotoOptions, setTakePhotoOptions] = useState( initialPhotoOptions );
