2a0d9511a5ca306b43207fbacc8dc5d07ed151e3
Amanda Bullington <albullington@gmail.com>
2024-04-10 10:17:33 -0700

    Update RadioButton to make description pressable; closes #1188

--- src/components/SharedComponents/RadioButtonRow.js
+++ src/components/SharedComponents/RadioButtonRow.js
@@ -1,75 +1,55 @@
 // @flow
-import classnames from "classnames";
 import {
   Body1,
   INatIcon,
   List2
 } from "components/SharedComponents";
-import { View } from "components/styledComponents";
+import { Pressable, View } from "components/styledComponents";
 import type { Node } from "react";
-import React, { useMemo } from "react";
-import { Platform } from "react-native";
+import React from "react";
 import { RadioButton, useTheme } from "react-native-paper";
 
 type Props = {
-  value: string,
-  icon: string,
   checked: boolean,
-  onPress: Function,
-  label: string,
   description: ?string,
-  style: ?Object
+  icon: string,
+  label: string,
+  onPress: Function,
+  value: string,
 }
 
 const RadioButtonRow = ( {
-  value, icon, checked, onPress, label, description, style
+  description,
+  checked,
+  label,
+  onPress,
+  icon,
+  value
 }: Props ): Node => {
   const theme = useTheme( );
 
-  const labelStyle = {
-    fontSize: 18,
-    lineHeight: 22,
-    fontFamily: `Whitney-Light${Platform.OS === "ios"
-      ? ""
-      : "-Pro"}`,
-    color: theme.colors.primary,
-    fontWeight: "500",
-    textAlign: "left",
-    textAlignVertical: "center"
-  };
-
-  const iconLabel = useMemo( () => (
-    <View className="flex-row">
-      <Body1 className="mr-2">{label}</Body1>
-      <INatIcon name={icon} size={19} color={theme.colors.secondary} />
-    </View>
-  ), [label, icon, theme.colors.secondary] );
-
-  const labelComponent = useMemo( () => {
-    if ( icon ) return iconLabel;
-    return label;
-  }, [icon, iconLabel, label] );
+  const status = checked
+    ? "checked"
+    : "unchecked";
 
   return (
-    <RadioButton.Group>
-      <RadioButton.Item
-        value={value}
-        status={checked
-          ? "checked"
-          : "unchecked"}
-        onPress={onPress}
-        mode="android"
-        label={labelComponent}
-        position="leading"
-        labelStyle={labelStyle}
-        className={classnames( "p-0" )}
-        accessibilityLabel={label}
-        style={style}
-      />
+    <Pressable accessibilityRole="button" onPress={onPress}>
+      <View className="flex-row items-center">
+        <RadioButton.Android
+          onPress={onPress}
+          value={value}
+          status={status}
+          accessibilityLabel={label}
+        />
+        <View className="flex-row">
+          <Body1 className="mr-2">{label}</Body1>
+          {icon && <INatIcon name={icon} size={19} color={theme.colors.secondary} />}
+        </View>
+      </View>
       {description && (
         <List2 className="ml-[37px] mr-[33px] py-1">{description}</List2>
       )}
-    </RadioButton.Group>
+    </Pressable>
   );
 };
 
