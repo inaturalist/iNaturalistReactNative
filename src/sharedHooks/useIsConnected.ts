7eb5c3dde58b711884f3d2f24588d98161c2d665
Yaron Budowski <budowski@gmail.com>
2024-04-11 00:52:42 +0300

    Undo useIsConnected settings

--- src/sharedHooks/useIsConnected.ts
+++ src/sharedHooks/useIsConnected.ts
@@ -2,10 +2,7 @@ import { useNetInfo } from "@react-native-community/netinfo";
 
 // Note that a return value of null means the state is unknown
 const useIsConnected = ( ): boolean | null => {
-  const { isInternetReachable } = useNetInfo({
-    reachabilityShortTimeout: 1000,
-    reachabilityRequestTimeout: 1000
-  } );
+  const { isInternetReachable } = useNetInfo( );
   return isInternetReachable;
 };
 
