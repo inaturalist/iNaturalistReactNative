2f7a02413f0d7f228fd0f5f2929928a802f3876e
Johannes Klein <johannes.t.klein@gmail.com>
2024-04-11 23:05:06 +0200

    Add default value for boolean setting

--- src/sharedHooks/useStorage.ts
+++ src/sharedHooks/useStorage.ts
@@ -1,11 +1,10 @@
 import { useMMKVBoolean } from "react-native-mmkv";
 
 const useStorage = ( ): {
-  // TODO: Can I set a default value so that this is always a boolean?
-  isAdvancedUser: boolean | undefined,
+  isAdvancedUser: boolean,
   setIsAdvancedUser: ( value: boolean ) => void
 } => {
-  const [isAdvancedUser, setIsAdvancedUser] = useMMKVBoolean( "isAdvancedUser" );
+  const [isAdvancedUser = false, setIsAdvancedUser] = useMMKVBoolean( "isAdvancedUser" );
   return { isAdvancedUser, setIsAdvancedUser };
 };
 
