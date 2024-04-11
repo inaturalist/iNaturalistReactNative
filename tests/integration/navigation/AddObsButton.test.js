a1b938b204d028e46319435e3f810906bd6e0ccc
Johannes Klein <johannes.t.klein@gmail.com>
2024-04-11 12:30:21 +0200

    Add test case for advanced user

--- tests/integration/navigation/AddObsButton.test.js
+++ tests/integration/navigation/AddObsButton.test.js
@@ -2,6 +2,7 @@ import { screen, userEvent } from "@testing-library/react-native";
 import AddObsButton from "components/SharedComponents/Buttons/AddObsButton";
 import i18next from "i18next";
 import React from "react";
+import * as useStorage from "sharedHooks/useStorage.ts";
 import { renderComponent } from "tests/helpers/render";
 
 const actor = userEvent.setup();
@@ -38,13 +39,44 @@ describe( "AddObsButton", ( ) => {
   } );
 } );
 
-//     expect( noEvidenceButton ).toBeTruthy( );
-//     await user.press( noEvidenceButton );
-//     await waitFor( ( ) => {
-//       expect( mockNavigate ).toHaveBeenCalledWith( "CameraNavigator", {
-//         screen: "ObsEdit",
-//         params: { previousScreen: null }
-//       } );
-//     } );
-//   } );
-// } );
+describe( "for advanced user", ( ) => {
+  beforeAll( ( ) => {
+    jest.useFakeTimers( );
+    jest.spyOn( useStorage, "default" ).mockImplementation( () => ( {
+      isAdvancedUser: true
+    } ) );
+  } );
+
+  it( "opens AddObsModal", async ( ) => {
+    renderComponent( <AddObsButton /> );
+
+    const addObsButton = screen.getByLabelText(
+      i18next.t( "Add-observations" )
+    );
+    expect( addObsButton ).toBeTruthy( );
+    await actor.press( addObsButton );
+
+    const noEvidenceButton = screen.getByLabelText(
+      i18next.t( "Observation-with-no-evidence" )
+    );
+    expect( noEvidenceButton ).toBeTruthy( );
+  } );
+
+  it( "navigates user to obs edit with no evidence", async ( ) => {
+    renderComponent( <AddObsButton /> );
+    const addObsButton = screen.getByLabelText(
+      i18next.t( "Add-observations" )
+    );
+    expect( addObsButton ).toBeTruthy( );
+    await actor.press( addObsButton );
+    const noEvidenceButton = screen.getByLabelText(
+      i18next.t( "Observation-with-no-evidence" )
+    );
+    expect( noEvidenceButton ).toBeTruthy( );
+    await actor.press( noEvidenceButton );
+    expect( mockNavigate ).toHaveBeenCalledWith( "CameraNavigator", {
+      screen: "ObsEdit",
+      params: { previousScreen: null }
+    } );
+  } );
+} );
