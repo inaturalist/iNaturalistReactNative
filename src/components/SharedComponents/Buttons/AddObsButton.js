a37a3d3d863df501742f61741f38c780c9ce23c2
Johannes Klein <johannes.t.klein@gmail.com>
2024-04-12 22:32:20 +0200

    Refactor GradientButton out of AddObsButton

--- src/components/SharedComponents/Buttons/AddObsButton.js
+++ src/components/SharedComponents/Buttons/AddObsButton.js
@@ -12,6 +12,46 @@ import useStore from "stores/useStore";
 import { dropShadow } from "styles/global";
 import colors from "styles/tailwindColors";
 
+export const GradientButton = ( {
+  onPress,
+  accessibilityHint,
+  iconName,
+  iconSize
+}: {
+  onPress: Function,
+  accessibilityHint: string,
+  iconName: string,
+  iconSize: number
+} ): React.Node => (
+  <Pressable
+    className="w-[69px] h-[69px] rounded-full overflow-hidden"
+    testID="add-obs-button"
+    style={dropShadow}
+    onPress={onPress}
+    disabled={false}
+    accessibilityLabel={t( "Add-observations" )}
+    accessibilityHint={accessibilityHint || t( "Opens-ar-camera" )}
+    accessibilityRole="button"
+    accessibilityState={{
+      disabled: false
+    }}
+  >
+    <LinearGradient
+      colors={[colors.inatGreen, "#297F87"]}
+      angle={156.95}
+      useAngle
+    >
+      <View className="grow aspect-square flex items-center justify-center">
+        <INatIcon
+          name={iconName || "arcamera"}
+          size={iconSize || 37}
+          color={colors.white}
+        />
+      </View>
+    </LinearGradient>
+  </Pressable>
+);
+
 const AddObsButton = (): React.Node => {
   const [showModal, setModal] = React.useState( false );
 
@@ -44,41 +84,14 @@ const AddObsButton = (): React.Node => {
         closeModal={closeModal}
         modal={addObsModal}
       />
-      <Pressable
-        className="w-[69px] h-[69px] rounded-full overflow-hidden"
-        style={dropShadow}
+      <GradientButton
         onPress={isAdvancedUser
           ? openModal
           : navToARCamera}
-        testID="add-obs-button"
-        disabled={false}
-        accessibilityLabel={t( "Add-observations" )}
-        accessibilityHint={isAdvancedUser
-          ? t( "Opens-add-observation-modal" )
-          : t( "Opens-ar-camera" )}
-        accessibilityRole="button"
-        accessibilityState={{
-          disabled: false
-        }}
-      >
-        <LinearGradient
-          colors={[colors.inatGreen, "#297F87"]}
-          angle={156.95}
-          useAngle
-        >
-          <View className="grow aspect-square flex items-center justify-center">
-            <INatIcon
-              name={isAdvancedUser
-                ? "plus"
-                : "arcamera"}
-              size={isAdvancedUser
-                ? 31
-                : 37}
-              color={colors.white}
-            />
-          </View>
-        </LinearGradient>
-      </Pressable>
+        accessibilityHint={isAdvancedUser && t( "Opens-add-observation-modal" )}
+        iconName={isAdvancedUser && "plus"}
+        iconSize={isAdvancedUser && 31}
+      />
     </>
   );
 };
