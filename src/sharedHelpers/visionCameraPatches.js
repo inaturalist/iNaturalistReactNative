b67f3b97329c6d9b1163218ab7407f5631dc5139
Johannes Klein <johannes.t.klein@gmail.com>
2024-04-23 10:33:36 +0200

    Remove one patch

--- src/sharedHelpers/visionCameraPatches.js
+++ src/sharedHelpers/visionCameraPatches.js
@@ -18,16 +18,6 @@ import {
 } from "sharedHooks/useDeviceOrientation";
 
 // Needed for react-native-vision-camera v3.9.0
-// This patch is used to set the pixelFormat prop which should not be needed because the default
-// value would be fine for both platforms.
-// However, on Android for the "native" pixelFormat I could not find any method or properties to
-// transform the frame into a Bitmap which we need for the classifier currently.
-// So we use the "yuv" pixelFormat which is the only one that works for now but less performant.
-export const pixelFormatPatch = () => ( Platform.OS === "ios"
-  ? "native"
-  : "yuv" );
-
-// Needed for react-native-vision-camera v3.9.0
 // This patch is used to determine the orientation prop for the Camera component.
 // On Android, the orientation prop is not used, so we return null.
 // On iOS, the orientation prop is undocumented, but it does get used in a sense that the
