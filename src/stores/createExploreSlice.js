7082cf13a4b540bec50dc617139ea3af17377d28
Amanda Bullington <albullington@gmail.com>
2024-04-25 14:56:58 -0700

    Keep previous explore view when changing filters; closes #1454

--- src/stores/createExploreSlice.js
+++ src/stores/createExploreSlice.js
@@ -1,6 +1,10 @@
 const createExploreSlice = set => ( {
   storedParams: {},
-  setStoredParams: params => set( ( ) => ( { storedParams: params } ) )
+  setStoredParams: params => set( ( ) => ( { storedParams: params } ) ),
+  exploreView: "observations",
+  setExploreView: exploreView => set( ( ) => ( {
+    exploreView
+  } ) )
 } );
 
 export default createExploreSlice;
