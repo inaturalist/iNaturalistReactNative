cf572d78d12e66f93c0353a2ab491c471ab35c1b
Amanda Bullington <albullington@gmail.com>
2024-04-25 16:16:59 -0700

    Save previous map region; update zustand slice with TS; closes #1273

---
+++ src/stores/createExploreSlice.ts
@@ -0,0 +1,33 @@
+import { StateCreator } from "zustand";
+
+const DELTA = 0.2;
+
+export const initialMapRegion = {
+  latitude: 0.0,
+  longitude: 0.0,
+  latitudeDelta: DELTA,
+  longitudeDelta: DELTA
+};
+
+interface MapRegion {
+  latitude: number,
+  longitude: number,
+  latitudeDelta: double,
+  longitudeDelta: double
+}
+
+interface ExploreSlice {
+  storedParams: Object,
+  setStoredParams: ( _params: Object ) => void,
+  mapRegion: MapRegion,
+  setMapRegion: ( _region: MapRegion ) => void
+}
+
+const createExploreSlice: StateCreator<ExploreSlice> = set => ( {
+  storedParams: {},
+  setStoredParams: params => set( ( ) => ( { storedParams: params } ) ),
+  mapRegion: initialMapRegion,
+  setMapRegion: region => set( ( ) => ( { mapRegion: region } ) )
+} );
+
+export default createExploreSlice;
