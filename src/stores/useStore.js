db24b6f662a619c051ca57ef4bf0595c057a481f
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-05-08 16:00:08 -0700

    Minor Zustand cleanup
    * Prevent namespace clobbering: Zustand "slices" apparently do not manage
      different parts of state, they just divide management of the same namespace
      across different files, so if slice A and slice B add the same key and B
      gets added after A, it just clobbers A's version of the key, silently. I
      added a check to make sure we throw an exception if a developer adds a
      non-unique key like this.
    * Rename the resetStore in the createObservationFlow slice because it doesn't
      reset the whole store, just the data that the createObservationFlowSlice
      manages

--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -20,11 +20,45 @@ const zustandStorage: StateStorage = {
 // Using slices to separate store for Explore and Observation creation flow
 // https://docs.pmnd.rs/zustand/guides/slices-pattern
 const useStore = create( persist(
-  ( ...a ) => ( {
-    ...createExploreSlice( ...a ),
-    ...createObservationFlowSlice( ...a ),
-    ...createLayoutSlice( ...a )
-  } ),
+  ( ...args ) => {
+    // Let's make our slices
+    const slices = [
+      createExploreSlice( ...args ),
+      createObservationFlowSlice( ...args ),
+      createLayoutSlice( ...args )
+    ];
+
+    // Now let's make sure they're not clobbering each other because
+    // everything in Zustand state exists in a single namespace and
+    // clobbering can happen silently... which is bad.
+    const allKeys = slices.map( slice => Object.keys( slice ) ).flat( );
+    const keyCounts = allKeys.reduce(
+      ( memo, curr ) => {
+        memo[curr] ||= 0;
+        memo[curr] += 1;
+        return memo;
+      },
+      {}
+    );
+    const nonUniqueKeys = Object.keys( keyCounts ).reduce(
+      ( memo, curr ) => {
+        if ( keyCounts[curr] > 1 ) memo.push( curr );
+        return memo;
+      },
+      []
+    );
+    if ( nonUniqueKeys.length > 0 ) {
+      throw new Error(
+        `You have multiple Zustand slices with the following keys: ${nonUniqueKeys}`
+      );
+    }
+
+    // All good? Now let's combine them into one enormous hideous object
+    return slices.reduce(
+      ( memo, curr ) => ( { ...memo, ...curr } ),
+      {}
+    );
+  },
   {
     name: "persisted-zustand",
     partialize: state => ( {
