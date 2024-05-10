1f7cbf1ba804bad0bdfbf4b58c1c859512a4f730
Amanda Bullington <albullington@gmail.com>
2024-05-10 14:37:30 -0700

    Add mock for useQuery

--- tests/integration/Notifications.test.tsx
+++ tests/integration/Notifications.test.tsx
@@ -20,7 +20,8 @@ jest.mock( "providers/contexts", ( ) => {
     ...originalModule,
     RealmContext: {
       ...originalModule.RealmContext,
-      useRealm: ( ) => global.mockRealms[mockRealmIdentifier]
+      useRealm: ( ) => global.mockRealms[mockRealmIdentifier],
+      useQuery: ( ) => []
     }
   };
 } );
