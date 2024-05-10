de939c676bbb4216dcb8961f2e3f603d6ca3bc50
Johannes Klein <johannes.t.klein@gmail.com>
2024-05-10 10:59:11 +0200

    Add KebabMenu Item component

--- src/components/SharedComponents/KebabMenu.js
+++ src/components/SharedComponents/KebabMenu.js
@@ -3,6 +3,7 @@
 import INatIconButton from "components/SharedComponents/Buttons/INatIconButton";
 import type { Node } from "react";
 import React from "react";
+import { Pressable } from "react-native";
 import { Menu } from "react-native-paper";
 import { useTranslation } from "sharedHooks";
 import colors from "styles/tailwindColors";
@@ -17,6 +18,7 @@ type Props = {
   visible: boolean,
   white?: boolean,
 }
+import Body3 from "./Typography/Body3";
 
 const KebabMenu = ( {
   accessibilityHint,
@@ -62,5 +64,27 @@ const KebabMenu = ( {
     </Menu>
   );
 };
+
+const KebabMenuItem = ( {
+  accessibilityLabel,
+  onPress,
+  testID,
+  title
+} ) => (
+  <Pressable
+    testID={testID}
+    accessibilityRole="button"
+    accessibilityLabel={accessibilityLabel || title}
+    onPress={onPress}
+  >
+    <Body3
+      className="m-[14px] text-black"
+    >
+      {title}
+    </Body3>
+  </Pressable>
+);
+
+KebabMenu.Item = KebabMenuItem;
 
 export default KebabMenu;
