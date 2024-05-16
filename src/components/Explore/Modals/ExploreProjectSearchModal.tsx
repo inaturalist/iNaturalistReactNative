a87573dfd8e58b36a586cdde993e908546378577
Angie Ta <angie@inaturalist.org>
2024-05-15 19:37:44 -0700

    Adding explore user, location and project search modal files

---
+++ src/components/Explore/Modals/ExploreProjectSearchModal.tsx
@@ -0,0 +1,33 @@
+// @flow
+
+import ExploreProjectSearch from "components/Explore/SearchScreens/ExploreProjectSearch";
+import Modal from "components/SharedComponents/Modal";
+import type { Node } from "react";
+import React from "react";
+
+interface Props {
+  showModal: boolean,
+  closeModal: Function,
+  updateProject: Function
+}
+
+const ExploreProjectSearchModal = ( {
+  showModal,
+  closeModal,
+  updateProject
+}: Props ): Node => (
+  <Modal
+    showModal={showModal}
+    fullScreen
+    closeModal={closeModal}
+    disableSwipeDirection
+    modal={(
+      <ExploreProjectSearch
+        closeModal={closeModal}
+        updateProject={updateProject}
+      />
+    )}
+  />
+);
+
+export default ExploreProjectSearchModal;
