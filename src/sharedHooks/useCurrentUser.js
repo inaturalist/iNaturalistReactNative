5087b9adf1d44d6f3ee9f391a1ce35f236d1f7d9
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-05-23 15:29:02 -0700

    Revert changes to useCurrentUser

--- src/sharedHooks/useCurrentUser.js
+++ src/sharedHooks/useCurrentUser.js
@@ -8,15 +8,19 @@ const { useRealm } = RealmContext;
 
 const useCurrentUser = ( ): ?Object => {
   const realm = useRealm( );
-  const currentUserIsValid = User.currentUser( realm )?.isValid( );
+  // TODO there has to be a better way. useMemo isn't doing anything here
+  // because currentUser changes every time User.currentUser() gets called,
+  // resulting in a lot of unnecessary renders and effects getting
+  // triggered... but if you actually memoize the value, you end up
+  // with "Accessing object which has been invalidated or deleted" errors
+  const currentUser = User.currentUser( realm );
   return useMemo( ( ) => {
-    if ( currentUserIsValid ) {
-      return User.currentUser( realm );
+    if ( currentUser?.isValid( ) ) {
+      return currentUser;
     }
     return null;
   }, [
-    currentUserIsValid,
-    realm
+    currentUser
   ] );
 };
 
