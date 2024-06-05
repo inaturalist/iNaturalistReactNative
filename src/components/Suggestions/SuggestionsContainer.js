3b35e072b14e71264ea78a881c62624e6df2b1da
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2024-06-05 12:12:14 -0700

    Default to first photo in Suggestions (#1649)
    * Default to first photo in Suggestions; closes
    
    * Fix tests for default selection on Suggestions

--- src/components/Suggestions/SuggestionsContainer.js
+++ src/components/Suggestions/SuggestionsContainer.js
@@ -10,7 +10,6 @@ import React, {
   useState
 } from "react";
 import ObservationPhoto from "realmModels/ObservationPhoto";
-import Photo from "realmModels/Photo";
 import useStore from "stores/useStore";
 
 import useClearComputerVisionDirectory from "./hooks/useClearComputerVisionDirectory";
@@ -31,12 +30,8 @@ const SuggestionsContainer = ( ): Node => {
   const currentObservation = useStore( state => state.currentObservation );
   const innerPhotos = ObservationPhoto.mapInnerPhotos( currentObservation );
   const photoUris = ObservationPhoto.mapObsPhotoUris( currentObservation );
-  // Ensure that if this URI is a remote thumbnail that we are resizing
-  // a reasonably-sized image and not delivering a handful of
-  // upsampled pixels
-  const [selectedPhotoUri, setSelectedPhotoUri] = useState(
-    Photo.displayMediumPhoto( photoUris[photoUris.length - 1] )
-  );
+
+  const [selectedPhotoUri, setSelectedPhotoUri] = useState( photoUris[0] );
   const [selectedTaxon, setSelectedTaxon] = useState( null );
   const [mediaViewerVisible, setMediaViewerVisible] = useState( false );
   const [isLoading, setIsLoading] = useState( true );
