b97f2926499dfb5e3afab611c1313214c883932a
Johannes Klein <johannes.t.klein@gmail.com>
2024-06-17 15:44:38 +0200

    Testing something

--- e2e/helpers.js
+++ e2e/helpers.js
@@ -12,7 +12,8 @@ export async function iNatE2eBeforeAll( device ) {
 export async function iNatE2eBeforeEach( device ) {
   // device.launchApp would be preferred for an app of our complexity. It does work locally
   // for both, but on CI for Android it does not work. So we use reloadReactNative for Android.
-  if ( device.getPlatform() === "android" ) {
+  // if ( device.getPlatform() === "android" ) {
+  if ( false ) {
     await device.reloadReactNative();
   } else {
     await device.launchApp( {
