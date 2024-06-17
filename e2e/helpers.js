6dd72e65be452e078874c97a7168420fca7040a0
Johannes Klein <johannes.t.klein@gmail.com>
2024-06-17 15:49:03 +0200

    Update helpers.js

--- e2e/helpers.js
+++ e2e/helpers.js
@@ -13,8 +13,12 @@ export async function iNatE2eBeforeEach( device ) {
   // device.launchApp would be preferred for an app of our complexity. It does work locally
   // for both, but on CI for Android it does not work. So we use reloadReactNative for Android.
   // if ( device.getPlatform() === "android" ) {
-  if ( false ) {
-    await device.reloadReactNative();
+  if ( device.getPlatform() === "android" ) {
+    // await device.reloadReactNative();
+    await device.launchApp( {
+      newInstance: true,
+      permissions: { location: "always" }
+    } );
   } else {
     await device.launchApp( {
       newInstance: true,
