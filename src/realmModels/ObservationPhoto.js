753ea5ef2e870a713d555b55b87daa2ea22fe43a
Yaron Budowski <budowski@gmail.com>
2024-06-17 20:24:00 +0200

    Fixes related to tests failing

--- src/realmModels/ObservationPhoto.js
+++ src/realmModels/ObservationPhoto.js
@@ -1,5 +1,4 @@
 import { Realm } from "@realm/react";
-import { getJWT } from "components/LoginSignUp/AuthenticationService";
 import inatjs, { FileUpload } from "inaturalistjs";
 import uuid from "react-native-uuid";
 
@@ -94,6 +93,7 @@ class ObservationPhoto extends Realm.Object {
     );
 
     if ( obsPhotoToDelete ) {
+      const { getJWT } = require( "components/LoginSignUp/AuthenticationService" );
       const apiToken = await getJWT( );
       const options = { api_token: apiToken };
       await inatjs.observation_photos.delete( { id: obsPhotoToDelete.uuid }, options );
