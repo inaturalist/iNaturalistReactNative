80e6beaf6e0a64fd2bd34d0dfcac66fda90466ce
Johannes Klein <johannes.t.klein@gmail.com>
2024-06-27 16:18:30 +0200

    Add function to access local sound, like photo

--- src/stores/createObservationFlowSlice.js
+++ src/stores/createObservationFlowSlice.js
@@ -2,6 +2,7 @@
 import { Realm } from "@realm/react";
 import _ from "lodash";
 import Photo from "realmModels/Photo";
+import Sound from "realmModels/Sound";
 
 const DEFAULT_STATE = {
   cameraRollUris: [],
@@ -46,7 +47,7 @@ const removeObsSoundFromObservation = ( currentObservation, uri ) => {
   if ( obsSounds.length > 0 ) {
     _.remove(
       obsSounds,
-      obsSound => obsSound.sound.file_url === uri
+      obsSound => Sound.accessLocalSound( obsSound.sound.file_url ) === uri
     );
     updatedObservation.observationSounds = obsSounds;
     return [updatedObservation];
