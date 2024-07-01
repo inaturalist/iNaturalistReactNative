1672634ac2c6c4722cd42bb2c75fe40a9f7169ea
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-01 12:32:03 +0200

    Update types

--- src/components/Explore/hooks/useCurrentMapRegion.ts
+++ src/components/Explore/hooks/useCurrentMapRegion.ts
@@ -1,9 +1,10 @@
 import { useNavigationState } from "@react-navigation/native";
 import _ from "lodash";
 import { useCallback } from "react";
+import { Region } from "react-native-maps";
 import useStore from "stores/useStore";
 
-const useCurrentMapRegion = ( ): Object => {
+const useCurrentMapRegion = ( ) => {
   const mapRegion = useStore( s => s.mapRegion );
   const setMapRegion = useStore( s => s.setMapRegion );
   const rootMapRegion = useStore( s => s.rootMapRegion );
@@ -15,7 +16,7 @@ const useCurrentMapRegion = ( ): Object => {
     ? rootMapRegion
     : mapRegion;
 
-  const setCurrentMapRegion = useCallback( newView => {
+  const setCurrentMapRegion = useCallback( ( newView: Region ) => {
     if ( currentScreen === "RootExplore" ) {
       setRootMapRegion( newView );
     } else {
