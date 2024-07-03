6d6dbb4d9e4669db033b4a5724cf15cd39762884
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-03 12:42:10 +0200

    Query for map bounding box for given taxon_id and display

--- src/components/Explore/MapView.tsx
+++ src/components/Explore/MapView.tsx
@@ -1,13 +1,16 @@
+import { searchObservations } from "api/observations";
 import classnames from "classnames";
 import {
   Body1,
   Button,
   Map
 } from "components/SharedComponents";
+import { getMapRegion } from "components/SharedComponents/Map/helpers/mapHelpers.ts";
 import { View } from "components/styledComponents";
 import React, { useState } from "react";
 import { Platform } from "react-native";
 import { useDebugMode, useTranslation } from "sharedHooks";
+import useAuthenticatedQuery from "sharedHooks/useAuthenticatedQuery";
 import { getShadowForColor } from "styles/global";
 import colors from "styles/tailwindColors";
 
@@ -44,6 +47,29 @@ const MapView = ( {
     startAtNearby,
     updateMapBoundaries
   } = useMapLocation( );
+
+  const [storedBoundingBoxes, setStoredBoundingBoxes] = useState( {} );
+  const obsParams = {
+    ...queryParams, return_bounds: true, per_page: 0
+  };
+  const {
+    data
+  } = useAuthenticatedQuery(
+    ["fetchTaxonBoundingBox"],
+    optsWithAuth => searchObservations( obsParams, optsWithAuth )
+  );
+  // Only update the map once per taxon_id, so that it only zooms to the
+  // bounding box on initial load
+  if ( !storedBoundingBoxes[obsParams.taxon_id] ) {
+    if ( data && data.total_bounds && data.total_bounds.nelat !== undefined ) {
+      const boundsRegion = getMapRegion( data.total_bounds );
+      updateMapBoundaries( boundsRegion );
+      setStoredBoundingBoxes( {
+        ...storedBoundingBoxes,
+        [obsParams.taxon_id]: boundsRegion
+      } );
+    }
+  }
 
   const tileMapParams = {
     ...queryParams
