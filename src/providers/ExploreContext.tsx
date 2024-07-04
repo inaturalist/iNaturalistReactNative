59fb74b981f417d2cef8e71fcb51fecaf9f98b8d
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-04 11:22:27 +0200

    Update types

--- src/providers/ExploreContext.tsx
+++ src/providers/ExploreContext.tsx
@@ -255,11 +255,17 @@ const ExploreContext = React.createContext<
   {
     state: State;
     dispatch: Dispatch;
+    makeSnapshot(): void;
     differsFromSnapshot: boolean;
     isNotInitialState: boolean;
     discardChanges(): void;
     numberOfFilters: number;
-    defaultExploreLocation(): Promise<{place_guess: string}>;
+    defaultExploreLocation(): Promise<{
+      place_guess: string;
+      lat?: number;
+      lng?: number;
+      radius?: number;
+    }>;
       } | undefined>( undefined );
 
 // Every key in this object represents a numbered filter in the UI
