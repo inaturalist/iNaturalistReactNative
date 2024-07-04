b4d10d8c8e37c7a4e3b5e7eea23825bbb255d81a
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-04 09:37:48 +0200

    Actually, let's patch the radio button to not have intrinsic margin, that makes everything easy peasy

--- src/components/SharedComponents/RadioButtonRow.js
+++ src/components/SharedComponents/RadioButtonRow.js
@@ -43,26 +43,20 @@ const RadioButtonRow = ( {
 
   return (
     <Pressable testID={testID} accessibilityRole="button" onPress={onPress}>
-      {/*
-        RadioButton.Android has a built-in margin of 8, so we have to add a padding
-        of 4 to the left of the text container to match the design of distance of 12 between
-        button and text.
-      */}
       <View className="flex-row items-center">
-        {/* Essentially this element has m-2 built-in */}
         <RadioButton.Android
           onPress={onPress}
           value={value}
           status={status}
           accessibilityLabel={label}
         />
-        <View className="ml-1 flex-row">
+        <View className="ml-3 flex-row">
           <Label className="mr-[10px]">{label}</Label>
           {icon && <INatIcon name={icon} size={19} color={theme.colors.secondary} />}
         </View>
       </View>
       {description && (
-        <List2 className="ml-[41px] mr-[33px] py-1">{description}</List2>
+        <List2 className="ml-[32px] mr-[33px]">{description}</List2>
       )}
     </Pressable>
   );
