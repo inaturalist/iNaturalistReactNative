8cbad8a7b97f03d0fac9bb544b24071956ba07be
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-09 09:14:32 +0200

    Add callbacks to useLocationPermission hook

--- src/sharedHooks/useLocationPermission.tsx
+++ src/sharedHooks/useLocationPermission.tsx
@@ -9,6 +9,13 @@ import {
   RESULTS
 } from "react-native-permissions";
 
+interface LocationPermissionCallbacks {
+  onPermissionGranted: ( ) => void;
+  onPermissionDenied: ( ) => void;
+  onPermissionBlocked: ( ) => void;
+  onModalHide: ( ) => void;
+}
+
 /**
    * A hook to check and request location permissions.
    * @returns {boolean} hasPermissions - Undefined if permissions have not been checked yet.
@@ -17,29 +24,43 @@ import {
    * @returns {Function} requestPermissions - A function to request location permissions.
    * Essentially just a wrapper around toggling permissionNeeded for the LocationPermissionGate.
    */
-const useLocationPermission = () => {
+const useLocationPermission = ( ) => {
   const [hasPermissions, setHasPermissions] = useState<boolean>( );
   const [showPermissionGate, setShowPermissionGate] = useState( false );
 
-  const renderPermissionsGate = () => (
-    <LocationPermissionGate
-      permissionNeeded={showPermissionGate}
-      withoutNavigation
-      onModalHide={( ) => setShowPermissionGate( false )}
-      onPermissionGranted={( ) => {
-        setShowPermissionGate( false );
-        setHasPermissions( true );
-      }}
-      onPermissionDenied={( ) => {
-        setShowPermissionGate( false );
-        setHasPermissions( false );
-      }}
-      onPermissionBlocked={( ) => {
-        setShowPermissionGate( false );
-        setHasPermissions( false );
-      }}
-    />
-  );
+  const renderPermissionsGate = ( callbacks?: LocationPermissionCallbacks ) => {
+    const {
+      onPermissionGranted,
+      onPermissionDenied,
+      onPermissionBlocked,
+      onModalHide
+    } = callbacks || { };
+    return (
+      <LocationPermissionGate
+        permissionNeeded={showPermissionGate}
+        withoutNavigation
+        onModalHide={( ) => {
+          setShowPermissionGate( false );
+          if ( onModalHide ) onModalHide( );
+        }}
+        onPermissionGranted={( ) => {
+          setShowPermissionGate( false );
+          setHasPermissions( true );
+          if ( onPermissionGranted ) onPermissionGranted( );
+        }}
+        onPermissionDenied={( ) => {
+          setShowPermissionGate( false );
+          setHasPermissions( false );
+          if ( onPermissionDenied ) onPermissionDenied( );
+        }}
+        onPermissionBlocked={( ) => {
+          setShowPermissionGate( false );
+          setHasPermissions( false );
+          if ( onPermissionBlocked ) onPermissionBlocked( );
+        }}
+      />
+    );
+  };
 
   function requestPermissions() {
     setShowPermissionGate( true );
