12c6fd9a0360d319c42924069a9c2e00cf5ee69c
Johannes Klein <johannes.t.klein@gmail.com>
2024-07-09 11:15:10 +0200

    Add helper function to show place text in Explore

---
+++ src/components/Explore/helpers/placeGuessText.ts
@@ -0,0 +1,25 @@
+import { PLACE_MODE } from "providers/ExploreContext.tsx";
+
+// eslint-disable-next-line max-len
+function placeGuessText( placeMode: PLACE_MODE, t: ( _key: string ) => string, exploreStatePlaceGuess: string ): string {
+  let placeGuess = "";
+  switch ( placeMode ) {
+    case PLACE_MODE.NEARBY:
+      placeGuess = t( "Nearby" );
+      break;
+    case PLACE_MODE.WORLDWIDE:
+      placeGuess = t( "Worldwide" );
+      break;
+    case PLACE_MODE.MAP_AREA:
+      placeGuess = t( "Map-Area" );
+      break;
+    case PLACE_MODE.PLACE:
+      placeGuess = exploreStatePlaceGuess;
+      break;
+    default:
+      break;
+  }
+  return placeGuess;
+}
+
+export default placeGuessText;
