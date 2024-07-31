206c200850c2aacf8a295b6d839525735525244b
Amanda Bullington <albullington@gmail.com>
2024-07-31 15:03:30 -0700

    Stop location fetch when nav to location picker; let useWatchPosition handle acc

--- src/sharedHelpers/shouldFetchObservationLocation.ts
+++ src/sharedHelpers/shouldFetchObservationLocation.ts
@@ -1,9 +1,6 @@
 import { galleryPhotosPath } from "appConstants/paths.ts";
 import RNFS from "react-native-fs";
 import { RealmObservation } from "realmModels/types.d.ts";
-import {
-  TARGET_POSITIONAL_ACCURACY
-} from "sharedHooks/useWatchPosition.ts";
 
 const shouldFetchObservationLocation = ( observation: RealmObservation ) => {
   const latitude = observation?.latitude;
@@ -20,14 +17,10 @@ const shouldFetchObservationLocation = ( observation: RealmObservation ) => {
     !observation?._created_at
     && !observation?._synced_at
   );
-  const accGoodEnough = (
-    observation?.positional_accuracy
-    && observation.positional_accuracy <= TARGET_POSITIONAL_ACCURACY
-  );
 
   return observation
     && isNewObservation
-    && ( !hasLocation || !accGoodEnough )
+    && !hasLocation
     && !isGalleryPhoto
     && !isSharedPhoto;
 };
