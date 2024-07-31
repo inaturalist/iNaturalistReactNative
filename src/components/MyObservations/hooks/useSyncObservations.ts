283b59cb45309bdc6a6d536a1adbcf341a283fda
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2024-07-31 12:17:25 -0700

    Keep app awake during bulk uploads (#1898)
    * Keep app awake while uploading; closes #1893
    
    * Only activate keep awake during manual sync, not automatic sync

--- src/components/MyObservations/hooks/useSyncObservations.ts
+++ src/components/MyObservations/hooks/useSyncObservations.ts
@@ -1,6 +1,7 @@
 import {
   useNetInfo
 } from "@react-native-community/netinfo";
+import { deactivateKeepAwake } from "@sayem314/react-native-keep-awake";
 import { INatApiError } from "api/error";
 import { deleteRemoteObservation } from "api/observations";
 import { RealmContext } from "providers/contexts";
@@ -162,6 +163,8 @@ const useSyncObservations = ( currentUserId, uploadObservations ): Object => {
     // being offline, so we're not checking internet connectivity here
     if ( loggedIn ) {
       await uploadObservations( );
+    } else {
+      deactivateKeepAwake( );
     }
     completeSync( );
   }, [
