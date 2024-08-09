fb9f8cfb413cf000b6054a60889d5b163409d1c4
Angie Ta <angie@inaturalist.org>
2024-08-08 22:30:14 -0700

    Location name wrap on obs cards

--- src/components/SharedComponents/ObservationLocation.js
+++ src/components/SharedComponents/ObservationLocation.js
@@ -108,7 +108,6 @@ const ObservationLocation = ( {
     <View className="flex-col space-y-[11px]">
       <Body4
         className="text-darkGray"
-        numberOfLines={1}
         ellipsizeMode="tail"
       >
         {displayLocation}
