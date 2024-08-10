0b2a9bddea051bf8ca8f275c6ec9aab628e61434
Amanda Bullington <albullington@gmail.com>
2024-08-09 23:49:09 -0700

    More mocks

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -80,86 +80,6 @@ jest.mock( "react-native/Libraries/LogBox/LogBox" );
 
 jest.mock( "react-native-device-info", () => mockRNDeviceInfo );
 
-jest.mock( "react-native-sensitive-info", () => {
-  class RNSInfo {
-    static stores = new Map();
-
-    static getServiceName( o = {} ) {
-      return o.sharedPreferencesName
-        || o.keychainService
-        || "default";
-    }
-
-    static validateString( s ) {
-      if ( typeof s !== "string" ) { throw new Error( "Invalid string:", s ); }
-    }
-
-    static getItem = jest.fn( async ( k, o ) => {
-      RNSInfo.validateString( k );
-
-      const serviceName = RNSInfo.getServiceName( o );
-      const service = RNSInfo.stores.get( serviceName );
-
-      if ( service ) { return service.get( k ) || null; }
-      return null;
-    } );
-
-    static getAllItems = jest.fn( async o => {
-      const serviceName = RNSInfo.getServiceName( o );
-      const service = RNSInfo.stores.get( serviceName );
-      let mappedValues = [];
-
-      if ( service?.size ) {
-        // for ( const [k, v] of service.entries() ) {
-        //   mappedValues.push( { key: k, value: v, service: serviceName } );
-        // }
-        mappedValues = service.entries( ).map(
-          ( key, value ) => ( { key, value, service: serviceName } )
-        );
-      }
-
-      return mappedValues;
-    } );
-
-    static setItem = jest.fn( async ( k, v, o ) => {
-      RNSInfo.validateString( k );
-      RNSInfo.validateString( v );
-
-      const serviceName = RNSInfo.getServiceName( o );
-      let service = RNSInfo.stores.get( serviceName );
-
-      if ( !service ) {
-        RNSInfo.stores.set( serviceName, new Map() );
-        service = RNSInfo.stores.get( serviceName );
-      }
-
-      service.set( k, v );
-
-      return null;
-    } );
-
-    static deleteItem = jest.fn( async ( k, o ) => {
-      RNSInfo.validateString( k );
-
-      const serviceName = RNSInfo.getServiceName( o );
-      const service = RNSInfo.stores.get( serviceName );
-
-      if ( service ) { service.delete( k ); }
-
-      return null;
-    } );
-
-    static hasEnrolledFingerprints = jest.fn( async () => true );
-
-    static setInvalidatedByBiometricEnrollment = jest.fn();
-
-    // "Touch ID" | "Face ID" | false
-    static isSensorAvailable = jest.fn( async () => "Face ID" );
-  }
-
-  return RNSInfo;
-} );
-
 // Some test environments may need a little more time
 jest.setTimeout( 50000 );
 
@@ -234,18 +154,6 @@ jest.mock( "inaturalistjs" );
 inatjs.observations.search.mockResolvedValue( makeResponse( ) );
 inatjs.observations.updates.mockResolvedValue( makeResponse( ) );
 
-// const mockErrorHandler = error => {
-//   console.log( error );
-// };
-// jest.mock( "react-native-exception-handler", () => ( {
-//   setJSExceptionHandler: jest
-//     .fn()
-//     .mockImplementation( () => mockErrorHandler() ),
-//   setNativeExceptionHandler: jest
-//     .fn()
-//     .mockImplementation( () => mockErrorHandler() )
-// } ) );
-
 // Set up mocked fetch for testing (or disabling) fetch requests
 fetchMock.enableMocks( );
 fetchMock.dontMock( );
@@ -260,16 +168,6 @@ jest.mock( "jsrsasign" );
 
 inatjs.announcements.search.mockResolvedValue( makeResponse( ) );
 inatjs.observations.updates.mockResolvedValue( makeResponse( ) );
-
-jest.mock( "react-native-webview", () => {
-  const MockWebView = jest.requireActual( "react-native" ).View;
-
-  return {
-    __esModule: true,
-    WebView: MockWebView,
-    default: MockWebView
-  };
-} );
 
 jest.mock( "react-native/Libraries/TurboModule/TurboModuleRegistry", () => {
   const turboModuleRegistry = jest
