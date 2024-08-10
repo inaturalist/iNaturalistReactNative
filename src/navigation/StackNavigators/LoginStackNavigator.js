fb005a19f3cca50e6828d5d25dff5357c4341a54
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-08-09 17:09:46 -0700

    Restore FullPageWebView functionality to signup screens

--- src/navigation/StackNavigators/LoginStackNavigator.js
+++ src/navigation/StackNavigators/LoginStackNavigator.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FullPageWebView from "components/FullPageWebView/FullPageWebView.tsx";
 import ForgotPassword from "components/LoginSignUp/ForgotPassword";
 import LicensePhotos from "components/LoginSignUp/LicensePhotos";
 import Login from "components/LoginSignUp/Login.tsx";
@@ -9,7 +10,8 @@ import SignUp from "components/LoginSignUp/SignUp";
 import SignUpConfirmation from "components/LoginSignUp/SignUpConfirmation";
 import { CloseButton } from "components/SharedComponents";
 import {
-  hideHeaderLeft
+  hideHeaderLeft,
+  showSimpleCustomHeader
 } from "navigation/navigationOptions";
 import type { Node } from "react";
 import React from "react";
@@ -34,31 +36,39 @@ const LOGIN_SCREEN_OPTIONS = {
 };
 
 const LoginStackNavigator = ( ): Node => (
-  <Stack.Navigator
-    screenOptions={LOGIN_SCREEN_OPTIONS}
-  >
+  <Stack.Navigator>
     <Stack.Screen
       name="Login"
       component={Login}
       options={{
+        ...LOGIN_SCREEN_OPTIONS,
         ...hideHeaderLeft
       }}
     />
     <Stack.Screen
       name="SignUp"
       component={SignUp}
+      options={LOGIN_SCREEN_OPTIONS}
     />
     <Stack.Screen
       name="ForgotPassword"
       component={ForgotPassword}
+      options={LOGIN_SCREEN_OPTIONS}
     />
     <Stack.Screen
       name="LicensePhotos"
       component={LicensePhotos}
+      options={LOGIN_SCREEN_OPTIONS}
     />
     <Stack.Screen
       name="SignUpConfirmation"
       component={SignUpConfirmation}
+      options={LOGIN_SCREEN_OPTIONS}
+    />
+    <Stack.Screen
+      name="FullPageWebView"
+      component={FullPageWebView}
+      options={showSimpleCustomHeader}
     />
   </Stack.Navigator>
 );
