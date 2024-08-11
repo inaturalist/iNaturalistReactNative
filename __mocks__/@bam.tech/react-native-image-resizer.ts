cf70a4358a4342b74f67c8c0eec0771d18a2cfc2
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2024-08-11 11:27:52 -0700

    Move modules from jest setup to __mocks__ (#1949)
    * Get a few mocks working in __mocks__ folder
    
    * Move more mocks into __mocks__
    
    * Move mocks
    
    * Move zustand mock
    
    * Add more mocks to __mocks__
    
    * More mocks
    
    * Move more files to __mocks__ and audit existing mocks
    
    * Mock react navigation in __mocks__
    
    * Restore test to help with imports after jest env torn down
    
    * Add fake timer to integration test with userEvent
    
    * Add RN paper mock

---
+++ __mocks__/@bam.tech/react-native-image-resizer.ts
@@ -0,0 +1,18 @@
+import mockNodePath from "path";
+
+export default ( {
+  createResizedImage: jest.fn(
+    async (
+      path,
+      _maxWidth,
+      _maxHeight,
+      _compressFormat,
+      _quality,
+      _rotation,
+      outputPath
+    ) => {
+      const filename = mockNodePath.basename( path );
+      return { uri: mockNodePath.join( outputPath, filename ) };
+    }
+  )
+} );
---
+++ __mocks__/react-native-exif-reader.ts
@@ -0,0 +1,2 @@
+export const readExif = jest.fn( );
+export const writeLocation = jest.fn( );
