416161c9d73cbd427cbca0538c0d572d83af3feb
Johannes Klein <johannes.t.klein@gmail.com>
2024-09-25 22:35:16 +0200

    Add test ID

--- src/components/Camera/Buttons/TakePhoto.tsx
+++ src/components/Camera/Buttons/TakePhoto.tsx
@@ -47,6 +47,7 @@ const TakePhoto = ( {
       accessibilityState={{ disabled }}
       disabled={disabled}
       style={DROP_SHADOW}
+      testID="take-photo-button"
     >
       {showPrediction
         ? (
