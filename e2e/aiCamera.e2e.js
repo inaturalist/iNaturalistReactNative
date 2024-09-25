a382f69084b65a2eff192c0dbd88d5cc40e01d6b
Johannes Klein <johannes.t.klein@gmail.com>
2024-09-25 22:25:24 +0200

    Basic test that starts to open the camera

---
+++ e2e/aiCamera.e2e.js
@@ -0,0 +1,44 @@
+import {
+  by, device, element, expect, waitFor
+} from "detox";
+
+import { iNatE2eBeforeAll, iNatE2eBeforeEach } from "./helpers";
+
+describe( "AICamera", () => {
+  beforeAll( async () => iNatE2eBeforeAll( device ) );
+  beforeEach( async () => iNatE2eBeforeEach( device ) );
+
+  it( "should work", async () => {
+    const loginText = element( by.id( "log-in-to-iNaturalist-button.text" ) );
+    await waitFor( loginText ).toBeVisible().withTimeout( 10000 );
+    await expect( loginText ).toBeVisible();
+
+    /*
+    / 2. Switch UI to power user mode
+    */
+    const drawerButton = element( by.id( "OPEN_DRAWER" ) );
+    await waitFor( drawerButton ).toBeVisible().withTimeout( 10000 );
+    await drawerButton.tap();
+    // Tap the settings drawer menu item
+    const settingsDrawerMenuItem = element( by.id( "settings" ) );
+    await waitFor( settingsDrawerMenuItem ).toBeVisible().withTimeout( 10000 );
+    await settingsDrawerMenuItem.tap();
+    // Tap the settings radio button for power user mode
+    const powerUserRadioButton = element( by.id( "all-observation-option" ) );
+    await waitFor( powerUserRadioButton ).toBeVisible().withTimeout( 10000 );
+    await powerUserRadioButton.tap();
+
+    // Open AICamera
+    const addObsButton = element( by.id( "add-obs-button" ) );
+    await waitFor( addObsButton ).toBeVisible().withTimeout( 10000 );
+    await addObsButton.tap();
+    const arCameraButton = element( by.id( "arcamera-button" ) );
+    await waitFor( arCameraButton ).toBeVisible().withTimeout( 10000 );
+    await arCameraButton.tap();
+
+    // Take a photo
+    // const takePhotoButton = element( by.id( "take-photo-button" ) );
+    // await waitFor( takePhotoButton ).toBeVisible().withTimeout( 10000 );
+    // await takePhotoButton.tap();
+  } );
+} );
