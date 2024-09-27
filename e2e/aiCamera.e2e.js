ed8f00f977afdf2526c57b198d1bab878a29cc93
Johannes Klein <johannes.t.klein@gmail.com>
2024-09-27 12:42:06 +0200

    Check for model is loaded text

--- e2e/aiCamera.e2e.js
+++ e2e/aiCamera.e2e.js
@@ -43,6 +43,10 @@ describe( "AICamera", () => {
       by.text( "Loading iNaturalist's AI Camera" )
     );
     await waitFor( loadingText ).toBeVisible().withTimeout( 10000 );
+    const scanText = element(
+      by.text( "Scan the area around you for organisms." )
+    );
+    await waitFor( scanText ).toBeVisible().withTimeout( 10000 );
     const takePhotoButton = element( by.id( "take-photo-button" ) );
     await waitFor( takePhotoButton ).toBeVisible().withTimeout( 10000 );
     await takePhotoButton.tap();
