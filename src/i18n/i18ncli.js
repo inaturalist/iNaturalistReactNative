186237a9fc0b75d9f7d4f826d07b30628825083a
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-10-03 11:11:30 -0700

    Removed debugging

--- src/i18n/i18ncli.js
+++ src/i18n/i18ncli.js
@@ -305,22 +305,16 @@ async function untranslatable( ) {
 async function normalizeFileNames( ) {
   const paths = await l10nFtlPaths();
   return Promise.all( paths.map( async l10nPath => {
-    console.log( "[DEBUG i18ncli.js] normalizing ", l10nPath );
     const locale = path.basename( l10nPath, ".ftl" );
     const [lng, region] = locale.split( "-" );
     // No need to move anything if there's no region
-    if ( !region ) {
-      console.log( "[DEBUG i18ncli.js]   no region, skipping" );
-      return;
-    }
+    if ( !region ) return;
+
     // We need to keep some regions
-    if ( SUPPORTED_REGIONAL_LOCALES.indexOf( locale ) >= 0 ) {
-      console.log( "[DEBUG i18ncli.js]   region supported, skipping" );
-      return;
-    }
+    if ( SUPPORTED_REGIONAL_LOCALES.indexOf( locale ) >= 0 ) return;
+
     // Everything else needs to be regionless
     const newPath = path.join( path.dirname( l10nPath ), `${lng}.ftl` );
-    console.log( "[DEBUG i18ncli.js]   moving to ", newPath );
     await fsp.rename( l10nPath, newPath );
   } ) );
 }
