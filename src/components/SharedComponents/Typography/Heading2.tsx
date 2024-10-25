e767e89dd9a3da1b93ae02658a512610eff016f3
Angie <angie@inaturalist.org>
2024-10-24 19:51:10 -0700

    Fix: Prevents permission gate text from flowing off screen with large font sizes. Closes #2136. (#2292)

--- src/components/SharedComponents/Typography/Heading2.tsx
+++ src/components/SharedComponents/Typography/Heading2.tsx
@@ -5,16 +5,27 @@ import classnames from "classnames";
 import React from "react";
 import { Text, TextProps } from "react-native";
 
-const Heading2 = ( props: TextProps ) => (
-  <Text
-    maxFontSizeMultiplier={2}
-    className={classnames(
-      "text-2xl text-darkGray",
-      tailwindFontBold
-    )}
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    {...props}
-  />
-);
+interface OwnProps {
+  maxFontSizeMultiplier?: number;
+}
+
+type CustomTextProps = TextProps & OwnProps;
+
+const Heading2 = ( props:CustomTextProps ) => {
+  const {
+    maxFontSizeMultiplier = 2
+  } = props;
+  return (
+    <Text
+      maxFontSizeMultiplier={maxFontSizeMultiplier}
+      className={classnames(
+        "text-2xl text-darkGray",
+        tailwindFontBold
+      )}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    />
+  );
+};
 
 export default Heading2;
