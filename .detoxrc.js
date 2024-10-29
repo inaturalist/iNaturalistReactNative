b5c552a876cee991a87e1d9bb200e866299c652a
Johannes Klein <johannes.t.klein@gmail.com>
2024-10-29 23:28:52 +0100

    Revert "Update .detoxrc.js"
    This reverts commit 0a5400963db73af7e738b33c72a4918ef5e23556.

--- .detoxrc.js
+++ .detoxrc.js
@@ -52,7 +52,7 @@ module.exports = {
     simulator: {
       type: "ios.simulator",
       device: {
-        type: "iPhone 16 Pro",
+        type: "iPhone 15 Pro",
       },
     },
     emulator: {
