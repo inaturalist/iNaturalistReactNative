3def5183aac18fbac16b75e79a3b0b0fd436c5a4
Ken-ichi <kenichi.ueda@gmail.com>
2024-11-06 11:48:56 -0800

    test: wait for OTHER SUGGESTIONS in aicamera test (#2375)
    * test: wait for OTHER SUGGESTIONS in aicamera test in an attempt to deflake
    * test: try a longer timeout in aicamera test, try launching app again if it fails

--- e2e/helpers.js
+++ e2e/helpers.js
@@ -19,28 +19,37 @@ export async function iNatE2eBeforeEach( device ) {
   // for both, but on CI for Android it does not work. So we use reloadReactNative for Android.
   if ( device.getPlatform() === "android" ) {
     await device.reloadReactNative();
-  } else {
-    await device.launchApp( {
-      newInstance: true,
-      permissions: {
-        location: "always",
-        camera: "YES",
-        medialibrary: "YES",
-        photos: "YES"
-      }
-    } );
-    // disable password autofill
-    execSync(
-      // eslint-disable-next-line max-len
-      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles/UserSettings.plist`
-    );
-    execSync(
-      // eslint-disable-next-line max-len
-      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/EffectiveUserSettings.plist`
-    );
-    execSync(
-      // eslint-disable-next-line max-len
-      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/PublicInfo/PublicEffectiveUserSettings.plist`
-    );
+    return;
   }
+  const launchAppOptions = {
+    newInstance: true,
+    permissions: {
+      location: "always",
+      camera: "YES",
+      medialibrary: "YES",
+      photos: "YES"
+    }
+  };
+  try {
+    await device.launchApp( launchAppOptions );
+  } catch ( launchAppError ) {
+    if ( !launchAppError.message.match( /unexpectedly disconnected/ ) ) {
+      throw launchAppError;
+    }
+    // Try it one more time
+    await device.launchApp( launchAppOptions );
+  }
+  // disable password autofill
+  execSync(
+    // eslint-disable-next-line max-len
+    `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles/UserSettings.plist`
+  );
+  execSync(
+    // eslint-disable-next-line max-len
+    `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/EffectiveUserSettings.plist`
+  );
+  execSync(
+    // eslint-disable-next-line max-len
+    `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/PublicInfo/PublicEffectiveUserSettings.plist`
+  );
 }
