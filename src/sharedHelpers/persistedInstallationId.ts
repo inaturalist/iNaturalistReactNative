d5923e641fd267c0bd66851770b676f2e2220870
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2024-11-07 11:51:02 -0800

    refactor: give the install MMKV instance a more accurate name

--- src/sharedHelpers/persistedInstallationId.ts
+++ src/sharedHelpers/persistedInstallationId.ts
@@ -1,21 +1,39 @@
+import RNFS from "react-native-fs";
 import { MMKV } from "react-native-mmkv";
 import uuid from "react-native-uuid";
 
+const MMKV_ID = "install-data";
+
 const INSTALL_ID = "installID";
 
-// 20240729 amanda - we're keeping this MMKV instance separate from the
-// one wrapping zustand because we want to be able to persist the installation ID
-// even if a user logs out of the app. this means on signout, we're clearing
-// the storage for the MMKV instance wrapping zustand but leaving this one untouched
-const persistedInstallationId = new MMKV( {
-  id: "user-data"
-} );
-export default persistedInstallationId;
+// This store is separate from the zustand store b/c it needs to survive sign
+// out, i.e these values should remain untill the app is uninstalled
+const store = new MMKV( { id: MMKV_ID } );
 
+// Migrate old MMKV data if it exists. This data is explicitly *not*
+// linked to a user
+const LEGACY_MMKV_ID = "user-data";
+const legacyStore = new MMKV( { id: LEGACY_MMKV_ID } );
+if ( legacyStore.getAllKeys().length > 0 ) {
+  // Migrate data if present
+  legacyStore.getAllKeys().forEach( key => {
+    store.set( key, legacyStore.getString( key ) );
+  } );
+  // Delete the old data on disk so we never have to do this again
+  RNFS.readDir( `${RNFS.DocumentDirectoryPath}/mmkv` ).then( contents => {
+    contents.forEach( item => {
+      if ( item.path.match( new RegExp( `/${LEGACY_MMKV_ID}` ) ) ) {
+        RNFS.unlink( item.path );
+      }
+    } );
+  } );
+}
+
+// eslint-disable-next-line import/prefer-default-export
 export function installID( ) {
-  const id = persistedInstallationId.getString( INSTALL_ID );
+  const id = store.getString( INSTALL_ID );
   if ( id ) return id;
   const newID: string = uuid.v4( ).toString( );
-  persistedInstallationId.set( INSTALL_ID, newID );
+  store.set( INSTALL_ID, newID );
   return newID;
 }
