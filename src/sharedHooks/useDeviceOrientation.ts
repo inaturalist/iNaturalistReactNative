c6a09a2a8cd4807230756410fbdb37e3ff4827bd
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2024-11-13 16:24:08 -0800

    Refactor to begin simplifying device orientation/rotation code (#2422)
    * Refactor useDeviceOrientation to remove extra useEffects
    
    * Rotation code cleanup
    
    * Update mock with useDeviceOrientationChange

--- src/sharedHooks/useDeviceOrientation.ts
+++ src/sharedHooks/useDeviceOrientation.ts
@@ -1,14 +1,19 @@
 import {
-  useCallback, useEffect, useState
+  useMemo, useState
 } from "react";
 import { Dimensions } from "react-native";
 import DeviceInfo from "react-native-device-info";
-import Orientation, { OrientationType } from "react-native-orientation-locker";
+import {
+  OrientationType,
+  useDeviceOrientationChange
+} from "react-native-orientation-locker";
 
 export const LANDSCAPE_LEFT = "landscape-left" as const;
 export const LANDSCAPE_RIGHT = "landscape-right" as const;
 export const PORTRAIT = "portrait" as const;
 export const PORTRAIT_UPSIDE_DOWN = "portrait-upside-down" as const;
+
+const { width, height } = Dimensions.get( "screen" );
 
 export function orientationLockerToIosOrientation( orientation: OrientationType ): string {
   // react-native-orientation-locker and react-native-vision-camera  different
@@ -27,66 +32,43 @@ export function orientationLockerToIosOrientation( orientation: OrientationType 
 }
 
 const useDeviceOrientation = ( ) => {
-  const { width, height } = Dimensions.get( "screen" );
-  const [screenWidth, setScreenWidth] = useState( width );
-  const [screenHeight, setScreenHeight] = useState( height );
-
   const [deviceOrientation, setDeviceOrientation] = useState<string>( );
 
-  useEffect( ( ) => {
-    // Word of caution: getInitialOrientation gets the orientation when JS
-    // started, so it's almost always PORTRAIT; getOrientation gets the
-    // orientation the app thinks it's in, which should be PORTRAIT on
-    // phones; getDeviceOrientation gets the actual current orientation of
-    // the device, which is what we want here for the initial value
-    Orientation.getDeviceOrientation( newOrientation => {
-      setDeviceOrientation( orientationLockerToIosOrientation( newOrientation ) );
-    } );
-  }, [] );
-
-  const isLandscapeMode = [LANDSCAPE_LEFT, LANDSCAPE_RIGHT].includes( deviceOrientation );
-  const isTablet = DeviceInfo.isTablet();
-  // detect device rotation instead of using screen orientation change
-  const onDeviceRotation = useCallback(
-    ( orientation: OrientationType ) => {
+  useDeviceOrientationChange( ( orientation: OrientationType ) => {
     // FACE-UP and FACE-DOWN could be portrait or landscape, I guess the
     // device can't tell, so I'm just not changing the layout at all for
     // those. ~~~ kueda 20230420
-      if ( orientation === "FACE-UP" || orientation === "FACE-DOWN" ) {
-        return;
-      }
-      setDeviceOrientation( orientationLockerToIosOrientation( orientation ) );
-    },
-    [setDeviceOrientation]
-  );
-
-  useEffect( () => {
-    Orientation.addDeviceOrientationListener( onDeviceRotation );
-
-    return () => {
-      Orientation?.removeOrientationListener( onDeviceRotation );
-    };
+    if ( orientation === "FACE-UP" || orientation === "FACE-DOWN" ) {
+      return;
+    }
+    const newOrientation = orientationLockerToIosOrientation( orientation );
+    if ( deviceOrientation === newOrientation ) {
+      return;
+    }
+    setDeviceOrientation( newOrientation );
   } );
 
-  useEffect( ( ) => {
-    // it doesn't seem like Dimensions changes the width immediately on device rotation
-    // so doing this manually
-    if ( isLandscapeMode ) {
-      setScreenWidth( Math.max( width, height ) );
-      setScreenHeight( Math.min( width, height ) );
-    } else {
-      setScreenWidth( Math.min( width, height ) );
-      setScreenHeight( Math.max( width, height ) );
-    }
-  }, [isLandscapeMode, width, height] );
+  const orientation = useMemo( ( ) => {
+    const isLandscapeMode = [LANDSCAPE_LEFT, LANDSCAPE_RIGHT].includes( deviceOrientation );
 
-  return {
-    deviceOrientation,
-    isTablet,
-    isLandscapeMode,
-    screenWidth,
-    screenHeight
-  };
+    const screenWidth = isLandscapeMode
+      ? Math.max( width, height )
+      : Math.min( width, height );
+
+    const screenHeight = isLandscapeMode
+      ? Math.min( width, height )
+      : Math.max( width, height );
+
+    return {
+      deviceOrientation,
+      isTablet: DeviceInfo.isTablet( ),
+      isLandscapeMode,
+      screenWidth,
+      screenHeight
+    };
+  }, [deviceOrientation] );
+
+  return orientation;
 };
 
 export default useDeviceOrientation;
