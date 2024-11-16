bf781e5107fcfe36781a50808dc325e2c55114fc
Johannes Klein <johannes.t.klein@gmail.com>
2024-11-16 14:34:46 +0100

    Comment out camera test

--- e2e/aiCamera.e2e.js
+++ e2e/aiCamera.e2e.js
@@ -1,15 +1,18 @@
 import {
-  by, device, element, waitFor
+  // by,
+  device
+  // element,
+  // waitFor
 } from "detox";
 
 import { iNatE2eBeforeAll, iNatE2eBeforeEach } from "./helpers";
 import closeOnboarding from "./sharedFlows/closeOnboarding";
-import deleteObservation from "./sharedFlows/deleteObservation";
-import signIn from "./sharedFlows/signIn";
-import switchPowerMode from "./sharedFlows/switchPowerMode";
-import uploadObservation from "./sharedFlows/uploadObservation";
+// import deleteObservation from "./sharedFlows/deleteObservation";
+// import signIn from "./sharedFlows/signIn";
+// import switchPowerMode from "./sharedFlows/switchPowerMode";
+// import uploadObservation from "./sharedFlows/uploadObservation";
 
-const TIMEOUT = 10_000;
+// const TIMEOUT = 10_000;
 
 describe( "AICamera", () => {
   beforeAll( async () => iNatE2eBeforeAll( device ) );
@@ -19,70 +22,70 @@ describe( "AICamera", () => {
     "should open the ai camera, take photo, select a suggestion, upload and delete observation",
     async () => {
       await closeOnboarding( );
-      /*
-      / 1. Sign in
-      */
-      const username = await signIn();
+      // /*
+      // / 1. Sign in
+      // */
+      // const username = await signIn();
 
-      /*
-      / 2. Switch UI to power user mode
-      */
-      await switchPowerMode();
+      // /*
+      // / 2. Switch UI to power user mode
+      // */
+      // await switchPowerMode();
 
-      /*
-      / 3. Take photo with AI Camera, select a suggestion, upload and delete observation
-      */
-      // Tap to open AICamera
-      const addObsButton = element( by.id( "add-obs-button" ) );
-      await waitFor( addObsButton ).toBeVisible().withTimeout( TIMEOUT );
-      await addObsButton.tap();
-      const aiCameraButton = element( by.id( "aicamera-button" ) );
-      await waitFor( aiCameraButton ).toBeVisible().withTimeout( TIMEOUT );
-      await aiCameraButton.tap();
+      // /*
+      // / 3. Take photo with AI Camera, select a suggestion, upload and delete observation
+      // */
+      // // Tap to open AICamera
+      // const addObsButton = element( by.id( "add-obs-button" ) );
+      // await waitFor( addObsButton ).toBeVisible().withTimeout( TIMEOUT );
+      // await addObsButton.tap();
+      // const aiCameraButton = element( by.id( "aicamera-button" ) );
+      // await waitFor( aiCameraButton ).toBeVisible().withTimeout( TIMEOUT );
+      // await aiCameraButton.tap();
 
-      // Check that the camera screen is visible
-      const cameraContainer = element( by.id( "CameraWithDevice" ) );
-      await waitFor( cameraContainer ).toBeVisible().withTimeout( TIMEOUT );
-      // Check that the mocked cv suggestion is visible
-      const taxonResult = element( by.id( "AICamera.taxa.51779" ) );
-      await waitFor( taxonResult ).toBeVisible().withTimeout( TIMEOUT );
-      // Tap the take photo button
-      const takePhotoButton = element( by.id( "take-photo-button" ) );
-      await waitFor( takePhotoButton ).toBeVisible().withTimeout( TIMEOUT );
-      await takePhotoButton.tap();
+      // // Check that the camera screen is visible
+      // const cameraContainer = element( by.id( "CameraWithDevice" ) );
+      // await waitFor( cameraContainer ).toBeVisible().withTimeout( TIMEOUT );
+      // // Check that the mocked cv suggestion is visible
+      // const taxonResult = element( by.id( "AICamera.taxa.51779" ) );
+      // await waitFor( taxonResult ).toBeVisible().withTimeout( TIMEOUT );
+      // // Tap the take photo button
+      // const takePhotoButton = element( by.id( "take-photo-button" ) );
+      // await waitFor( takePhotoButton ).toBeVisible().withTimeout( TIMEOUT );
+      // await takePhotoButton.tap();
 
-      // On suggestions find the first element in the suggestions list
-      const otherSuggestionsTitle = element( by.text( "OTHER SUGGESTIONS" ) );
-      await waitFor( otherSuggestionsTitle ).toBeVisible( ).withTimeout( 30_000 );
-      const firstSuggestion = element( by.id( /SuggestionsList\.taxa\..*/ ) ).atIndex(
-        0
-      );
-      await waitFor( firstSuggestion ).toBeVisible().withTimeout( TIMEOUT );
-      const suggestionAttributes = await firstSuggestion.getAttributes();
-      const taxonID = suggestionAttributes.elements
-        ? suggestionAttributes.elements[0].identifier.split( "." ).pop()
-        : suggestionAttributes.identifier.split( "." ).pop();
-      await firstSuggestion.tap();
+      // // On suggestions find the first element in the suggestions list
+      // const otherSuggestionsTitle = element( by.text( "OTHER SUGGESTIONS" ) );
+      // await waitFor( otherSuggestionsTitle ).toBeVisible( ).withTimeout( 30_000 );
+      // const firstSuggestion = element( by.id( /SuggestionsList\.taxa\..*/ ) ).atIndex(
+      //   0
+      // );
+      // await waitFor( firstSuggestion ).toBeVisible().withTimeout( TIMEOUT );
+      // const suggestionAttributes = await firstSuggestion.getAttributes();
+      // const taxonID = suggestionAttributes.elements
+      //   ? suggestionAttributes.elements[0].identifier.split( "." ).pop()
+      //   : suggestionAttributes.identifier.split( "." ).pop();
+      // await firstSuggestion.tap();
 
-      // On Taxon Detail
-      const selectTaxonButon = element( by.id( "TaxonDetails.SelectButton" ) );
-      await waitFor( selectTaxonButon ).toBeVisible().withTimeout( TIMEOUT );
-      await selectTaxonButon.tap();
+      // // On Taxon Detail
+      // const selectTaxonButon = element( by.id( "TaxonDetails.SelectButton" ) );
+      // await waitFor( selectTaxonButon ).toBeVisible().withTimeout( TIMEOUT );
+      // await selectTaxonButon.tap();
 
-      await uploadObservation( { upload: true } );
+      // await uploadObservation( { upload: true } );
 
-      // Check that the display taxon name is visible
-      const displayTaxonName = element( by.id( `display-taxon-name.${taxonID}` ) ).atIndex(
-        0
-      );
-      await waitFor( displayTaxonName ).toBeVisible().withTimeout( TIMEOUT );
-      await displayTaxonName.tap();
+      // // Check that the display taxon name is visible
+      // const displayTaxonName = element( by.id( `display-taxon-name.${taxonID}` ) ).atIndex(
+      //   0
+      // );
+      // await waitFor( displayTaxonName ).toBeVisible().withTimeout( TIMEOUT );
+      // await displayTaxonName.tap();
 
-      // Delete the observation
-      await deleteObservation( { uploaded: true } );
+      // // Delete the observation
+      // await deleteObservation( { uploaded: true } );
 
-      // Make sure we're back on MyObservations
-      await waitFor( username ).toBeVisible().withTimeout( TIMEOUT );
+      // // Make sure we're back on MyObservations
+      // await waitFor( username ).toBeVisible().withTimeout( TIMEOUT );
     }
   );
 } );
