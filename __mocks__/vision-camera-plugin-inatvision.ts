a2271e46abbaf0b800130e2f29516f42d6edd42c
Johannes Klein <johannes.t.klein@gmail.com>
2025-01-09 15:35:59 +0100

    Rename function mock

--- __mocks__/vision-camera-plugin-inatvision.ts
+++ __mocks__/vision-camera-plugin-inatvision.ts
@@ -2,7 +2,7 @@ export const getPredictionsForImage = jest.fn( () => Promise.resolve( { predicti
 export const getPredictionsForLocation = jest.fn( () => Promise.resolve( { predictions: [] } ) );
 export const removeLogListener = jest.fn( );
 export const resetStoredResults = jest.fn( );
-export const lookUpLocation = jest.fn( location => ( {
+export const getCellLocation = jest.fn( location => ( {
   ...location,
   elevation: 12
 } ) );
