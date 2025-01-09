0a5d6161ba34bf32a5ceb3e70a76f9b05c8e36c9
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-01-09 15:47:59 -0800

    fix: save or discard the observation from the match screen

--- src/components/Match/MatchContainer.js
+++ src/components/Match/MatchContainer.js
@@ -1,17 +1,22 @@
 import { useNavigation } from "@react-navigation/native";
+import { RealmContext } from "providers/contexts.ts";
 import React from "react";
+import saveObservation from "sharedHelpers/saveObservation.ts";
 import { useLocationPermission, useTaxon } from "sharedHooks";
 import useStore from "stores/useStore";
 
 import Match from "./Match";
 
+const { useRealm } = RealmContext;
+
 const MatchContainer = ( ) => {
   const currentObservation = useStore( state => state.currentObservation );
+  const cameraRollUris = useStore( state => state.cameraRollUris );
   const matchScreenSuggestion = useStore( state => state.matchScreenSuggestion );
   const navigation = useNavigation( );
   const { hasPermissions, renderPermissionsGate, requestPermissions } = useLocationPermission( );
-
   const { taxon } = useTaxon( matchScreenSuggestion?.taxon );
+  const realm = useRealm( );
 
   const navToTaxonDetails = ( ) => {
     navigation.push( "TaxonDetails", {
@@ -20,7 +25,17 @@ const MatchContainer = ( ) => {
     } );
   };
 
-  const handleSaveOrDiscardPress = action => console.log( action, "action" );
+  const handleSaveOrDiscardPress = async action => {
+    if ( action === "save" ) {
+      await saveObservation( currentObservation, cameraRollUris, realm );
+    }
+    navigation.navigate( "TabNavigator", {
+      screen: "TabStackNavigator",
+      params: {
+        screen: "ObsList"
+      }
+    } );
+  };
 
   const openLocationPicker = ( ) => {
     navigation.navigate( "LocationPicker" );
