1afaf58d6650290ac215311cf6f28c58fd6b91fd
Amanda Bullington <albullington@gmail.com>
2025-01-09 16:58:10 -0800

    Generate strings for match header based on rank and confidence

--- src/components/Match/MatchHeader.js
+++ src/components/Match/MatchHeader.js
@@ -8,8 +8,11 @@ import {
 import {
   View
 } from "components/styledComponents";
+import { RealmContext } from "providers/contexts.ts";
 import React from "react";
 import { useTranslation } from "sharedHooks";
+
+const { useRealm } = RealmContext;
 
 type Props = {
   taxon: {
@@ -22,9 +25,65 @@ type Props = {
 
 const MatchHeader = ( { taxon, confidence }: Props ) => {
   const { t } = useTranslation( );
+  const realm = useRealm( );
+
+  const hasSeenSpecies = realm.objects( "Observation" )
+    .filtered( `taxon.id == ${taxon.id} && taxon.rank_level == 10` )[0];
 
   const suggestedTaxon = taxon;
   const taxonId = taxon?.id || "unknown";
+
+  const observationStatus = ( ) => {
+    let confidenceType = "may_have_observed";
+    if ( confidence >= 93 ) {
+      confidenceType = "observed";
+    } else if ( confidence >= 50 && confidence < 93 ) {
+      confidenceType = "likely_observed";
+    }
+
+    let rankDescription = "organism";
+    if ( !hasSeenSpecies ) {
+      rankDescription = "new_species";
+    } else if ( taxon.rank_level === 10 ) {
+      rankDescription = "species";
+    }
+
+    return { confidenceType, rankDescription };
+  };
+
+  const generateCongratulatoryText = ( ) => {
+    let congratulatoryText;
+    const { confidenceType, rankDescription } = observationStatus( );
+
+    if ( confidenceType === "observed" ) {
+      if ( rankDescription === "new_species" ) {
+        congratulatoryText = t( "You-observed-a-new-species" );
+      } else if ( rankDescription === "species" ) {
+        congratulatoryText = t( "You-observed-a-species" );
+      } else if ( rankDescription === "organism" ) {
+        congratulatoryText = t( "You-observed-an-organism-in-this-group" );
+      }
+    }
+    if ( confidenceType === "likely_observed" ) {
+      if ( rankDescription === "new_species" ) {
+        congratulatoryText = t( "You-likely-observed-a-new-species" );
+      } else if ( rankDescription === "species" ) {
+        congratulatoryText = t( "You-likely-observed-a-species" );
+      } else if ( rankDescription === "organism" ) {
+        congratulatoryText = t( "You-likely-observed-an-organism-in-this-group" );
+      }
+    }
+    if ( confidenceType === "may_have_observed" ) {
+      if ( rankDescription === "new_species" ) {
+        congratulatoryText = t( "You-may-have-observed-a-new-species" );
+      } else if ( rankDescription === "species" ) {
+        congratulatoryText = t( "You-may-have-observed-a-species" );
+      } else if ( rankDescription === "organism" ) {
+        congratulatoryText = t( "You-may-have-observed-an-organism-in-this-group" );
+      }
+    }
+    return congratulatoryText;
+  };
 
   const showSuggestedTaxon = ( ) => (
     <View className="shrink">
@@ -40,7 +99,7 @@ const MatchHeader = ( { taxon, confidence }: Props ) => {
 
   return (
     <View>
-      <Body2 className="mb-2">{t( "You-observed-a-new-species" )}</Body2>
+      <Body2 className="mb-2">{generateCongratulatoryText( )}</Body2>
       <View className="flex-row justify-between items-center">
         {showSuggestedTaxon( )}
         <View className="justify-end items-center ml-5">
