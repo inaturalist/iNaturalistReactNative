e59aa0738c209da542ea87dd5d2117703299a622
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-01-23 12:24:22 -0800

    fix: allow Explore searches in places again
    * remove assumed and undocumented meaning of page being zero from our use of
      useInfiniteScroll in Explore; uses explicit type checks instead
    * document byzantine pagination logic in useInfiniteExploreScroll
    * fix broken infinite scroll when sorting by faves (was only showing first
      page)
    
    The underlying problem from 96c316a was setting initialPageParam to 1 in
    useAuthenticatedInfiniteQuery, which seems rational, except
    useInfiniteExploreScroll was using a false-ish value of page to detect when
    it was requesting its first page. This does the same by being more explicit
    about page's type without injecting undocumented meaning into the params we
    hand to useInfiniteScroll.

--- src/components/Explore/hooks/useInfiniteExploreScroll.js
+++ src/components/Explore/hooks/useInfiniteExploreScroll.js
@@ -29,28 +29,55 @@ const useInfiniteExploreScroll = ( { params: newInputParams, enabled }: Object )
 
   const queryKey = ["useInfiniteExploreScroll", newInputParams];
 
+  // Pagination using our API is complicated. The page param will only work
+  // for the first 10k results. Frequently-updated result sets (like the
+  // default), can potentially show duplicate observations on subsequent
+  // pages using the page param as well. To avoid both of those issues, we're
+  // paginating results using the id_below / id_above or d1 / d2 params when
+  // possible, i.e. when sorting by date created or date observed. Other
+  // sorts must use the page param and suffer the limitations described
+  // above.
   const getNextPageParam = useCallback( lastPage => {
     const lastObs = last( lastPage.results );
     const orderBy = baseParams.order_by;
 
+    // Returning null tells useInfiniteQuery there is no next page, so the
+    // query method should not even run. If there are no observations in the
+    // results, we're done and can stop requesting new pages.
     if ( !lastObs ) return null;
 
+    // Datetime sorts need to use d1 / d2 or created_d1 / created_d2 to
+    // paginate results, so were storing a datetime from the last observation
+    // as the "page" and we'll use that to adjust the query when we perform
+    // it
     if ( ["observed_on", "created_at"].includes( orderBy ) ) {
       const lastObsDate = orderBy === "observed_on"
         ? lastObs?.time_observed_at
         : lastObs?.created_at;
 
+      // If there are results but the last one doesn't have a datetime, we're
+      // also done... but this is probably impossible.
       if ( !lastObsDate ) {
         return null;
       }
 
       const lastObsDateParsed = parseISO( lastObsDate );
+      // Adding / subtracting a second helps us not include this last
+      // observation on the next page of results
       const newObsDate = addSeconds( lastObsDateParsed, baseParams.order === "asc"
         ? 1
         : -1 );
       return formatISO( newObsDate );
     }
 
+    // Any sort that isn't observed_on or created_at and isn't the default
+    // (blank, meaning created_at) should use basic pagination.
+    if ( typeof ( orderBy ) === "string" ) {
+      return lastPage.page + 1;
+    }
+
+    // If we got here that means orderBy is undefined or null, i.e. the
+    // default sort order by id
     return lastObs?.id;
   }, [baseParams.order_by, baseParams.order] );
 
@@ -65,36 +92,45 @@ const useInfiniteExploreScroll = ( { params: newInputParams, enabled }: Object )
   } = useAuthenticatedInfiniteQuery(
     queryKey,
     async ( { pageParam }, optsWithAuth ) => {
-      const params = {
-        ...baseParams
-      };
+      const params = { ...baseParams };
 
-      if ( pageParam ) {
-        if ( params.order_by === "observed_on" ) {
-          if ( baseParams.order === "asc" ) {
-            params.d1 = pageParam;
-          } else {
-            params.d2 = pageParam;
-          }
-        } else if ( params.order_by === "created_at" ) {
-          if ( baseParams.order === "asc" ) {
-            params.created_d1 = pageParam;
-          } else {
-            params.created_d2 = pageParam;
-          }
-        } else {
-          // $FlowIgnore
-          params.id_below = pageParam;
-        }
-      } else {
-        // $FlowIgnore
-        params.page = 1;
-        // For the first page and only for the first page, get the bounds as
-        // well
+      // For the purposes of Explore, an undefined page or a page of 0 is the
+      // default state before any data has loaded, so those values mean we're
+      // requesting the first page
+      const requestingFirstPage = typeof ( pageParam ) === "undefined" || pageParam === 0;
+      if ( requestingFirstPage ) {
+        // For the first page and only for the first page, we need to retrieve
+        // the georaphic bounds of the results so we can pan and zoom the map
+        // to contain them
         params.return_bounds = true;
+      } else if ( params.order_by === "observed_on" ) {
+        // If we're ordering by date observed, we are "paginating" by date and
+        // getNextPageParam will have set the pageParam to a date string from
+        // the last obs in the previous page
+        if ( baseParams.order === "asc" ) {
+          params.d1 = pageParam;
+        } else {
+          params.d2 = pageParam;
+        }
+      } else if ( params.order_by === "created_at" ) {
+        // If we're ordering by date created, we are "paginating" by date and
+        // getNextPageParam will have set the pageParam to a date string from
+        // the last obs in the previous page
+        if ( baseParams.order === "asc" ) {
+          params.created_d1 = pageParam;
+        } else {
+          params.created_d2 = pageParam;
+        }
+      } else if ( typeof ( params.order_by ) === "string" ) {
+        // If this is any kind of sort other than the date sorts and isn't the
+        // default (blank), assume basic pagination using the page param
+        params.page = pageParam;
+      } else {
+        // If we're using default sort order by id, getNextPageParam will have
+        // set pageParam to a serial id
+        params.id_below = pageParam;
       }
-      const response = await searchObservations( params, optsWithAuth );
-      return response;
+      return searchObservations( params, optsWithAuth );
     },
     {
       getNextPageParam,
