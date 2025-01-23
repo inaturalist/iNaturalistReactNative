623b0f054787d54f5fcc279101e2632bdc6f6074
Amanda Bullington <albullington@gmail.com>
2025-01-22 17:13:20 -0800

    Add a loading wheel and freeze frame before awaiting logs in AICamera

---
+++ src/components/Camera/FadeInOutView.tsx
@@ -0,0 +1,54 @@
+import { ActivityIndicator } from "components/SharedComponents";
+import type { Node } from "react";
+import React, { useEffect, useRef } from "react";
+import { Animated } from "react-native";
+import colors from "styles/tailwindColors";
+
+interface Props {
+  takingPhoto: boolean,
+  cameraType: string
+}
+
+const fade = value => ( {
+  toValue: value,
+  duration: 100,
+  useNativeDriver: true
+} );
+
+const FadeInOutView = ( { takingPhoto, cameraType }: Props ): Node => {
+  const fadeAnimation = useRef( new Animated.Value( 0 ) ).current;
+
+  useEffect( ( ) => {
+    if ( takingPhoto ) {
+      Animated.sequence( [
+        Animated.timing( fadeAnimation, fade( 1 ) ),
+        Animated.timing( fadeAnimation, fade( 0 ) )
+      ] ).start( );
+    }
+  }, [takingPhoto, fadeAnimation] );
+
+  return (
+    <>
+      <Animated.View
+        pointerEvents="none"
+        className="items-center justify-center"
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          position: "absolute",
+          height: "100%",
+          width: "100%",
+          backgroundColor: colors.black,
+          opacity: fadeAnimation
+        }}
+      />
+      {( takingPhoto && cameraType === "AI" ) && (
+        <ActivityIndicator
+          size={50}
+          color={colors.white}
+        />
+      )}
+    </>
+  );
+};
+
+export default FadeInOutView;
