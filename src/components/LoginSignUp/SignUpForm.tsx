6a8fe1e7cf9f4f2a064a3fff0a4a37a2f06d15e3
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-03 16:24:45 +0100

    Remove superfluous margin

--- src/components/LoginSignUp/SignUpForm.tsx
+++ src/components/LoginSignUp/SignUpForm.tsx
@@ -48,7 +48,7 @@ const SignUpForm = ( { hideFooter }: Props ) => {
 
   return (
     <TouchableWithoutFeedback accessibilityRole="button" onPress={blurFields}>
-      <View className="px-4 mt-[9px] justify-end">
+      <View className="px-4 justify-end">
         <LoginSignUpInputField
           ref={emailRef}
           accessibilityLabel={t( "EMAIL" )}
