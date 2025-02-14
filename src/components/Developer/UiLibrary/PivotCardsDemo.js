2854f9ac786a382f2cf305bd40965158b5999bb6
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-14 23:07:30 +0100

    Add cards to UILibrary

---
+++ src/components/Developer/UiLibrary/PivotCardsDemo.js
@@ -0,0 +1,65 @@
+import {
+  AccountCreationCard,
+  NotificationOnboarding,
+  SecondObservationCard
+} from "components/OnboardingModal/PivotCards.tsx";
+import {
+  Button,
+  ScrollViewWrapper
+} from "components/SharedComponents";
+import { View } from "components/styledComponents";
+import React, { useState } from "react";
+
+/* eslint-disable i18next/no-literal-string */
+/* eslint-disable react/no-unescaped-entities */
+const Buttons = ( ) => {
+  const [showModal, setShowModal] = useState( false );
+  const [modalIndex, setModalIndex] = useState( 0 );
+
+  const setShowingModal = index => {
+    setModalIndex( index );
+    setShowModal( true );
+  };
+
+  const closeModal = () => {
+    setShowModal( false );
+  };
+
+  const pivotCards = [
+    {
+      title: "Account Creation",
+      component: AccountCreationCard
+    },
+    {
+      title: "Second Observation",
+      component: SecondObservationCard
+    },
+    {
+      title: "Notification Onboarding",
+      component: NotificationOnboarding
+    }
+  ];
+
+  return (
+    <ScrollViewWrapper>
+      {
+        pivotCards.map( ( { title, component }, index ) => (
+          <View className="p-4">
+            <Button
+              className="mb-2"
+              level="primary"
+              text={title}
+              onPress={() => setShowingModal( index )}
+            />
+            {React.createElement(
+              component,
+              { showModal: showModal && modalIndex === index, closeModal }
+            )}
+          </View>
+        ) )
+      }
+    </ScrollViewWrapper>
+  );
+};
+
+export default Buttons;
