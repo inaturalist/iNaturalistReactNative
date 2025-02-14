0d7cf45c8342bc066f87ce61cb8dc7865ef4275c
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-14 20:32:23 +0100

    Basic account creation card

--- src/components/OnboardingModal/PivotCards.tsx
+++ src/components/OnboardingModal/PivotCards.tsx
@@ -23,6 +23,24 @@ const NotificationOnboarding = ( { showModal, closeModal }: Props ) => {
   );
 };
 
+const AccountCreationCard = ( { showModal, closeModal }: Props ) => {
+  const { t } = useTranslation( );
+  return (
+    <OnboardingModal
+      showModal={showModal}
+      closeModal={closeModal}
+      slides={[
+        {
+          title: t( "Your-observations-can-now-help-scientists" ),
+          description: t( "All-observations-submitted-to-iNaturalist-need-a-date-and-location" ),
+          imageSource: require( "images/background/camera-finder.png" )
+        }
+      ]}
+    />
+  );
+};
+
 export {
+  AccountCreationCard,
   NotificationOnboarding,
 };
