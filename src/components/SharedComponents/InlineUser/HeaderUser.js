3834b0c8e251954ff1cec6ca8ba87c7aa8c7b227
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-20 12:04:24 +0100

    Refactor User components to use a base component

--- src/components/SharedComponents/InlineUser/HeaderUser.js
+++ src/components/SharedComponents/InlineUser/HeaderUser.js
@@ -1,19 +1,11 @@
 // @flow
 
-import { useNavigation } from "@react-navigation/native";
-// Directly imported, not from index.js to avoid circular dependency
-import INatIcon from "components/SharedComponents/INatIcon";
 // Directly imported, not from index.js to avoid circular dependency
 import Heading3 from "components/SharedComponents/Typography/Heading3.tsx";
-// Directly imported, not from index.js to avoid circular dependency
-import UserIcon from "components/SharedComponents/UserIcon.tsx";
-import {
-  Pressable, View
-} from "components/styledComponents";
 import type { Node } from "react";
 import React from "react";
-import { useTranslation } from "react-i18next";
-import User from "realmModels/User.ts";
+
+import InlineUserBase from "./InlineUserBase";
 
 type Props = {
   user: {
@@ -24,49 +16,14 @@ type Props = {
   isConnected: boolean
 };
 
-const HeaderUser = ( { user, isConnected }: Props ): Node => {
-  const navigation = useNavigation();
-  const userImgUri = User.uri( user );
-  const userHandle = user?.login;
-
-  const { t } = useTranslation( );
-
-  const renderUserIcon = () => {
-    if ( !userImgUri || !isConnected ) {
-      return (
-        <INatIcon
-          testID="HeaderUser.FallbackPicture"
-          name="person"
-          size={32}
-        />
-      );
-    }
-    return <UserIcon size={32} uri={userImgUri} />;
-  };
-
-  return (
-    <Pressable
-      testID="HeaderUser"
-      className="flex flex-row items-center shrink"
-      accessibilityRole="link"
-      accessibilityLabel={t( "User", { userHandle } )}
-      accessibilityHint={t( "Navigates-to-user-profile" )}
-      onPress={() => {
-        navigation.navigate( "UserProfile", { userId: user.id } );
-      }}
-    >
-      <View className="mr-[7px]">{renderUserIcon()}</View>
-      <Heading3
-        className="w-3/4"
-        numberOfLines={1}
-        ellipsizeMode="tail"
-        selectable
-        maxFontSizeMultiplier={1}
-      >
-        {userHandle}
-      </Heading3>
-    </Pressable>
-  );
-};
+const HeaderUser = ( { user, isConnected }: Props ): Node => (
+  <InlineUserBase
+    user={user}
+    isConnected={isConnected}
+    TextComponent={Heading3}
+    testID="HeaderUser"
+    useBigIcon
+  />
+);
 
 export default HeaderUser;
--- src/components/SharedComponents/InlineUser/InlineUser.js
+++ src/components/SharedComponents/InlineUser/InlineUser.js
@@ -1,19 +1,11 @@
 // @flow
 
-import { useNavigation } from "@react-navigation/native";
-// Directly imported, not from index.js to avoid circular dependency
-import INatIcon from "components/SharedComponents/INatIcon";
 // Directly imported, not from index.js to avoid circular dependency
 import Body3 from "components/SharedComponents/Typography/Body3.tsx";
-// Directly imported, not from index.js to avoid circular dependency
-import UserIcon from "components/SharedComponents/UserIcon.tsx";
-import {
-  Pressable, View
-} from "components/styledComponents";
 import type { Node } from "react";
 import React from "react";
-import { useTranslation } from "react-i18next";
-import User from "realmModels/User.ts";
+
+import InlineUserBase from "./InlineUserBase";
 
 type Props = {
   user: {
@@ -24,49 +16,13 @@ type Props = {
   isConnected: boolean
 };
 
-const InlineUser = ( { user, isConnected }: Props ): Node => {
-  const navigation = useNavigation();
-  const userImgUri = User.uri( user );
-  const userHandle = user?.login;
-
-  const { t } = useTranslation( );
-
-  const renderUserIcon = () => {
-    if ( !userImgUri || !isConnected ) {
-      return (
-        <INatIcon
-          testID="InlineUser.FallbackPicture"
-          name="person"
-          size={22}
-        />
-      );
-    }
-    return <UserIcon uri={userImgUri} small />;
-  };
-
-  return (
-    <Pressable
-      testID="InlineUser"
-      className="flex flex-row items-center shrink"
-      accessibilityRole="link"
-      accessibilityLabel={t( "User", { userHandle } )}
-      accessibilityHint={t( "Navigates-to-user-profile" )}
-      onPress={() => {
-        navigation.navigate( "UserProfile", { userId: user.id } );
-      }}
-    >
-      <View className="mr-[7px]">{renderUserIcon()}</View>
-      <Body3
-        className="w-3/4"
-        numberOfLines={1}
-        ellipsizeMode="tail"
-        selectable
-        maxFontSizeMultiplier={1}
-      >
-        {userHandle}
-      </Body3>
-    </Pressable>
-  );
-};
+const InlineUser = ( { user, isConnected }: Props ): Node => (
+  <InlineUserBase
+    user={user}
+    isConnected={isConnected}
+    TextComponent={Body3}
+    testID="InlineUser"
+  />
+);
 
 export default InlineUser;
