0afcfe886533343d78350aec3f3e0a6f9c22a088
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-21 09:58:50 +0100

    Add top margin to pivot card x button

--- src/components/OnboardingModal/OnboardingModalBase.tsx
+++ src/components/OnboardingModal/OnboardingModalBase.tsx
@@ -2,6 +2,7 @@ import { INatIconButton } from "components/SharedComponents";
 import { View } from "components/styledComponents";
 import React, { PropsWithChildren } from "react";
 import { StatusBar } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useTranslation } from "sharedHooks";
 import colors from "styles/tailwindColors";
 
@@ -14,11 +15,13 @@ const OnboardingModalBase = ( {
   children
 }: Props ) => {
   const { t } = useTranslation( );
+  const { top } = useSafeAreaInsets();
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="black" />
       <View className="flex-1 relative">
-        <View className="absolute right-0">
+        <View className={`absolute mt-[${top}px] right-0`}>
           <INatIconButton
             icon="close"
             color={colors?.white}
