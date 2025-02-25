0527b2f7bb607cf2d80b8eff6e88ed322c33ea10
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-25 11:13:42 +0100

    Update PhotoImport.test.js

--- tests/integration/PhotoImport.test.js
+++ tests/integration/PhotoImport.test.js
@@ -90,7 +90,8 @@ beforeAll( async () => {
 beforeEach( ( ) => {
   useStore.setState( {
     layout: {
-      isDefaultMode: false
+      isDefaultMode: false,
+      isSuggestionsFlowMode: true
     },
     isAdvancedUser: true
   } );
