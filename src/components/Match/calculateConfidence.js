845ed377f8ecdbcec4fd93b246aff5e38875e13e
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-28 13:20:02 +0100

    Common ancestor should use combine_score

--- src/components/Match/calculateConfidence.js
+++ src/components/Match/calculateConfidence.js
@@ -4,12 +4,8 @@ const calculateConfidence = suggestion => {
   }
 
   // Note: combined_score as returned from vision-plugin >v5 as well as iNatVisionAPI
-  // have values between 0 and 100. common_ancestor from the API has a score field
-  // with a range of 0-100
-  const score = suggestion?.score
-    ? suggestion.score
-    : suggestion.combined_score;
-  const confidence = parseFloat( score.toFixed( 1 ) );
+  // have values between 0 and 100.
+  const confidence = parseFloat( suggestion.combined_score.toFixed( 1 ) );
   return confidence;
 };
 
