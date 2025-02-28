262cc4184032406d1827a45fef19e98753c1184b
Johannes Klein <johannes.t.klein@gmail.com>
2025-02-28 13:34:09 +0100

    Fic typo in tests

--- tests/integration/SuggestionsWithUnsyncedObs.test.js
+++ tests/integration/SuggestionsWithUnsyncedObs.test.js
@@ -22,15 +22,15 @@ const mockModelResult = {
   predictions: [
     factory( "ModelPrediction", {
       rank_level: 30,
-      combine_score: 86
+      combined_score: 86
     } ),
     factory( "ModelPrediction", {
       rank_level: 20,
-      combine_score: 96
+      combined_score: 96
     } ),
     factory( "ModelPrediction", {
       rank_level: 10,
-      combine_score: 40
+      combined_score: 40
     } )]
 };
 
@@ -38,11 +38,11 @@ const mockModelResultNoConfidence = {
   predictions: [
     factory( "ModelPrediction", {
       rank_level: 30,
-      combine_score: 70
+      combined_score: 70
     } ),
     factory( "ModelPrediction", {
       rank_level: 20,
-      combine_score: 65
+      combined_score: 65
     } )
   ]
 };
@@ -51,11 +51,11 @@ const mockModelResultWithHuman = {
   predictions: [
     factory( "ModelPrediction", {
       rank_level: 20,
-      combine_score: 86
+      combined_score: 86
     } ),
     factory( "ModelPrediction", {
       rank_level: 30,
-      combine_score: 96,
+      combined_score: 96,
       name: "Homo"
     } )
   ]
