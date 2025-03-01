f0e0f7ca3d8c2034607c4e51b7f53900d2406417
Kirk van Gorkom <kirk@vangorkom.org>
2025-02-28 22:16:44 -0800

    Revert "Patch confidence calculation crash by handling both score and combined_score"
    This reverts commit 4bab4b222dd50455fc4532c959632c47957acef0.
    
    Didn't catch an error when trying to change to a new branch, accidentally committed and pushed to main.

--- src/components/Match/calculateConfidence.js
+++ src/components/Match/calculateConfidence.js
@@ -5,15 +5,8 @@ const calculateConfidence = suggestion => {
 
   // Note: combined_score as returned from vision-plugin >v5 as well as iNatVisionAPI
   // have values between 0 and 100.
-  // For common_ancestor from API, the score is in score field rather than combined_score
-  if ( suggestion.combined_score === undefined && suggestion.score !== undefined ) {
-    return parseFloat( suggestion.score.toFixed( 1 ) );
-  } if ( suggestion.combined_score !== undefined ) {
-    return parseFloat( suggestion.combined_score.toFixed( 1 ) );
-  }
-
-  // Return default confidence if neither score exists
-  return 0;
+  const confidence = parseFloat( suggestion.combined_score.toFixed( 1 ) );
+  return confidence;
 };
 
 export default calculateConfidence;
