6918849cc8c648369c09a34bc6e7af1395274389
Kirk van Gorkom <kirk@vangorkom.org>
2025-02-28 22:13:58 -0800

    Patch confidence calculation crash by handling both score and combined_score

--- src/components/Match/calculateConfidence.js
+++ src/components/Match/calculateConfidence.js
@@ -5,8 +5,15 @@ const calculateConfidence = suggestion => {
 
   // Note: combined_score as returned from vision-plugin >v5 as well as iNatVisionAPI
   // have values between 0 and 100.
-  const confidence = parseFloat( suggestion.combined_score.toFixed( 1 ) );
-  return confidence;
+  // For common_ancestor from API, the score is in score field rather than combined_score
+  if ( suggestion.combined_score === undefined && suggestion.score !== undefined ) {
+    return parseFloat( suggestion.score.toFixed( 1 ) );
+  } if ( suggestion.combined_score !== undefined ) {
+    return parseFloat( suggestion.combined_score.toFixed( 1 ) );
+  }
+
+  // Return default confidence if neither score exists
+  return 0;
 };
 
 export default calculateConfidence;
