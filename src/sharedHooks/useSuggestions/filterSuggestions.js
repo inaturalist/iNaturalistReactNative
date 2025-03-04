38b89e1cf2236030c01ed93366d90ba9c00aec03
Johannes Klein <johannes.t.klein@gmail.com>
2025-03-04 15:27:28 +0100

    Update comments

--- src/sharedHooks/useSuggestions/filterSuggestions.js
+++ src/sharedHooks/useSuggestions/filterSuggestions.js
@@ -14,15 +14,14 @@ const THRESHOLD = 78;
 
 // this function does a few things:
 // 1. it makes sure that if there is a human suggestion, human is the only result returned
-// 2. it makes sure we're filtering out any results below the THRESHOLD
-// so we only show results we're fairly confident in
-// 3. it splits the top suggestion result out from the rest of the suggestions, which is helpful for
+// 2. it splits the top suggestion result out from the rest of the suggestions, which is helpful for
 // displaying in SuggestionsContainer
-// 4. it checks for a common ancestor as a fallback top suggestion if user is online
+// 4. it checks for a common ancestor as a fallback top suggestion
 // 5. it returns topSuggestionType which is useful for debugging, since we're doing a lot of
 // filtering of both online and offline suggestions
 const filterSuggestions = ( suggestionsToFilter, commonAncestor ) => {
   const sortedSuggestions = _.orderBy(
+    // TODO: handling humans is implemented in the vision-plugin, can it be removed here?
     isolateHumans( suggestionsToFilter ),
     "combined_score",
     "desc"
