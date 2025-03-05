ea183f96172ed1ade5988fd1f1b8c686bda7325c
Amanda Bullington <albullington@gmail.com>
2025-03-04 16:11:57 -0800

    Selective changes from 4916cf70fb8cc90496927cf9e502bf702b37f690

---
+++ tests/unit/components/Match/Match.test.js
@@ -0,0 +1,77 @@
+import Geolocation from "@react-native-community/geolocation";
+import { screen } from "@testing-library/react-native";
+import Match from "components/Match/Match";
+import initI18next from "i18n/initI18next";
+import React from "react";
+import * as useLocationPermission from "sharedHooks/useLocationPermission.tsx";
+import factory from "tests/factory";
+import { renderComponent } from "tests/helpers/render";
+
+// Initialize i18next for translations
+beforeAll( async () => {
+  await initI18next();
+} );
+
+describe( "Match", ( ) => {
+  // Mock props that would normally come from MatchContainer
+  const defaultProps = {
+    observation: factory( "LocalObservation" ),
+    obsPhotos: [factory( "LocalObservationPhoto" )],
+    onSuggestionChosen: jest.fn(),
+    handleSaveOrDiscardPress: jest.fn(),
+    navToTaxonDetails: jest.fn(),
+    handleAddLocationPressed: jest.fn(),
+    scrollRef: { current: null }
+  };
+
+  beforeEach( () => {
+    jest.clearAllMocks();
+  } );
+
+  it( "should show location permissions button if permissions not granted", () => {
+    jest.spyOn( useLocationPermission, "default" ).mockImplementation( ( ) => ( {
+      hasPermissions: false,
+      renderPermissionsGate: jest.fn( )
+    } ) );
+    renderComponent(
+      <Match
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...defaultProps}
+        observation={defaultProps.observation}
+      />
+    );
+
+    const addLocationButton = screen.queryByText( /ADD LOCATION FOR BETTER IDS/i );
+    expect( addLocationButton ).toBeVisible();
+  } );
+
+  it( "should not show location permissions button if permissions granted", () => {
+    const mockWatchPosition = jest.fn( ( success, _error, _options ) => success( {
+      coords: {
+        latitude: 56,
+        longitude: 9,
+        accuracy: 8
+      }
+    } ) );
+    Geolocation.watchPosition.mockImplementation( mockWatchPosition );
+    jest.spyOn( useLocationPermission, "default" ).mockImplementation( ( ) => ( {
+      hasPermissions: true,
+      renderPermissionsGate: jest.fn( )
+    } ) );
+
+    renderComponent(
+      <Match
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...defaultProps}
+        observation={{
+          ...defaultProps.observation,
+          latitude: 24,
+          longitude: -24
+        }}
+      />
+    );
+
+    const addLocationButton = screen.queryByText( /ADD LOCATION FOR BETTER IDS/i );
+    expect( addLocationButton ).toBeFalsy( );
+  } );
+} );
