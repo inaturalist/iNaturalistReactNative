6be41e3023d17020b3236c5132088b585728549a
Johannes Klein <johannes.t.klein@gmail.com>
2025-03-06 10:17:16 +0100

    Comment out step in e2e test

--- e2e/sharedFlows/switchPowerMode.js
+++ e2e/sharedFlows/switchPowerMode.js
@@ -11,9 +11,10 @@ export default async function switchPowerMode() {
   await waitFor( settingsDrawerMenuItem ).toBeVisible().withTimeout( 10000 );
   await settingsDrawerMenuItem.tap();
   // Tap the settings switch for advanced interface mode
-  const advancedInterfaceSwitch = element( by.id( "advanced-interface-option" ) );
-  await waitFor( advancedInterfaceSwitch ).toBeVisible().withTimeout( 10000 );
-  await advancedInterfaceSwitch.tap();
+  // TODO: this will be required when the advanced interface mode is toggled off by default
+  // const advancedInterfaceSwitch = element( by.id( "advanced-interface-option" ) );
+  // await waitFor( advancedInterfaceSwitch ).toBeVisible().withTimeout( 10000 );
+  // await advancedInterfaceSwitch.tap();
   // Tap the settings radio button for power user mode
   const powerUserRadioButton = element( by.id( "all-observation-options" ) );
   await waitFor( powerUserRadioButton ).toBeVisible().withTimeout( 10000 );
