7e46357e57d73aa4b66308d2e74ecb1e45032437
Amanda Bullington <albullington@gmail.com>
2025-03-07 12:39:05 -0800

    Reset zoom when tapping a different photo in media viewer

--- src/components/MediaViewer/CustomImageZoom.tsx
+++ src/components/MediaViewer/CustomImageZoom.tsx
@@ -2,7 +2,9 @@
 import { ImageZoom } from "@likashefqet/react-native-image-zoom";
 import type { Node } from "react";
 import React, {
-  useMemo
+  useEffect,
+  useMemo,
+  useRef
 } from "react";
 import useDeviceOrientation from "sharedHooks/useDeviceOrientation.ts";
 
@@ -11,19 +13,35 @@ const MAX_SCALE = 5;
 
 interface Props {
   uri: string
-  setZooming: ( ) => void
+  setZooming: ( ) => void,
+  selectedMediaIndex: number
 }
 
-const CustomImageZoom = ( { uri, setZooming }: Props ): Node => {
+const CustomImageZoom = ( {
+  uri,
+  setZooming,
+  selectedMediaIndex
+}: Props ): Node => {
   const { screenWidth, screenHeight } = useDeviceOrientation( );
+  const imageZoomRef = useRef( null );
 
   const style = useMemo( ( ) => ( {
     height: screenHeight,
     width: screenWidth
   } ), [screenHeight, screenWidth] );
 
+  useEffect( () => {
+    if ( imageZoomRef?.current ) {
+      // Reset zoom when image uri changes. This supports tapping a thumbnail
+      // but not scrolling left/right since we want the user to be able to pan
+      // and drag a zoomed in photo
+      imageZoomRef?.current?.reset( );
+    }
+  }, [selectedMediaIndex] );
+
   return (
     <ImageZoom
+      ref={imageZoomRef}
       testID={`CustomImageZoom.${uri}`}
       uri={uri}
       style={style}
