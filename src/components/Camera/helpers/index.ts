08eced93e92b86c3245155ba63f80fce6f3db0c5
Johannes Klein <johannes.t.klein@gmail.com>
2025-03-08 00:30:27 +0100

    Adding some more info to errors thrown from camera (#2731)
    * Use entire error as param outside of CameraView
    
    * Pass error through to helpers
    
    * Log entire error object to logstash and Alert

--- src/components/Camera/helpers/index.ts
+++ src/components/Camera/helpers/index.ts
@@ -1,38 +1,35 @@
 import { Alert } from "react-native";
+import { CameraRuntimeError } from "react-native-vision-camera";
 
 import { log } from "../../../../react-native-logs.config";
 
 const logger = log.extend( "CameraView" );
 
-type Error = {
-  message: string;
-}
-
 type Event = {
   log: string;
 }
 
-const handleClassifierError = ( error: Error ) => {
+const handleClassifierError = ( error: CameraRuntimeError ) => {
   // When we hit this error, there is an error with the classifier.
-  logger.error( "handleClassifierError: ", error );
-  Alert.alert( "error", error.message );
+  logger.error( "handleClassifierError: ", JSON.stringify( error ) );
+  Alert.alert( "error", JSON.stringify( error ) );
 };
 
-const handleDeviceNotSupported = ( error: Error ) => {
+const handleDeviceNotSupported = ( error: CameraRuntimeError ) => {
   // When we hit this error, something with the current device is not supported.
-  Alert.alert( "error", error.message );
+  Alert.alert( "error", JSON.stringify( error ) );
 };
 
-const handleCaptureError = ( error: Error ) => {
+const handleCaptureError = ( error: CameraRuntimeError ) => {
   // When we hit this error, taking a photo did not work correctly
-  logger.error( "handleCaptureError: ", error );
-  Alert.alert( "error", error.message );
+  logger.error( "handleCaptureError: ", JSON.stringify( error ) );
+  Alert.alert( "error", JSON.stringify( error ) );
 };
 
-const handleCameraError = ( error: Error ) => {
+const handleCameraError = ( error: CameraRuntimeError ) => {
   // This error is thrown when it does not fit in any of the above categories.
-  logger.error( "handleCameraError ", error );
-  Alert.alert( "error", error.message );
+  logger.error( "handleCameraError ", JSON.stringify( error ) );
+  Alert.alert( "error", JSON.stringify( error ) );
 };
 
 const handleLog = ( event: Event ) => {
