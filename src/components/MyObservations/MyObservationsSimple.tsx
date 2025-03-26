6f64f50a9919ef9a1f355e0e8540cbf78a20b380
Johannes Klein <johannes.t.klein@gmail.com>
2025-03-26 07:47:22 +0100

    In obs list show metadata (date, location) for a advanced user (#2798)
    * In obs list show metadata (date, location) for a advanced logged-in user
    
    * Also show date and location for logged out users

--- src/components/MyObservations/MyObservationsSimple.tsx
+++ src/components/MyObservations/MyObservationsSimple.tsx
@@ -278,7 +278,7 @@ const MyObservationsSimple = ( {
               handlePullToRefresh={handlePullToRefresh}
               handleIndividualUploadPress={handleIndividualUploadPress}
               hideLoadingWheel
-              hideMetadata
+              hideMetadata={isDefaultMode}
               hideObsUploadStatus={!currentUser}
               hideObsStatus={isDefaultMode}
               isFetchingNextPage={isFetchingNextPage}
