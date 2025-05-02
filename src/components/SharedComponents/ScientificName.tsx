fef34a44a23fd45f454b8ed52c54be3d659147a1
Johannes Klein <johannes.t.klein@gmail.com>
2025-05-02 11:23:53 +0200

    Migrate flow to TS

--- src/components/SharedComponents/ScientificName.tsx
+++ src/components/SharedComponents/ScientificName.tsx
@@ -1,27 +1,25 @@
-// @flow
 import classNames from "classnames";
 import { Body3 } from "components/SharedComponents";
 import { random } from "lodash";
-import type { Node } from "react";
 import React from "react";
 import Taxon from "realmModels/Taxon";
 import { translatedRank } from "sharedHelpers/taxon";
 import useTranslation from "sharedHooks/useTranslation";
 
-type Props = {
-  fontComponent: Object,
-  isHorizontal: boolean,
-  isFirst?: boolean,
-  isTitle?: boolean,
-  keyBase: string,
-  rank: string,
-  rankLevel: number,
-  rankPiece: string,
-  scientificNamePieces: Object,
-  taxonId: string,
-  textClassName?: string,
-  maxFontSizeMultiplier?: number
-};
+interface Props {
+  fontComponent: Object;
+  isHorizontal: boolean;
+  isFirst?: boolean;
+  isTitle?: boolean;
+  keyBase: string;
+  rank: string;
+  rankLevel: number;
+  rankPiece: string;
+  scientificNamePieces: Object;
+  taxonId: string;
+  textClassName?: string;
+  maxFontSizeMultiplier?: number;
+}
 
 const ScientificName = ( {
   fontComponent,
@@ -36,7 +34,7 @@ const ScientificName = ( {
   taxonId,
   textClassName,
   maxFontSizeMultiplier
-}: Props ): Node => {
+}: Props ) => {
   const { t } = useTranslation( );
   const scientificNameArray = scientificNamePieces?.map( ( piece, index ) => {
     const isItalics = piece !== rankPiece && (
