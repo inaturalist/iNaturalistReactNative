70bfcd2fa6900eb02d6e195125af8c18ce5de4c2
Johannes Klein <johannes.t.klein@gmail.com>
2025-05-12 21:43:20 +0200

    Add Boolean to control button being shown
    Same Button component, only with changed indentation

--- src/components/ObsEdit/IdentificationSection.js
+++ src/components/ObsEdit/IdentificationSection.js
@@ -81,32 +81,37 @@ const IdentificationSection = ( {
     }
   }, [resetScreen, setResetScreen] );
 
+  const showIDButton = false;
   const renderIconicTaxonChooser = ( ) => (
     <View key={resetScreen?.toString( )}>
       <IconicTaxonChooser
         before={(
           <View className="flex-row">
-            <Button
-              level={identTaxon
-                ? "neutral"
-                : "focus"}
-              onPress={navToSuggestions}
-              text={t( "ID-WITH-AI" )}
-              className={classnames( "rounded-full py-1 h-[36px] ml-6", {
-                "border border-darkGray border-[2px]": identTaxon
-              } )}
-              testID="ObsEdit.Suggestions"
-              icon={(
-                <INatIcon
-                  name="sparkly-label"
-                  size={24}
-                  color={identTaxon
-                    ? colors.darkGray
-                    : colors.white}
+            {showIDButton
+              ? (
+                <Button
+                  level={identTaxon
+                    ? "neutral"
+                    : "focus"}
+                  onPress={navToSuggestions}
+                  text={t( "ID-WITH-AI" )}
+                  className={classnames( "rounded-full py-1 h-[36px] ml-6", {
+                    "border border-darkGray border-[2px]": identTaxon
+                  } )}
+                  testID="ObsEdit.Suggestions"
+                  icon={(
+                    <INatIcon
+                      name="sparkly-label"
+                      size={24}
+                      color={identTaxon
+                        ? colors.darkGray
+                        : colors.white}
+                    />
+                  )}
+                  accessibilityLabel={t( "View-suggestions" )}
                 />
-              )}
-              accessibilityLabel={t( "View-suggestions" )}
-            />
+              )
+              : null}
             <Button
               level="neutral"
               onPress={navToSuggestionsSearch}
