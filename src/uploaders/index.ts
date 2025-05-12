f592993889531e878f8d279b6a92e57df407d468
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2025-05-12 12:16:52 -0700

    Maintenance: break upload code into smaller modules -- data transformation (#2884)
    * Create module for realm syncing and write unit tests before refactoring
    
    * Refactor realmSync functions for readability & make sure realm errors aren't silently swallowed
    
    * unit tests
    
    * Update tests to use factory functions for media
    
    * Update tests to use factory
    
    * Remove sanitize function since it's redundant with mapping; move mapping & add unit tests
    
    ---------
    
    Co-authored-by: Johannes Klein <johannes.t.klein@gmail.com>

--- src/uploaders/index.ts
+++ src/uploaders/index.ts
@@ -1,2 +1,8 @@
+export {
+  default as prepareMediaForUpload
+} from "./dataTransformation/prepareMediaForUpload";
+export {
+  default as prepareObservationForUpload
+} from "./dataTransformation/prepareObservationForUpload";
 export { default as handleUploadError } from "./utils/errorHandling";
 export { default as markRecordUploaded } from "./utils/realmSync";
