f5239881dd61cb2e5ddbdb9cb6c303bf0b15cef0
Amanda Bullington <35536439+albullington@users.noreply.github.com>
2025-05-21 14:40:54 -0700

    Speed up Explore nearby tile loading (#2912)
    * Create a performance tracker to show urltile load time in debug mode
    
    * Set default radius to 1km, not 50km
    
    * Code cleanup & TypeScript definitions
    
    * Fix TypeError
    
    * Address eslint error
    
    * Make github actions happy
    
    ---------
    
    Co-authored-by: Johannes Klein <johannes.t.klein@gmail.com>

---
+++ src/sharedHelpers/mapPerformanceTracker.ts
@@ -0,0 +1,48 @@
+class MapPerformanceTracker {
+  private screenLoadTime: number;
+
+  private mapReadyStartTime: number;
+
+  private tilesVisibleStartTime: number;
+
+  private mapReadyDuration: number;
+
+  private tilesVisibleDuration: number;
+
+  constructor( ) {
+    this.reset( );
+  }
+
+  reset( ): void {
+    // Timestamps
+    this.screenLoadTime = Date.now();
+    this.mapReadyStartTime = 0;
+    this.tilesVisibleStartTime = 0;
+
+    // Calculated durations (in ms)
+    this.mapReadyDuration = 0;
+    this.tilesVisibleDuration = 0;
+  }
+
+  markMapReady( ): void {
+    this.mapReadyStartTime = Date.now();
+    this.mapReadyDuration = this.mapReadyStartTime - this.screenLoadTime;
+  }
+
+  markTilesVisible( ): void {
+    if ( this.tilesVisibleStartTime === 0 ) {
+      this.tilesVisibleStartTime = Date.now();
+      this.tilesVisibleDuration = this.tilesVisibleStartTime - this.screenLoadTime;
+    }
+  }
+
+  getSummary( ): { mapReadyTime: number; tilesVisibleTime: number } {
+    return {
+      mapReadyTime: this.mapReadyDuration,
+      tilesVisibleTime: this.tilesVisibleDuration
+    };
+  }
+}
+
+const mapTracker = new MapPerformanceTracker( );
+export default mapTracker;
