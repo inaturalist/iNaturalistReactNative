627b970a383f6dbf462ef2fabe1a714d63675868
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-26 16:17:12 +0200

    Basic test for tooltip

--- tests/unit/components/AddObsModal/AddObsButton.test.js
+++ tests/unit/components/AddObsModal/AddObsButton.test.js
@@ -1,7 +1,9 @@
 import { screen } from "@testing-library/react-native";
 import AddObsButton from "components/AddObsModal/AddObsButton";
 import React from "react";
-// import useStore, { zustandStorage } from "stores/useStore";
+import * as useCurrentUser from "sharedHooks/useCurrentUser.ts";
+import { zustandStorage } from "stores/useStore";
+import factory from "tests/factory";
 import { renderComponent } from "tests/helpers/render";
 
 // Mock getCurrentRoute to return ObsList
@@ -9,6 +11,13 @@ jest.mock( "navigation/navigationUtils", () => ( {
   getCurrentRoute: () => ( {
     name: "ObsList"
   } )
+} ) );
+
+const mockUser = factory( "LocalUser" );
+
+jest.mock( "sharedHooks/useCurrentUser", () => ( {
+  __esModule: true,
+  default: () => undefined
 } ) );
 
 describe( "AddObsButton", () => {
@@ -23,23 +32,38 @@ describe( "AddObsButton", () => {
     // Snapshot test
     expect( screen ).toMatchSnapshot();
   } );
+
+  it( "does not render tooltip in default state", () => {
+    renderComponent( <AddObsButton /> );
+
+    const tooltipText = screen.queryByText(
+      "Press and hold to view more options"
+    );
+    expect( tooltipText ).toBeFalsy();
+  } );
 } );
 
-// describe( "shows tooltip", () => {
-//   beforeAll( ( ) => {
+describe( "shows tooltip", () => {
+  it( "for logged out users with 2 observations", async () => {
+    zustandStorage.setItem( "numOfUserObservations", 2 );
 
-//   } );
-//   it( "for logged out users with 2 observations", async () => {
-//     renderComponent( <AddObsButton /> );
+    renderComponent( <AddObsButton /> );
 
-//     useStore.setState( {
-//       numOfUserObservations: 2
-//     } );
-//     // setStoreStateLayout( {
-//     //   isDefaultMode: false
-//     // } );
+    const tooltipText = await screen.findByText(
+      "Press and hold to view more options"
+    );
+    expect( tooltipText ).toBeTruthy();
+  } );
 
-//     const tooltipText = await screen.findByText( "Press and hold to view more options" );
-//     expect( tooltipText ).toBeVisible();
-//   } );
-// } );
+  it( "for logged in users with less than 50 observations", async () => {
+    zustandStorage.setItem( "numOfUserObservations", 2 );
+    jest.spyOn( useCurrentUser, "default" ).mockImplementation( () => mockUser );
+
+    renderComponent( <AddObsButton /> );
+
+    const tooltipText = await screen.findByText(
+      "Press and hold to view more options"
+    );
+    expect( tooltipText ).toBeTruthy();
+  } );
+} );
