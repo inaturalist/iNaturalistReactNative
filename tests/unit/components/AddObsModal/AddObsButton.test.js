a067a8bc2dd1f050086da7dd159c385b06529681
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-27 10:51:02 +0200

    Add tests for two conditions that require a card dismissal

--- tests/unit/components/AddObsModal/AddObsButton.test.js
+++ tests/unit/components/AddObsModal/AddObsButton.test.js
@@ -5,6 +5,7 @@ import * as useCurrentUser from "sharedHooks/useCurrentUser.ts";
 import { zustandStorage } from "stores/useStore";
 import factory from "tests/factory";
 import { renderComponent } from "tests/helpers/render";
+import setStoreStateLayout from "tests/helpers/setStoreStateLayout";
 
 // Mock getCurrentRoute to return ObsList
 jest.mock( "navigation/navigationUtils", () => ( {
@@ -44,7 +45,7 @@ describe( "AddObsButton", () => {
 } );
 
 describe( "shows tooltip", () => {
-  it( "for logged out users with 2 observations", async () => {
+  it( "to logged out users with 2 observations", async () => {
     zustandStorage.setItem( "numOfUserObservations", 2 );
 
     renderComponent( <AddObsButton /> );
@@ -55,7 +56,7 @@ describe( "shows tooltip", () => {
     expect( tooltipText ).toBeTruthy();
   } );
 
-  it( "for logged in users with less than 50 observations", async () => {
+  it( "to logged in users with less than 50 observations", async () => {
     zustandStorage.setItem( "numOfUserObservations", 2 );
     jest.spyOn( useCurrentUser, "default" ).mockImplementation( () => mockUser );
 
@@ -65,5 +66,52 @@ describe( "shows tooltip", () => {
       "Press and hold to view more options"
     );
     expect( tooltipText ).toBeTruthy();
+  } );
+
+  it( "to new users only after they dismissed the account creation card", async () => {
+    zustandStorage.setItem( "numOfUserObservations", 1 );
+    setStoreStateLayout( {
+      justFinishedSignup: true
+    } );
+
+    renderComponent( <AddObsButton /> );
+
+    const tooltipText = screen.queryByText(
+      "Press and hold to view more options"
+    );
+    expect( tooltipText ).toBeFalsy();
+
+    setStoreStateLayout( {
+      shownOnce: {
+        "account-creation": true
+      }
+    } );
+
+    const tooltipTextAfter = await screen.findByText(
+      "Press and hold to view more options"
+    );
+    expect( tooltipTextAfter ).toBeTruthy();
+  } );
+
+  it( "to logged in users with more than 50 observations after card dismissal", async () => {
+    zustandStorage.setItem( "numOfUserObservations", 51 );
+
+    renderComponent( <AddObsButton /> );
+
+    const tooltipText = screen.queryByText(
+      "Press and hold to view more options"
+    );
+    expect( tooltipText ).toBeFalsy();
+
+    setStoreStateLayout( {
+      shownOnce: {
+        "fifty-observation": true
+      }
+    } );
+
+    const tooltipTextAfter = await screen.findByText(
+      "Press and hold to view more options"
+    );
+    expect( tooltipTextAfter ).toBeTruthy();
   } );
 } );
