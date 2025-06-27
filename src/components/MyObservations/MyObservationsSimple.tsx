895bab8ffac409b51b83c3ed30613f6f62c2200f
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-27 23:10:45 +0200

    Remove tenth observation pivot card (#2986)

--- src/components/MyObservations/MyObservationsSimple.tsx
+++ src/components/MyObservations/MyObservationsSimple.tsx
@@ -7,8 +7,7 @@ import {
   AccountCreationCard,
   FiftyObservationCard,
   FiveObservationCard,
-  OneObservationCard,
-  TenObservationCard
+  OneObservationCard
 } from "components/OnboardingModal/PivotCards.tsx";
 import {
   Body1,
@@ -367,7 +366,6 @@ const MyObservationsSimple = ( {
             }}
           />
           <FiveObservationCard triggerCondition={numTotalObservations === 5} />
-          <TenObservationCard triggerCondition={numTotalObservations === 10} />
           <FiftyObservationCard
             triggerCondition={
               loggedInWhileInDefaultMode && !!currentUser && numTotalObservations >= 50
