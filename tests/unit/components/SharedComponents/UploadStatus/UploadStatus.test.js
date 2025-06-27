f2145dc86dabab16587b73609d706870a7998e8e
Johannes Klein <johannes.t.klein@gmail.com>
2025-06-28 00:29:21 +0200

    Update react-native-reanimated (#2987)
    * Update package.json
    
    * Update package-lock.json
    
    * Update Podfile.lock
    
    * Reactivate snapshot tests
    
    * Update Podfile.lock

--- tests/unit/components/SharedComponents/UploadStatus/UploadStatus.test.js
+++ tests/unit/components/SharedComponents/UploadStatus/UploadStatus.test.js
@@ -13,60 +13,28 @@ jest.mock( "react-native-circular-progress-indicator", () => "CircularProgress" 
 describe( "UploadStatus", () => {
   it( "displays start icon when upload is unsynced and not queued", async () => {
     render( <UploadStatus progress={0} queued={false} /> );
+
     expect( screen.getByTestId( /UploadIcon.start/ ) ).toBeTruthy();
-    // TODO: (Johannes 2025-06-13) An update to react-native-reanimated (to 3.16.6) caused this test
-    // to fail. However, only the snapshot matching is affected, the component still renders
-    // correctly, and the test passes, i.e. the correct icon is shown. The actual error thrown is a
-    // recursion happening in the pretty-format library. So, since we are currently only half-way
-    // through our updating process of major react-native versions and libraries I vouch to comment
-    // out these snapshot tests for now and reinstate them later with the latest RN version.
-    /*
-      RangeError: Invalid string length
-      at printObjectProperties (node_modules/pretty-format/build/collections.js:170:47)
-      at key (node_modules/pretty-format/build/index.js:386:38)
-      at AsymmetricMatcher (node_modules/pretty-format/build/index.js:469:3)
-      at printObjectProperties (node_modules/pretty-format/build/collections.js:169:21)
-      at key (node_modules/pretty-format/build/index.js:386:38)
-      at AsymmetricMatcher (node_modules/pretty-format/build/index.js:469:3)
-      at printObjectProperties (node_modules/pretty-format/build/collections.js:169:21)
-      at key (node_modules/pretty-format/build/index.js:386:38)
-      at AsymmetricMatcher (node_modules/pretty-format/build/index.js:469:3)
-    */
-    // expect( screen ).toMatchSnapshot();
+    expect( screen ).toMatchSnapshot();
   } );
 
   it( "displays rotating circle progress when upload is queued but not started", async () => {
     render( <UploadStatus progress={0} queued /> );
+
     expect( screen.getByTestId( "UploadStatus.QueuedRotatingIcon" ) ).toBeTruthy();
-    // TODO: (Johannes 2025-06-13) An update to react-native-reanimated (to 3.16.6) caused this test
-    // to fail. However, only the snapshot matching is affected, the component still renders
-    // correctly, and the test passes, i.e. the correct icon is shown. The actual error thrown is a
-    // recursion happening in the pretty-format library. So, since we are currently only half-way
-    // through our updating process of major react-native versions and libraries I vouch to comment
-    // out these snapshot tests for now and reinstate them later with the latest RN version.
-    /*
-      RangeError: Invalid string length
-      at printObjectProperties (node_modules/pretty-format/build/collections.js:170:47)
-      at key (node_modules/pretty-format/build/index.js:386:38)
-      at AsymmetricMatcher (node_modules/pretty-format/build/index.js:469:3)
-      at printObjectProperties (node_modules/pretty-format/build/collections.js:169:21)
-      at key (node_modules/pretty-format/build/index.js:386:38)
-      at AsymmetricMatcher (node_modules/pretty-format/build/index.js:469:3)
-      at printObjectProperties (node_modules/pretty-format/build/collections.js:169:21)
-      at key (node_modules/pretty-format/build/index.js:386:38)
-      at AsymmetricMatcher (node_modules/pretty-format/build/index.js:469:3)
-    */
-    // expect( screen ).toMatchSnapshot();
+    expect( screen ).toMatchSnapshot();
   } );
 
   it( "displays progress bar when progress is greater than 5%", async () => {
     render( <UploadStatus progress={0.5} /> );
+
     expect( screen.getByTestId( "UploadStatus.CircularProgress" ) ).toBeTruthy();
     expect( screen ).toMatchSnapshot();
   } );
 
   it( "displays complete icon when progress is 1", async () => {
     render( <UploadStatus progress={1} /> );
+
     expect( screen.getByTestId( "UploadStatus.Complete" ) ).toBeTruthy();
     expect( screen ).toMatchSnapshot();
   } );
