829ec666f11f96a2dfd840326621d91d401aec66
Johannes Klein <johannes.t.klein@gmail.com>
2025-07-07 16:46:31 +0200

    Add TODO

--- src/realmModels/ObservationPhoto.js
+++ src/realmModels/ObservationPhoto.js
@@ -68,6 +68,9 @@ class ObservationPhoto extends Realm.Object {
     };
   }
 
+  // TODO: I don't know how what the type for this is, I think it is only called
+  // after certain transformations on the Realm result, but it is not important for my current
+  // linear ticket so I'll skip it
   static mapObservationPhotoForMyObsDefaultMode( observationPhoto ) {
     return {
       photo: {
