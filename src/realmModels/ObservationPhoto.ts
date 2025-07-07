19aa9e526cae7cc532557aa2c62ee4b96a9f8702
Johannes Klein <johannes.t.klein@gmail.com>
2025-07-07 14:44:17 +0200

    Add param type

--- src/realmModels/ObservationPhoto.ts
+++ src/realmModels/ObservationPhoto.ts
@@ -1,4 +1,5 @@
 import { Realm } from "@realm/react";
+import type { ApiObservationPhoto } from "api/types";
 import { getJWT } from "components/LoginSignUp/AuthenticationService.ts";
 import inatjs, { FileUpload } from "inaturalistjs";
 import * as uuid from "uuid";
@@ -37,7 +38,7 @@ class ObservationPhoto extends Realm.Object {
     return this._synced_at !== null;
   }
 
-  static mapApiToRealm( observationPhoto, realm = null ) {
+  static mapApiToRealm( observationPhoto: ApiObservationPhoto, realm = null ) {
     const localObsPhoto = {
       ...observationPhoto,
       _synced_at: new Date( ),
