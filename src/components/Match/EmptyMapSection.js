325be3451bb894e61b9d6df8f149f6010d7c01b6
Yaron Budowski <budowski@gmail.com>
2025-07-08 12:02:58 +0200

    Various fixes according to PR feedback

--- src/components/Match/EmptyMapSection.js
+++ src/components/Match/EmptyMapSection.js
@@ -1,12 +1,11 @@
 import {
-  Heading4,
-  INatIcon
+  Button
 } from "components/SharedComponents";
-import { ImageBackground, Pressable } from "components/styledComponents";
+import { ImageBackground } from "components/styledComponents";
+import LocationIndicatorIcon from "images/svg/location_indicator.svg";
 import React from "react";
 import { View } from "react-native";
 import { useTranslation } from "sharedHooks";
-import colors from "styles/tailwindColors";
 
 type Props = {
   handleAddLocationPressed: ( ) => void
@@ -23,18 +22,20 @@ const EmptyMapSection = ( {
         source={require( "images/topographic-map.png" )}
         accessibilityIgnoresInvertColors
       >
-        <INatIcon name="map-marker-outline" size={40} color={colors.white} />
+        <LocationIndicatorIcon
+          testID="Map.LocationIndicator"
+          width={40}
+          height={40}
+        />
 
-        <Pressable
-          className="mt-3 px-14 py-4 rounded-lg bg-white ml-2 mr-2 active:opacity-75"
-          accessibilityLabel={t( "ADD-LOCATION-FOR-BETTER-IDS" )}
-          accessibilityRole="link"
+        <Button
+          className="mt-5 pl-6 pr-6 border-0"
+          level="neutral"
+          text={t( "ADD-LOCATION-FOR-BETTER-IDS" )}
           onPress={handleAddLocationPressed}
-        >
-          <Heading4 className="text-darkGray text-center">
-            {t( "ADD-LOCATION-FOR-BETTER-IDS" )}
-          </Heading4>
-        </Pressable>
+          accessibilityLabel={t( "Edit-location" )}
+          accessibilityHint={t( "Add-location-to-refresh-suggestions" )}
+        />
       </ImageBackground>
     </View>
   );
