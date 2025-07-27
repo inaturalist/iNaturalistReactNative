3f0411e0b2c71792b5bafc7630bbc973b2ad4a1e
Johannes Klein <johannes.t.klein@gmail.com>
2025-07-28 00:49:13 +0200

    Update FullScreenActivityIndicator.tsx

--- src/components/MyObservations/FullScreenActivityIndicator.tsx
+++ src/components/MyObservations/FullScreenActivityIndicator.tsx
@@ -1,11 +1,8 @@
-// @flow
-
 import { ActivityIndicator } from "components/SharedComponents";
 import { View } from "components/styledComponents";
-import type { Node } from "react";
 import React from "react";
 
-const FullScreenActivityIndicator = ( ): Node => (
+const FullScreenActivityIndicator = () => (
   <View
     className="flex-1 justify-center items-center bg-white"
     testID="FullScreenActivityIndicator"
