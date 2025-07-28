00fe8d0a46afe72c9400a29ef3e2790fc169d148
Johannes Klein <johannes.t.klein@gmail.com>
2025-07-28 17:25:52 +0200

    Update TextSheet.tsx

--- src/components/SharedComponents/Sheets/TextSheet.tsx
+++ src/components/SharedComponents/Sheets/TextSheet.tsx
@@ -1,24 +1,21 @@
-// @flow
-
 import {
   Body3, BottomSheet, Button
 } from "components/SharedComponents";
 import { View } from "components/styledComponents";
-import type { Node } from "react";
 import React, { useCallback } from "react";
 import useTranslation from "sharedHooks/useTranslation.ts";
 
-type Props = {
-  headerText: string,
-  texts: [string],
-  setShowSheet: Function
+interface Props {
+  headerText: string;
+  texts: string[];
+  setShowSheet: ( show: boolean ) => void;
 }
 
 const TextSheet = ( {
   headerText,
   texts,
   setShowSheet
-}: Props ): Node => {
+}: Props ) => {
   const { t } = useTranslation( );
 
   const onPressClose = useCallback(
