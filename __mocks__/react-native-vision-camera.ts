3b8170b4476dda5ee86eb9a7b0450a9f8081447c
Johannes Klein <johannes.t.klein@gmail.com>
2025-08-13 14:58:16 +0200

    Add missing mock

--- __mocks__/react-native-vision-camera.ts
+++ __mocks__/react-native-vision-camera.ts
@@ -2,12 +2,14 @@ import {
   mockCamera,
   mockSortDevices,
   mockUseCameraDevice,
+  mockUseCameraDevices,
   mockUseCameraFormat
 } from "tests/vision-camera/vision-camera";
 
 export const Camera = mockCamera;
 export const sortDevices = mockSortDevices;
 export const useCameraDevice = mockUseCameraDevice;
+export const useCameraDevices = mockUseCameraDevices;
 export const useCameraFormat = mockUseCameraFormat;
 export const VisionCameraProxy = {
   initFrameProcessorPlugin: jest.fn( )
