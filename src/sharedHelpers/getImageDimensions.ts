d6753a9d2cd0d27fdafa2830784476cad95eb022
Corey Farwell <coreyf@rwell.org>
2025-08-27 10:53:03 -0400

    Migrate `src/sharedHelpers/getImageDimensions` to TypeScript.

--- src/sharedHelpers/getImageDimensions.ts
+++ src/sharedHelpers/getImageDimensions.ts
@@ -1,7 +1,12 @@
 import { Image } from "react-native";
 
+type ImageDimensions = {
+  width: number;
+  height: number;
+};
+
 // A helper function to get the image dimensions
-const getImageDimensions = async uri => new Promise( resolve => {
+const getImageDimensions = async ( uri: string ) => new Promise<ImageDimensions>( resolve => {
   Image.getSize( uri, ( width, height ) => {
     resolve( { width, height } );
   } );
