3c04df39a3bd25abc9924cd36da3060a380e55e6
Johannes Klein <johannes.t.klein@gmail.com>
2025-09-27 08:28:13 -0600

    Enable New Architecture (#3111)
    * Update gradle.properties
    
    * Update Podfile
    
    * Update react-native-mmkv
    
    * Update Podfile.lock
    
    * Delete useObservationsUpdatesWhenFocused.test.js
    
    * Update closeOnboarding.js
    
    * Fix failing button tap in e2e tests
    
    * Create react-native-sensitive-info+6.0.0-alpha.9.patch
    
    * Update bottom-sheets
    
    * Refactor e2e timeout to file-wide const
    
    * Remove check that fails
    
    * Check for entire list item instead of comments count
    
    * Longer delay to wait for observation deletion to make UI disappear

--- e2e/sharedFlows/switchPowerMode.js
+++ e2e/sharedFlows/switchPowerMode.js
@@ -2,16 +2,18 @@ import {
   by, element, waitFor
 } from "detox";
 
+const TIMEOUT = 10_000;
+
 export default async function switchPowerMode() {
   const drawerButton = element( by.id( "OPEN_DRAWER" ) );
-  await waitFor( drawerButton ).toBeVisible().withTimeout( 10000 );
-  await drawerButton.tap();
+  await waitFor( drawerButton ).toBeVisible().withTimeout( TIMEOUT );
+  await drawerButton.tap( { x: 0, y: 0 } );
   // Tap the settings drawer menu item
   const settingsDrawerMenuItem = element( by.id( "settings" ) );
-  await waitFor( settingsDrawerMenuItem ).toBeVisible().withTimeout( 10000 );
+  await waitFor( settingsDrawerMenuItem ).toBeVisible().withTimeout( TIMEOUT );
   await settingsDrawerMenuItem.tap();
   // Switch settings to advanced interface mode
   const advancedInterfaceSwitch = element( by.id( "advanced-interface-switch.switch" ) );
-  await waitFor( advancedInterfaceSwitch ).toBeVisible().withTimeout( 10000 );
+  await waitFor( advancedInterfaceSwitch ).toBeVisible().withTimeout( TIMEOUT );
   await advancedInterfaceSwitch.tap();
 }
