68adfa16a92e87275d60f7fcc74fec507ba87477
Johannes Klein <johannes.t.klein@gmail.com>
2025-10-14 00:24:41 +0200

    What does happen?

--- .detoxrc.js
+++ .detoxrc.js
@@ -56,8 +56,7 @@ module.exports = {
     simulator: {
       type: "ios.simulator",
       device: {
-        type: "iPhone 16 Pro",
-        os: "iOS 18.6"
+        type: "iPhone 16 Pro"
       }
     },
     emulator: {
