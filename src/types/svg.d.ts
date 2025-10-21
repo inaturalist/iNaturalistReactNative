15254d544202fb0b8afc9b0e87c297771b7db388
Corey Farwell <coreyf@rwell.org>
2025-10-21 12:01:44 -0400

    Split up declarations file

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,5 +1,3 @@
-import { RootStackParamList } from "@react-navigation/native";
-
 // Using typescript with react-native-svg-transformer
 // from https://www.npmjs.com/package/react-native-svg-transformer?activeTab
 declare module "*.svg" {
@@ -8,10 +6,4 @@ declare module "*.svg" {
 
   const content: FC<SvgProps>;
   export default content;
-}
-
-declare global {
-  namespace ReactNavigation {
-    type RootParamList = RootStackParamList
-  }
 }
