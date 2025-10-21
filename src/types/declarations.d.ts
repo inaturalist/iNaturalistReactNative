125bcb465c2bc5a677e413b593a7649375cdb106
Corey Farwell <coreyf@rwell.org>
2025-10-21 10:51:07 -0400

    Fix the existing SVG declaration

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -3,10 +3,10 @@ import { RootStackParamList } from "@react-navigation/native";
 // Using typescript with react-native-svg-transformer
 // from https://www.npmjs.com/package/react-native-svg-transformer?activeTab
 declare module "*.svg" {
-  import React from "react";
+  import { FC } from "react";
   import { SvgProps } from "react-native-svg";
 
-  const content: React.FC<SvgProps>;
+  const content: FC<SvgProps>;
   export default content;
 }
 
