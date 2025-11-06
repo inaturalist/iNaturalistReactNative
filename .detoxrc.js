419ab28073f3813d98178a407c36ad341f1d2b7c
Ryan Stelly <ryan.stelly@live.com>
2025-11-06 15:41:08 -0600

    deactivate firebase, use a google api image

--- .detoxrc.js
+++ .detoxrc.js
@@ -66,7 +66,7 @@ module.exports = {
     emulator: {
       type: "android.emulator",
       device: {
-        avdName: "Pixel_5_API_31_AOSP"
+        avdName: "Medium_Phone_API_36.1"
       }
     }
   },
