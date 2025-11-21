221ef816d4c4b7242e2ee1d516969919a7773872
sepeterson <10458078+sepeterson@users.noreply.github.com>
2025-11-21 11:18:19 -0600

    MOB-991 revise screen tracking

--- src/sharedHelpers/tracking.ts
+++ src/sharedHelpers/tracking.ts
@@ -1,4 +1,6 @@
-import analytics from "@react-native-firebase/analytics";
+import {
+  getAnalytics, logEvent, logScreenView
+} from "@react-native-firebase/analytics";
 import { log } from "sharedHelpers/logger";
 
 const logger = log.extend( "tracing.ts" );
@@ -8,11 +10,23 @@ type FirebaseParameters = Record<string, string | number | string[]>
 // eslint-disable-next-line import/prefer-default-export
 export const logFirebaseEvent = (
   eventId: string,
-  parameters: FirebaseParameters
+  parameters?: FirebaseParameters
 ) => {
   // eslint-disable-next-line no-undef
   if ( __DEV__ ) {
     logger.info( `Firebase event: ${eventId} ${JSON.stringify( parameters )}` );
   }
-  void analytics().logEvent( eventId, parameters );
+  const analytics = getAnalytics();
+  logEvent( analytics, eventId, parameters );
+};
+
+export const logFirebaseScreenView = (
+  screenName: string
+) => {
+  // eslint-disable-next-line no-undef
+  if ( __DEV__ ) {
+    logger.info( `Firebase screen view: ${screenName}` );
+  }
+  const analytics = getAnalytics();
+  logScreenView( analytics, { screen_name: screenName, screen_class: screenName } );
 };
