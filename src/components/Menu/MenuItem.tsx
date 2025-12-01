120ee8ce65d0488cac0b1e7f0a31a66fb405a2e3
Abbey Campbell <campbellabbeya@gmail.com>
2025-12-01 14:18:25 -0800

    add menu screen

---
+++ src/components/Menu/MenuItem.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+import { Heading4, INatIcon } from "components/SharedComponents";
+import { Pressable, View } from "components/styledComponents";
+import React from "react";
+
+import { MenuOption } from "./Menu";
+
+const MenuItem = ( {
+  item,
+  onPress
+}: {
+  item: MenuOption;
+  onPress: ( ) => void;
+} ) => (
+  <Pressable
+    className={classNames(
+      item.isLogout
+        ? "opacity-50"
+        : "",
+      "flex-row items-center pl-10 py-5 border-b border-lightGray"
+    )}
+    accessibilityRole="button"
+    accessibilityLabel={item.label}
+    onPress={onPress}
+  >
+    <View className="mr-5">
+      <INatIcon name={item.icon} size={22} color={item.color} />
+    </View>
+    <Heading4>{item.label}</Heading4>
+  </Pressable>
+);
+
+export default MenuItem;
