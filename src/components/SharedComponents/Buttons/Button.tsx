cc24f7e542050240eff54c714de5f8d5b6735b1b
Corey Farwell <coreyf@rwell.org>
2025-12-09 21:00:05 -0500

    Remove only type error in `Button` component.
    `className` is not a prop on this component.

--- src/components/SharedComponents/Buttons/Button.tsx
+++ src/components/SharedComponents/Buttons/Button.tsx
@@ -238,7 +238,6 @@ const Button = ( {
       {loading && (
         <ActivityIndicator
           size={18}
-          className="mr-3 absolute right-0"
           color={!isNeutral
             ? activityIndicatorColor( {
               isPrimary, isWarning, isFocus
