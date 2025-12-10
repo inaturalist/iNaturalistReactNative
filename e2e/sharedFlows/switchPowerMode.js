f05d79be2424067e34d9cbeaeaf3cd43945a20b5
Abbey Campbell <campbellabbeya@gmail.com>
2025-12-09 18:30:25 -0800

    remove isTest flag, override bottom-tabs animation in jest setup, add comments to detox fix

--- e2e/sharedFlows/switchPowerMode.js
+++ e2e/sharedFlows/switchPowerMode.js
@@ -7,6 +7,8 @@ const TIMEOUT = 10_000;
 
 export default async function switchPowerMode() {
   const menuButton = element( by.id( "OPEN_MENU" ) );
+  // Temporarily disable synchronization to work around fade animation timing
+  // issues that can cause Detox to wait indefinitely for animations to complete
   await device.disableSynchronization();
   await waitFor( menuButton ).toBeVisible().withTimeout( TIMEOUT );
   await menuButton.tap( { x: 0, y: 0 } );
