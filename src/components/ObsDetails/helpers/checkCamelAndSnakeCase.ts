27f3d75202b482b17cf4e021bdaf2f355a77b7d5
Corey Farwell <coreyf@rwell.org>
2025-12-11 10:26:31 -0500

    Fetching from an object can return any type

--- src/components/ObsDetails/helpers/checkCamelAndSnakeCase.ts
+++ src/components/ObsDetails/helpers/checkCamelAndSnakeCase.ts
@@ -2,7 +2,7 @@ const checkCamelAndSnakeCase = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   object: Record<string, any>,
   camelCaseKey: string
-): string|undefined => {
+): any => { // eslint-disable-line @typescript-eslint/no-explicit-any
   if ( !object ) { return ""; }
   const snakeCaseKey = camelCaseKey.replace( /[A-Z]/g, letter => `_${letter.toLowerCase()}` );
 
