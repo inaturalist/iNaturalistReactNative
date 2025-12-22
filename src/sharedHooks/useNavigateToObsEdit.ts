3093b4f113dd6f97a6c72c92ce78abb3ed1227aa
sepeterson <10458078+sepeterson@users.noreply.github.com>
2025-12-22 16:33:28 -0600

    MOB-1077: simple navigation.navigate for ObsEdit

--- src/sharedHooks/useNavigateToObsEdit.ts
+++ src/sharedHooks/useNavigateToObsEdit.ts
@@ -1,36 +1,17 @@
-import { CommonActions, useNavigation } from "@react-navigation/native";
+import type { ParamListBase } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import type { RealmObservation } from "realmModels/types";
 import useStore from "stores/useStore";
 
 function useNavigateToObsEdit() {
-  const navigation = useNavigation( );
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
   const prepareObsEdit = useStore( state => state.prepareObsEdit );
   const setMyObsOffsetToRestore = useStore( state => state.setMyObsOffsetToRestore );
 
   function navigateToObsEdit( localObservation: RealmObservation ) {
     prepareObsEdit( localObservation );
-    // since we can access ObsEdit from two separate stacks, the TabStackNavigator
-    // and the NoBottomTabStackNavigator, we don't want ObsEdit to land on the previous
-    // history of the NoBottomTabStackNavigator (i.e. anything from the ObsCreate flow)
-    // when we're navigating via the TabStack (i.e. MyObservations, ObsDetails)
-    navigation.dispatch(
-      CommonActions.reset( {
-        index: 0,
-        routes: [
-          {
-            name: "NoBottomTabStackNavigator",
-            state: {
-              index: 0,
-              routes: [
-                {
-                  name: "ObsEdit"
-                }
-              ]
-            }
-          }
-        ]
-      } )
-    );
+    navigation.navigate( "ObsEdit" );
     setMyObsOffsetToRestore();
   }
 
