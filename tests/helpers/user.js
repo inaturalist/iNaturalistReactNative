113a5c1cf7884ca36071cac6198d494f7fa08341
Johannes Klein <johannes.t.klein@gmail.com>
2026-02-12 14:17:28 +0100

    Update test helpers

--- tests/helpers/user.js
+++ tests/helpers/user.js
@@ -17,6 +17,9 @@ import { makeResponse } from "tests/factory";
 
 const TEST_JWT = "test-json-web-token";
 const TEST_ACCESS_TOKEN = "test-access-token";
+const rnsiOptions = {
+  keychainService: "app",
+};
 
 async function signOut( options = {} ) {
   const realm = options.realm || global.realm;
@@ -32,20 +35,20 @@ async function signOut( options = {} ) {
   }, "deleting entire realm in signOut function, user.js" );
   const systemLocale = getInatLocaleFromSystemLocale( );
   changeLanguage( systemLocale );
-  await RNSInfo.deleteItem( "username" );
-  await RNSInfo.deleteItem( "jwtToken" );
-  await RNSInfo.deleteItem( "jwtGeneratedAt" );
-  await RNSInfo.deleteItem( "accessToken" );
+  await RNSInfo.deleteItem( "username", rnsiOptions );
+  await RNSInfo.deleteItem( "jwtToken", rnsiOptions );
+  await RNSInfo.deleteItem( "jwtGeneratedAt", rnsiOptions );
+  await RNSInfo.deleteItem( "accessToken", rnsiOptions );
   clearAuthCache( );
   inatjs.users.me.mockClear( );
 }
 
 async function signIn( user, options = {} ) {
   const realm = options.realm || global.realm;
-  await RNSInfo.setItem( "username", user.login );
-  await RNSInfo.setItem( "jwtToken", TEST_JWT );
-  await RNSInfo.setItem( "jwtGeneratedAt", Date.now( ).toString( ), {} );
-  await RNSInfo.setItem( "accessToken", TEST_ACCESS_TOKEN );
+  await RNSInfo.setItem( "username", user.login, rnsiOptions );
+  await RNSInfo.setItem( "jwtToken", TEST_JWT, rnsiOptions );
+  await RNSInfo.setItem( "jwtGeneratedAt", Date.now( ).toString( ), rnsiOptions );
+  await RNSInfo.setItem( "accessToken", TEST_ACCESS_TOKEN, rnsiOptions );
   clearAuthCache( );
   inatjs.users.me.mockResolvedValue( makeResponse( [user] ) );
   user.signedIn = true;
