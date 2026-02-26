abab8b13322842a2dcf249b01cd390fe40b716cd
Johannes Klein <johannes.t.klein@gmail.com>
2026-02-26 12:19:57 +0100

    Use wrapper for close button

--- src/components/Camera/Buttons/Close.tsx
+++ src/components/Camera/Buttons/Close.tsx
@@ -1,8 +1,7 @@
+import RotatableIconWrapper from "components/Camera/RotatableIconWrapper";
 import { TransparentCircleButton } from "components/SharedComponents";
 import React from "react";
 import type { ViewStyle } from "react-native";
-import DeviceInfo from "react-native-device-info";
-import Animated from "react-native-reanimated";
 import { useTranslation } from "sharedHooks";
 
 interface Props {
@@ -10,20 +9,18 @@ interface Props {
   rotatableAnimatedStyle?: ViewStyle;
 }
 
-const isTablet = DeviceInfo.isTablet();
-
 const Close = ( { handleClose, rotatableAnimatedStyle }: Props ) => {
   const { t } = useTranslation( );
 
   return (
-    <Animated.View style={!isTablet && rotatableAnimatedStyle}>
+    <RotatableIconWrapper rotatableAnimatedStyle={rotatableAnimatedStyle}>
       <TransparentCircleButton
         onPress={handleClose}
         accessibilityLabel={t( "Close" )}
         accessibilityHint={t( "Navigates-to-previous-screen" )}
         icon="close"
       />
-    </Animated.View>
+    </RotatableIconWrapper>
   );
 };
 
