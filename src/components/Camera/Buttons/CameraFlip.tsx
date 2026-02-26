537360557b25f5084e826b2ed674b4bf77f477bf
Johannes Klein <johannes.t.klein@gmail.com>
2026-02-26 11:10:28 +0100

    Add rotatable view to camera flip button

--- src/components/Camera/Buttons/CameraFlip.tsx
+++ src/components/Camera/Buttons/CameraFlip.tsx
@@ -1,27 +1,38 @@
 import { TransparentCircleButton } from "components/SharedComponents";
 import React from "react";
-import type { GestureResponderEvent } from "react-native";
+import type {
+  GestureResponderEvent,
+  ViewStyle,
+} from "react-native";
+import DeviceInfo from "react-native-device-info";
+import Animated from "react-native-reanimated";
 import { useTranslation } from "sharedHooks";
 
 interface Props {
   flipCamera: ( _event: GestureResponderEvent ) => void;
   cameraFlipClasses?: string;
+  rotatableAnimatedStyle?: ViewStyle;
 }
+
+const isTablet = DeviceInfo.isTablet();
 
 const CameraFlip = ( {
   flipCamera,
   cameraFlipClasses,
+  rotatableAnimatedStyle,
 }: Props ) => {
   const { t } = useTranslation( );
 
   return (
-    <TransparentCircleButton
-      optionalClasses={cameraFlipClasses}
-      onPress={flipCamera}
-      accessibilityLabel={t( "Flip-camera" )}
-      accessibilityHint={t( "Use-the-devices-other-camera" )}
-      icon="rotate"
-    />
+    <Animated.View style={!isTablet && rotatableAnimatedStyle}>
+      <TransparentCircleButton
+        optionalClasses={cameraFlipClasses}
+        onPress={flipCamera}
+        accessibilityLabel={t( "Flip-camera" )}
+        accessibilityHint={t( "Use-the-devices-other-camera" )}
+        icon="rotate"
+      />
+    </Animated.View>
   );
 };
 
