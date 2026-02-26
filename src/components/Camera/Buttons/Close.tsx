07153c62e12df1a8364dbc55393a577e7e65ff6c
Johannes Klein <johannes.t.klein@gmail.com>
2026-02-26 11:09:13 +0100

    Add rotatable view to close button

--- src/components/Camera/Buttons/Close.tsx
+++ src/components/Camera/Buttons/Close.tsx
@@ -1,21 +1,29 @@
 import { TransparentCircleButton } from "components/SharedComponents";
 import React from "react";
+import type { ViewStyle } from "react-native";
+import DeviceInfo from "react-native-device-info";
+import Animated from "react-native-reanimated";
 import { useTranslation } from "sharedHooks";
 
 interface Props {
   handleClose: ( ) => void;
+  rotatableAnimatedStyle?: ViewStyle;
 }
 
-const Close = ( { handleClose }: Props ) => {
+const isTablet = DeviceInfo.isTablet();
+
+const Close = ( { handleClose, rotatableAnimatedStyle }: Props ) => {
   const { t } = useTranslation( );
 
   return (
-    <TransparentCircleButton
-      onPress={handleClose}
-      accessibilityLabel={t( "Close" )}
-      accessibilityHint={t( "Navigates-to-previous-screen" )}
-      icon="close"
-    />
+    <Animated.View style={!isTablet && rotatableAnimatedStyle}>
+      <TransparentCircleButton
+        onPress={handleClose}
+        accessibilityLabel={t( "Close" )}
+        accessibilityHint={t( "Navigates-to-previous-screen" )}
+        icon="close"
+      />
+    </Animated.View>
   );
 };
 
