ef7f4668eece7f7e5a704ff84c8b8196c3dc3edc
Johannes Klein <johannes.t.klein@gmail.com>
2026-02-26 12:16:00 +0100

    Refactor common functionality of the rotating view into a new wrapper component

---
+++ src/components/Camera/RotatableIconWrapper.tsx
@@ -0,0 +1,28 @@
+import type { PropsWithChildren } from "react";
+import React from "react";
+import type { ViewStyle } from "react-native";
+import DeviceInfo from "react-native-device-info";
+import type { AnimatedStyle } from "react-native-reanimated";
+import Animated from "react-native-reanimated";
+
+interface Props extends PropsWithChildren {
+  rotatableAnimatedStyle?: ViewStyle | AnimatedStyle;
+  containerClass?: string;
+}
+
+const isTablet = DeviceInfo.isTablet();
+
+const RotatableIconWrapper = ( {
+  children,
+  rotatableAnimatedStyle,
+  containerClass,
+}: Props ) => (
+  <Animated.View
+    style={!isTablet && rotatableAnimatedStyle}
+    className={containerClass}
+  >
+    {children}
+  </Animated.View>
+);
+
+export default RotatableIconWrapper;
