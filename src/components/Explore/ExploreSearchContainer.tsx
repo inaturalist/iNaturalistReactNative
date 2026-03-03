e2586e04359de5f8e36448a6ca87b70b9ad686fa
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-03 10:27:59 +0200

    Copy over search UI into the new Universal Search screen

--- src/components/Explore/ExploreSearchContainer.tsx
+++ src/components/Explore/ExploreSearchContainer.tsx
@@ -1,16 +1,97 @@
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import type { ApiPlace, ApiProject, ApiTaxon } from "api/types";
 import { View } from "components/styledComponents";
 import {
   ExploreProvider,
 } from "providers/ExploreContext";
 import React from "react";
+import type { RealmTaxon } from "realmModels/types";
+import { useLocationPermission } from "sharedHooks";
+
+import ExploreLocationSearch from "./SearchScreens/ExploreLocationSearch";
+import ExploreProjectSearch from "./SearchScreens/ExploreProjectSearch";
+import ExploreTaxonSearch from "./SearchScreens/ExploreTaxonSearch";
+import ExploreUserSearch from "./SearchScreens/ExploreUserSearch";
 
 const ExploreSearchContainerWithContext = () => {
+  const navigation = useNavigation();
   const { params } = useRoute( );
+  const {
+    hasPermissions,
+    renderPermissionsGate,
+    requestPermissions,
+  } = useLocationPermission( );
+
   console.log( "params", params );
 
+  const initialSearchMode = params?.initialSearchMode || "none";
+
+  const closeModal = () => {
+    navigation.goBack();
+  };
+
+  const updateTaxon = (
+    taxon: {
+      name: string;
+    } | null,
+  ) => {
+    console.log( "Not implemented in ExploreV2 yet.", taxon );
+  };
+
+  const updateLocation = ( location: "worldwide" | ApiPlace ) => {
+    console.log( "Not implemented in ExploreV2 yet.", location );
+  };
+
+  const updateUser = ( user: null | { login: string } ) => {
+    console.log( "Not implemented in ExploreV2 yet.", user );
+  };
+  const updateProject = ( project: null | ApiProject ) => {
+    console.log( "Not implemented in ExploreV2 yet.", project );
+  };
+
+  if ( initialSearchMode === "taxon" ) {
+    return (
+      <ExploreTaxonSearch
+        closeModal={closeModal}
+        onPressInfo={( taxon: RealmTaxon | ApiTaxon ) => {
+          navigation.push( "TaxonDetails", { id: taxon.id } );
+        }}
+        updateTaxon={updateTaxon}
+      />
+    );
+  }
+
+  if ( initialSearchMode === "location" ) {
+    return (
+      <ExploreLocationSearch
+        closeModal={closeModal}
+        hasPermissions={hasPermissions}
+        renderPermissionsGate={renderPermissionsGate}
+        requestPermissions={requestPermissions}
+        updateLocation={updateLocation}
+      />
+    );
+  }
+
+  if ( initialSearchMode === "users" ) {
+    return (
+      <ExploreUserSearch closeModal={closeModal} updateUser={updateUser} />
+    );
+  }
+
+  if ( initialSearchMode === "projects" ) {
+    return (
+      <ExploreProjectSearch
+        closeModal={closeModal}
+        updateProject={updateProject}
+      />
+    );
+  }
+
   return (
-    <View />
+    <View>
+      {renderPermissionsGate( {} )}
+    </View>
   );
 };
 
