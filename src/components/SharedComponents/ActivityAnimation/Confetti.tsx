e467565a38d436e77f8ee1662392f7cfb9b3eaee
Abbey Campbell <campbellabbeya@gmail.com>
2026-03-09 17:08:39 -0700

    fix confetti crash by removing worklet directive

--- src/components/SharedComponents/ActivityAnimation/Confetti.tsx
+++ src/components/SharedComponents/ActivityAnimation/Confetti.tsx
@@ -157,14 +157,10 @@ const Confetti = ( { count, duration = 5000 }: ConfettiProps ) => {
 // Helpers
 
 function clamp( value: number, lowerBound: number, upperBound: number ) {
-  "worklet";
-
   return Math.min( Math.max( lowerBound, value ), upperBound );
 }
 
 function getRandomStartEndCoordinates() {
-  "worklet";
-
   // First, generate a start x between 0 and 1
   // then subtract that from 1 to get the end x
   // In this way, we now that the end x will always
@@ -178,14 +174,10 @@ function getRandomStartEndCoordinates() {
 }
 
 function randomNumberInRange( min: number, max: number ) {
-  "worklet";
-
   return Math.random() * ( max - min ) + min;
 }
 
 function getAnimationConfigForElement( duration: number, index: number, count: number ) {
-  "worklet";
-
   // In a nutshell, to constrain the duration
   // of each element and also to ensure that it is evenly
   // distributing the elements across the duration [0, 1]
