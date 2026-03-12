78699597de7f8df6eca48b0b3d43ec3b6f3d4fdf
sepeterson <10458078+sepeterson@users.noreply.github.com>
2026-03-12 10:40:13 -0500

    MOB-1186: add back to App and StartupService

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import {
   useShare,
 } from "sharedHooks";
 import { isDebugMode } from "sharedHooks/useDebugMode";
+import useDroppedFrames from "sharedHooks/useDroppedFrames";
 
 import AppStateListener from "./AppStateListener";
 import useLinking from "./hooks/useLinking";
@@ -72,6 +73,7 @@ const App = ( { children }: Props ): Node => {
 
   useLinking( currentUser );
   useShare( onShare );
+  useDroppedFrames( );
 
   // this children prop is here for the sake of testing with jest
   // normally we would never do this in code
--- src/components/StartupService.tsx
+++ src/components/StartupService.tsx
@@ -6,11 +6,12 @@ import { RealmContext } from "providers/contexts";
 import { useEffect } from "react";
 import { LogBox } from "react-native";
 import DeviceInfo from "react-native-device-info";
+import RNFS from "react-native-fs";
 import Orientation from "react-native-orientation-locker";
 import Realm from "realm";
 import clearCaches from "sharedHelpers/clearCaches";
 import { IS_FRESH_INSTALL, store } from "sharedHelpers/installData";
-import { log } from "sharedHelpers/logger";
+import { log, logFilePath } from "sharedHelpers/logger";
 import { addARCameraFiles } from "sharedHelpers/mlModel";
 import { findAndLogSentinelFiles } from "sharedHelpers/sentinelFiles";
 import {
@@ -18,6 +19,7 @@ import {
 } from "sharedHooks";
 import { isDebugMode } from "sharedHooks/useDebugMode";
 import { zustandStorage } from "stores/useStore";
+import zustandMMKVBackingStorage from "stores/zustandMMKVBackingStorage";
 
 // Ignore warnings about 3rd parties that haven't implemented the new
 // NativeEventEmitter interface methods yet. As of 20230517, this is coming
@@ -94,6 +96,24 @@ const StartupService = ( ) => {
       logger.info(
         `App version: ${DeviceInfo.getVersion()}, build: ${DeviceInfo.getBuildNumber()}`,
       );
+
+      const logStorageMetrics = async ( ) => {
+        try {
+          const realmBytes = realm?.path
+            ? ( await RNFS.stat( realm.path ) ).size
+            : 0;
+          const logFileBytes = ( await RNFS.stat( logFilePath ) ).size;
+          logger.infoWithExtra( "storage_metrics", {
+            realm_db_bytes: realmBytes,
+            mmkv_bytes: zustandMMKVBackingStorage.size,
+            install_data_bytes: store.size,
+            log_file_bytes: logFileBytes,
+          } );
+        } catch ( e ) {
+          logger.info( "storage_metrics collection failed", e );
+        }
+      };
+      await logStorageMetrics( );
 
       try {
         await checkForSignedInUser( );
