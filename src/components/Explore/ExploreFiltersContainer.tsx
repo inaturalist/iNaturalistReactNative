41584ac1a5f5f829fd422e0d86e362fc16c87a99
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-17 10:44:31 +0100

    Restructure explore full screen experiences (#3416)
    * Fix previous comment
    
    * Add stub for ExploreFilters screen
    
    * Create FilterModalV2.tsx
    
    * Remove everything that is not needed from ExploreFilters stub
    
    * Add a dummy header link to navigate to ExploreFilters V2
    
    * Wrap ExploreFiltersContainer in Explore context provider
    
    This is probably not what we want in the long term but there are too many errors to fix now if I don't do this.
    
    * Replace project search modal with navigation
    
    * Stub for an ExploreSearch screen
    
    * Rename export
    
    * Replace user search modal with navigation call
    
    * Replace location search modal with navigation call
    
    * Add function stub
    
    * Replace location search modal with navigation call
    
    * Update type in ExploreLocationSearch
    
    * Copy over search UI into the new Universal Search screen
    
    * Remove console.log
    
    * Add params type

---
+++ src/components/Explore/ExploreFiltersContainer.tsx
@@ -0,0 +1,58 @@
+import { useNavigation } from "@react-navigation/native";
+import type { ApiProject } from "api/types";
+import {
+  ExploreProvider,
+} from "providers/ExploreContext";
+import React from "react";
+
+import FilterModalV2 from "./Modals/FilterModalV2";
+
+const ExploreFiltersContainerWithContext = () => {
+  const navigation = useNavigation();
+
+  const closeModal = () => {
+    navigation.goBack();
+  };
+
+  const filterByIconicTaxonUnknown = () => {
+    console.log( " Not implemented in ExploreV2 yet" );
+  };
+
+  const updateTaxon = (
+    taxon: {
+      name: string;
+    } | null,
+  ) => {
+    console.log( " Not implemented in ExploreV2 yet", taxon );
+  };
+
+  const updateUser = (
+    user: {
+      login: string;
+    } | null,
+  ) => {
+    console.log( " Not implemented in ExploreV2 yet", user );
+  };
+
+  const updateProject = ( project: ApiProject ) => {
+    console.log( " Not implemented in ExploreV2 yet", project );
+  };
+
+  return (
+    <FilterModalV2
+      closeModal={closeModal}
+      filterByIconicTaxonUnknown={filterByIconicTaxonUnknown}
+      updateTaxon={updateTaxon}
+      updateUser={updateUser}
+      updateProject={updateProject}
+    />
+  );
+};
+
+const ExploreFiltersContainer = () => (
+  <ExploreProvider>
+    <ExploreFiltersContainerWithContext />
+  </ExploreProvider>
+);
+
+export default ExploreFiltersContainer;
