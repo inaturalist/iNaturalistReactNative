bd5ab52190c3bbd9a493b114795a4f2d88b060ad
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-19 13:02:36 +0100

    Add a navigation types file with root stack params list

---
+++ src/navigation/types.tsx
@@ -0,0 +1,14 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/?config=dynamic
+ */
+
+// Note from the documentation:
+// The type containing the mapping must be a type alias. It cannot be an interface.
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+export type RootStackParamList = {
+  OnboardingStackNavigator: undefined;
+  TabNavigator: undefined;
+  NoBottomTabStackNavigator: undefined;
+  LoginStackNavigator: undefined;
+};
