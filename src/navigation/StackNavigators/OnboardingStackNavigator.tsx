0d412e962a33b710bf6028745fe4657815697df0
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-19 13:17:41 +0100

    Migrate OnboardingStackNavigator to TS

--- src/navigation/StackNavigators/OnboardingStackNavigator.tsx
+++ src/navigation/StackNavigators/OnboardingStackNavigator.tsx
@@ -1,12 +1,10 @@
-// @flow
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnboardingCarousel from "components/Onboarding/OnboardingCarousel";
-import type { Node } from "react";
 import React from "react";
 
 const Stack = createNativeStackNavigator( );
 
-const OnboardingStackNavigator = ( ): Node => (
+const OnboardingStackNavigator = ( ) => (
   <Stack.Navigator>
     <Stack.Screen
       name="Onboarding"
