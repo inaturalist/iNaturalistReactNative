ddaacd85b3c531b6912621b875349a9570c2214f
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-23 21:44:41 +0100

    Wrap log in try/catch as to not disturb actual query

--- src/api/observations.js
+++ src/api/observations.js
@@ -25,15 +25,21 @@ const searchObservations = async ( params: Object = {}, opts: Object = {} ): Pro
     const startedAt = Date.now( );
     const response = await inatjs.observations.search( params, opts );
     const elapsedMs = Date.now( ) - startedAt;
-    logger.infoWithExtra(
-      "EXPERIMENTAL COMPARISON: querying API v2 with fields",
-      {
-        hasFields: !!params?.fields,
-        durationMs: elapsedMs,
-        // Not sure if asking for smaller page has performance benefits, but log it just in case
-        per_page: params?.per_page,
-      },
-    );
+    // Wrapping this in try/catch just in case something goes wrong with logging,
+    // we don't want to fail the whole request just because of that
+    try {
+      logger.infoWithExtra(
+        "EXPERIMENTAL COMPARISON: querying API v2 with fields",
+        {
+          hasFields: !!params?.fields,
+          durationMs: elapsedMs,
+          // Not sure if asking for smaller page has performance benefits, but log it just in case
+          per_page: params?.per_page,
+        },
+      );
+    } catch ( e ) {
+      logger.error( "Error logging experimental comparison", e );
+    }
     response.results = response.results.map( mapToLocalSchema );
     return response;
   } catch ( e ) {
