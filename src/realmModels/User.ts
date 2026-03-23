c848871acb3fb4292c57bab759993aaee64f4cef
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-24 00:41:01 +0100

    Those sizes were not returned by the API

--- src/realmModels/User.ts
+++ src/realmModels/User.ts
@@ -39,16 +39,8 @@ class User extends Realm.Object {
     return User.uri( user )?.replace( "medium", "large" );
   }
 
-  static smallUri( user?: RealmUser | ApiUser ) {
-    return User.uri( user )?.replace( "medium", "small" );
-  }
-
   static thumbUri( user?: RealmUser | ApiUser ) {
     return User.uri( user )?.replace( "medium", "thumb" );
-  }
-
-  static squareUri( user?: RealmUser | ApiUser ) {
-    return User.uri( user )?.replace( "medium", "square" );
   }
 
   static currentUser( realm: Realm ) {
