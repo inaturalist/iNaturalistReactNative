2190ff145875520ddf76d7a67f81cf92ec5a2d64
Abbey Campbell <campbellabbeya@gmail.com>
2026-03-25 12:29:55 -0700

    add comment

--- src/navigation/BottomTabNavigator/tabScreenOptions.ts
+++ src/navigation/BottomTabNavigator/tabScreenOptions.ts
@@ -1,5 +1,7 @@
 import type { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
 
+// Note: this file exists so we can mock the screenOptions and disable animations while under test.
+// If you update this file, be sure to update the mock in tests/jest.setup.js as well.
 const tabScreenOptions: BottomTabNavigationOptions = {
   lazy: true,
   freezeOnBlur: true,
