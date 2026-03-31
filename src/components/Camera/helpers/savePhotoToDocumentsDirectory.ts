c2b704dc2cf913fba9393e69cf2c5f0ee25bbdbe
Johannes Klein <johannes.t.klein@gmail.com>
2026-03-31 18:17:03 +0200

    Revert "Add file to return path"
    This reverts commit 2991d13d99ca75109c5f5f2418fc127e60860cd6.

--- src/components/Camera/helpers/savePhotoToDocumentsDirectory.ts
+++ src/components/Camera/helpers/savePhotoToDocumentsDirectory.ts
@@ -1,7 +1,6 @@
 import {
   rotatedOriginalPhotosPath,
 } from "appConstants/paths";
-import { Platform } from "react-native";
 import RNFS from "react-native-fs";
 import type {
   PhotoFile,
@@ -15,9 +14,7 @@ const savePhotoToDocumentsDirectory = async (
   const filename = cameraPhoto.path.split( "/" ).at( -1 );
   const newPath = `${path}/${filename}`;
   await RNFS.moveFile( cameraPhoto.path, newPath );
-  return Platform.OS === "android"
-    ? `file://${newPath}`
-    : newPath;
+  return newPath;
 };
 
 export default savePhotoToDocumentsDirectory;
