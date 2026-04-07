fb01e5b61b2a020e879f8d2c17f540828153ff1f
FLGMwt <ryan.stelly@live.com>
2026-04-06 20:37:31 -0500

    return non-connected when airplane mode config is on

--- src/sharedHooks/useConnectionStatus.ts
+++ src/sharedHooks/useConnectionStatus.ts
@@ -1,5 +1,22 @@
 import { useNetInfo } from "@react-native-community/netinfo";
+import { useMemo } from "react";
+import { DevOnlyFlag } from "types/dynamicConfig";
 
-const useConnectionStatus = () => useNetInfo();
+import { useDynamicConfigInternals } from "./useDynamicConfig";
+
+const useConnectionStatus = () => {
+  const { overrideValue: simulateAirplaneModeEnabled }
+    = useDynamicConfigInternals( DevOnlyFlag.SimulateAirplaneModeEnabled );
+
+  const { isConnected } = useNetInfo();
+
+  const connectionStatus = useMemo( () => ( {
+    isConnected: simulateAirplaneModeEnabled === null
+      ? isConnected
+      : !simulateAirplaneModeEnabled,
+  } ), [isConnected, simulateAirplaneModeEnabled] );
+
+  return connectionStatus;
+};
 
 export default useConnectionStatus;
