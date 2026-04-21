a7c773e2cfcc2ac833782e29ac996ba8857439cc
FLGMwt <ryan.stelly@live.com>
2026-04-21 16:04:28 -0500

    add debugMode layout state and use for hook

--- src/sharedHooks/useDebugMode.ts
+++ src/sharedHooks/useDebugMode.ts
@@ -1,25 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
-import { zustandStorage } from "stores/useStore";
+import useStore, { zustandStorage } from "stores/useStore";
 
 const DEBUG_MODE = "debugMode";
 
 const useDebugMode = ( ): { isDebug: boolean; toggleDebug: () => void } => {
-  const [isDebug, setDebug] = useState( false );
-
-  useEffect( ( ) => {
-    const readDebugModeFromStorage = ( ) => {
-      const storedDebugMode = zustandStorage.getItem( DEBUG_MODE );
-      setDebug( storedDebugMode === "true" );
-    };
-
-    readDebugModeFromStorage( );
-  }, [] );
-
-  const toggleDebug = useCallback( ( ) => {
-    zustandStorage.setItem( DEBUG_MODE, ( !isDebug ).toString( ) );
-    setDebug( !isDebug );
-  }, [isDebug] );
-
+  const isDebug = useStore( state => state.layout.debugModeEnabled );
+  const toggleDebug = useStore( state => state.layout.toggleDebugMode );
   return {
     isDebug,
     toggleDebug,
