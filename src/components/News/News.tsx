dcfe9d601c9073589a7f3e2796c0a6e2072cfe71
Johannes Klein <johannes.t.klein@gmail.com>
2026-05-08 23:25:40 +0200

    Query for posts and pass to list

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,32 +1,38 @@
-import type { ApiPost } from "api/types";
+import fetchUserPosts from "api/posts";
+import ActivityIndicator from "components/SharedComponents/ActivityIndicator";
 import { View } from "components/styledComponents";
 import React from "react";
+import useAuthenticatedQuery from "sharedHooks/useAuthenticatedQuery";
+import useCurrentUser from "sharedHooks/useCurrentUser";
 
 import PostList from "./PostList";
 
-const testPosts: ApiPost[] = [
-  {
-    id: 1,
-    title: "New Feature Release",
-    body: "We are excited to announce the release of our new feature...",
-    published_at: "2024-06-01T00:00:00Z",
-    parent: {
-      id: 1,
-      title: "Project Alpha",
-      icon_url: "https://example.com/project-icon.png",
-      type: "collection",
-    },
-    user: {
-      login: "johndoe",
-      icon_url: "https://example.com/hero-image.jpg",
-    },
-  },
-];
+const News = ( ) => {
+  const currentUser = useCurrentUser( );
+  const { data: posts, isLoading: isLoadingPosts } = useAuthenticatedQuery(
+    ["fetchUserPosts"],
+    optsWithAuth => fetchUserPosts(
+      {
+        user_id: currentUser?.id,
+      },
+      optsWithAuth,
+    ),
+    { enabled: !!currentUser },
+  );
 
-const News = ( ) => (
-  <View className="flex-1">
-    <PostList posts={testPosts} />
-  </View>
-);
+  if ( isLoadingPosts ) {
+    return (
+      <View className="flex-1 bg-white">
+        <ActivityIndicator size={50} />
+      </View>
+    );
+  }
+
+  return (
+    <View className="flex-1 bg-white">
+      <PostList posts={posts} />
+    </View>
+  );
+};
 
 export default News;
