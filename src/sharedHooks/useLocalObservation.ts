258f0dc436c9d6c5fe5cb86046644c9ddf31ef21
Johannes Klein <johannes.t.klein@gmail.com>
2026-05-15 15:27:43 +0200

    Do not add a filtered set of comments and IDs to local obs
    Searching for visibleComments and visibleIdentifications anyways gives 0 results outside this file.

--- src/sharedHooks/useLocalObservation.ts
+++ src/sharedHooks/useLocalObservation.ts
@@ -1,17 +1,11 @@
 import { RealmContext } from "providers/contexts";
-import type { Results } from "realm";
-import type { RealmComment, RealmIdentification, RealmObservation } from "realmModels/types";
+import type { RealmObservation } from "realmModels/types";
 import safeRealmWrite from "sharedHelpers/safeRealmWrite";
 
 const { useRealm } = RealmContext;
 
-interface Observation extends RealmObservation {
-  visibleComments?: Results<RealmComment>;
-  visibleIdentifications?: Results<RealmIdentification>;
-}
-
 interface UseLocalObservation {
-  localObservation: Observation | null;
+  localObservation: RealmObservation | null;
   markDeletedLocally: ( ) => void;
   markViewedLocally: ( ) => void;
 }
@@ -62,23 +56,6 @@ const useLocalObservation = ( uuid: string ): UseLocalObservation => {
       observation.identifications_viewed = true;
     }, "marking viewed locally in ObsDetailsContainer" );
   };
-
-  Object.defineProperties( observation, {
-    visibleComments: {
-      get() {
-        return this.comments
-          ? this.comments.filtered( "hidden == false" )
-          : [];
-      },
-    },
-    visibleIdentifications: {
-      get() {
-        return this.identifications
-          ? this.identifications.filtered( "hidden == false" )
-          : [];
-      },
-    },
-  } );
 
   return {
     localObservation: observation,
