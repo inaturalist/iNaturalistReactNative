d00a4540c5ae495249cc4553156e3006478abcfa
Johannes Klein <johannes.t.klein@gmail.com>
2026-05-18 14:46:00 +0200

    Remove android specific launch and early return

--- e2e/helpers.js
+++ e2e/helpers.js
@@ -37,12 +37,6 @@ export async function iNatE2eBeforeAll( device ) {
 }
 
 export async function iNatE2eBeforeEach( device ) {
-  // device.launchApp would be preferred for an app of our complexity. It does work locally
-  // for both, but on CI for Android it does not work. So we use reloadReactNative for Android.
-  if ( device.getPlatform() === "android" ) {
-    await device.reloadReactNative();
-    return;
-  }
   const launchAppOptions = {
     newInstance: true,
     permissions: {
@@ -61,19 +55,21 @@ export async function iNatE2eBeforeEach( device ) {
     // Try it one more time
     await device.launchApp( launchAppOptions );
   }
-  // disable password autofill
-  execSync(
-    // eslint-disable-next-line max-len
-    `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles/UserSettings.plist`,
-  );
-  execSync(
-    // eslint-disable-next-line max-len
-    `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/EffectiveUserSettings.plist`,
-  );
-  execSync(
-    // eslint-disable-next-line max-len
-    `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/PublicInfo/PublicEffectiveUserSettings.plist`,
-  );
+  if ( device.getPlatform( ) === "ios" ) {
+    // disable password autofill
+    execSync(
+      // eslint-disable-next-line max-len
+      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles/UserSettings.plist`,
+    );
+    execSync(
+      // eslint-disable-next-line max-len
+      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/EffectiveUserSettings.plist`,
+    );
+    execSync(
+      // eslint-disable-next-line max-len
+      `plutil -replace restrictedBool.allowPasswordAutoFill.value -bool NO ~/Library/Developer/CoreSimulator/Devices/${device.id}/data/Library/UserConfigurationProfiles/PublicInfo/PublicEffectiveUserSettings.plist`,
+    );
+  }
 }
 
 async function getSimulatorId() {
