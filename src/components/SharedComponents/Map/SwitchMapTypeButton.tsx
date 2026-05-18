7b26dac14a8904f2c495147623f1898be4de8c0d
Johannes Klein <johannes.t.klein@gmail.com>
2026-05-18 13:27:53 +0200

    Update store from switcher button

--- src/components/SharedComponents/Map/SwitchMapTypeButton.tsx
+++ src/components/SharedComponents/Map/SwitchMapTypeButton.tsx
@@ -1,43 +1,29 @@
 import classnames from "classnames";
 import { INatIconButton } from "components/SharedComponents";
-import React, {
-  useEffect,
-} from "react";
+import React from "react";
 import { useTranslation } from "sharedHooks";
 import { MAP_TYPES } from "stores/createLayoutSlice";
-import { zustandStorage } from "stores/useStore";
+import useStore from "stores/useStore";
 import { getShadow } from "styles/global";
 
 const DROP_SHADOW = getShadow( );
 
 interface Props {
-  currentMapType?: string;
-  mapType?: string;
-  setCurrentMapType: ( mapType: string|number ) => void;
+  mapType?: MAP_TYPES;
   showSwitchMapTypeButton?: boolean;
   switchMapTypeButtonClassName?: string;
 }
 
 const SwitchMapTypeButton = ( {
-  currentMapType,
   mapType,
-  setCurrentMapType,
   showSwitchMapTypeButton,
   switchMapTypeButtonClassName,
 }: Props ) => {
   const { t } = useTranslation( );
-  useEffect( () => {
-    const value = zustandStorage.getItem( "mapType" );
-    if ( value && !mapType ) {
-      // Load last saved map type (unless explicitly overridden by the parent
-      // of the Map component)
-      setCurrentMapType( value );
-    }
-  }, [mapType, setCurrentMapType] );
+  const setMapType = useStore( state => state.layout.setMapType );
 
-  const changeMapType = ( newMapType: string ) => {
-    setCurrentMapType( newMapType );
-    zustandStorage.setItem( "mapType", newMapType );
+  const changeMapType = ( newMapType: MAP_TYPES ) => {
+    setMapType( newMapType );
   };
 
   return showSwitchMapTypeButton && (
@@ -49,13 +35,13 @@ const SwitchMapTypeButton = ( {
       )}
       style={DROP_SHADOW}
       accessibilityLabel={
-        currentMapType === MAP_TYPES.STANDARD
+        mapType === MAP_TYPES.STANDARD
           ? t( "Standard--map-type" )
           : t( "Satellite--map-type" )
       }
       accessibilityHint={t( "Toggle-map-type" )}
       onPress={( ) => {
-        changeMapType( currentMapType === MAP_TYPES.STANDARD
+        changeMapType( mapType === MAP_TYPES.STANDARD
           ? MAP_TYPES.HYBRID
           : MAP_TYPES.STANDARD );
       }}
