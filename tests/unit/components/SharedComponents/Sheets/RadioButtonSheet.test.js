fab478db7ad4740eca9b36a842998197fcf5b733
Johannes Klein <johannes.t.klein@gmail.com>
2026-05-21 17:28:07 +0200

    Add some radio button sheet unit tests

--- tests/unit/components/SharedComponents/Sheets/RadioButtonSheet.test.js
+++ tests/unit/components/SharedComponents/Sheets/RadioButtonSheet.test.js
@@ -1,5 +1,7 @@
+import { fireEvent, screen } from "@testing-library/react-native";
 import { RadioButtonSheet } from "components/SharedComponents";
 import React from "react";
+import { renderComponent } from "tests/helpers/render";
 
 const mockHandleClose = jest.fn( );
 const mockConfirm = jest.fn( );
@@ -18,6 +20,10 @@ const mockRadioValues = {
 };
 
 describe( "RadioButtonSheet", () => {
+  beforeEach( () => {
+    jest.clearAllMocks( );
+  } );
+
   it( "should not have accessibility errors", () => {
     const sheet = (
       <RadioButtonSheet
@@ -31,5 +37,58 @@ describe( "RadioButtonSheet", () => {
     // TODO: this errors because RadioButton from react-native-paper is not accessible
     console.log( "typeof sheet :>> ", typeof sheet );
     // expect( sheet ).toBeAccessible();
+  } );
+
+  it( "disables confirm when unchanged and requireSelectionChange is default", () => {
+    renderComponent(
+      <RadioButtonSheet
+        onPressClose={mockHandleClose}
+        confirm={mockConfirm}
+        headerText="header text"
+        radioValues={mockRadioValues}
+        selectedValue={mockRadioValues.first.value}
+      />,
+    );
+
+    const button = screen.getByText( "CONFIRM" );
+
+    expect( button ).toBeDisabled( );
+  } );
+
+  it( "enables confirm on initial render when requireSelectionChange is false", () => {
+    renderComponent(
+      <RadioButtonSheet
+        onPressClose={mockHandleClose}
+        confirm={mockConfirm}
+        headerText="header text"
+        radioValues={mockRadioValues}
+        requireSelectionChange={false}
+        selectedValue={mockRadioValues.first.value}
+      />,
+    );
+
+    const button = screen.getByText( "CONFIRM" );
+
+    expect( button ).toBeEnabled( );
+  } );
+
+  it( "enables confirm after changing option when requireSelectionChange is default", () => {
+    renderComponent(
+      <RadioButtonSheet
+        onPressClose={mockHandleClose}
+        confirm={mockConfirm}
+        headerText="header text"
+        radioValues={mockRadioValues}
+        selectedValue={mockRadioValues.first.value}
+      />,
+    );
+    const label2 = screen.getByText( "label 2" );
+    const button = screen.getByText( "CONFIRM" );
+
+    fireEvent.press( label2 );
+    expect( button ).toBeEnabled();
+    fireEvent.press( button );
+
+    expect( mockConfirm ).toHaveBeenCalledWith( "value-2" );
   } );
 } );
