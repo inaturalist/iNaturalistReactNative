387beec577ffa6f9fe11c5684719f3414590ea27
Abbey Campbell <campbellabbeya@gmail.com>
2026-05-27 15:52:20 -0700

    add shared observations sort enum

--- src/types/sorting.ts
+++ src/types/sorting.ts
@@ -4,13 +4,21 @@ export type SortItemType = "observations" | "taxa";
 
 export type SortDirection = "asc" | "desc";
 
+export enum OBSERVATIONS_SORT {
+  DATE_UPLOADED_NEWEST = "DATE_UPLOADED_NEWEST",
+  DATE_UPLOADED_OLDEST = "DATE_UPLOADED_OLDEST",
+  DATE_OBSERVED_NEWEST = "DATE_OBSERVED_NEWEST",
+  DATE_OBSERVED_OLDEST = "DATE_OBSERVED_OLDEST",
+  MOST_FAVED = "MOST_FAVED",
+}
+
 // eventually this will be a union type that accounts for different types of sorting, i.e. name
 export type SpeciesSortBy = "count"
 
 export interface ObservationSort {
   by:
     | "created_at"
-    | "observed_at";
+    | "observed_on";
   direction?: SortDirection;
 }
 
