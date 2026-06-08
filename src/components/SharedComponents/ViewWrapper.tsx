dbff93952e17e86132efb4888c0649f785760872
Johannes Klein <johannes.t.klein@gmail.com>
2026-06-08 19:18:18 +0200

    Revert "Split ViewWrapper into two components"
    This reverts commit cf2a55360d54c13b44419d1ff28b2b25e65c36fe.

--- src/components/SharedComponents/ViewWrapper.tsx
+++ src/components/SharedComponents/ViewWrapper.tsx
@@ -3,7 +3,6 @@ import { Body1 } from "components/SharedComponents";
 import { View } from "components/styledComponents";
 import type { PropsWithChildren } from "react";
 import * as React from "react";
-import type { StyleProp, ViewStyle } from "react-native";
 import { StatusBar } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
@@ -15,41 +14,6 @@ interface Props extends PropsWithChildren {
   wrapperClassName?: string;
   useTopInset?: boolean;
 }
-
-interface ScreenShellProps extends PropsWithChildren {
-  isDebug?: boolean;
-  style?: StyleProp<ViewStyle>;
-  testID?: string;
-  wrapperClassName?: string;
-}
-
-const ScreenShell = ( {
-  children,
-  isDebug,
-  style,
-  testID,
-  wrapperClassName,
-}: ScreenShellProps ) => (
-  <View
-    className={classnames(
-      "flex-1",
-      "bg-white",
-      wrapperClassName,
-      isDebug
-        ? "border-2 border-deepPink"
-        : null,
-    )}
-    style={style}
-    testID={testID}
-  >
-    {isDebug && (
-    // eslint-disable-next-line i18next/no-literal-string
-      <Body1 className="bg-deepPink text-white absolute bottom-0 right-0 z-10">DEBUG</Body1>
-    )}
-    <StatusBar barStyle="dark-content" />
-    {children}
-  </View>
-);
 
 const ViewWrapper = ( {
   children,
@@ -64,16 +28,26 @@ const ViewWrapper = ( {
       ? insets.top
       : 0,
   };
-
   return (
-    <ScreenShell
-      isDebug={isDebug}
+    <View
+      className={classnames(
+        "flex-1",
+        "bg-white",
+        wrapperClassName,
+        isDebug
+          ? "border-2 border-deepPink"
+          : null,
+      )}
       style={viewStyle}
       testID={testID}
-      wrapperClassName={wrapperClassName}
     >
+      {isDebug && (
+      // eslint-disable-next-line i18next/no-literal-string
+        <Body1 className="bg-deepPink text-white absolute bottom-0 right-0 z-10">DEBUG</Body1>
+      )}
+      <StatusBar barStyle="dark-content" />
       {children}
-    </ScreenShell>
+    </View>
   );
 };
 
