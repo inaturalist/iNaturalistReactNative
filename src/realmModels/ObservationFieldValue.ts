aa797d0bbf27b8b15d2f482e5fd7288ea6c14ac2
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-06-21 12:51:39 +0200

    Create new realm model for OFV
    Closely following ObservationPhoto. Not sure if `id` will ever be used.

---
+++ src/realmModels/ObservationFieldValue.ts
@@ -0,0 +1,60 @@
+import { Realm } from "@realm/react";
+import * as uuid from "uuid";
+
+class ObservationFieldValue extends Realm.Object {
+  _created_at?: Date;
+
+  _synced_at?: Date;
+
+  _updated_at?: Date;
+
+  needsSync( ) {
+    return !this._synced_at || this._synced_at <= this._updated_at;
+  }
+
+  wasSynced( ) {
+    return this._synced_at !== null;
+  }
+
+  static new(
+    projectId: number,
+    obsFieldId: number,
+    value?: string,
+  ) {
+    return {
+      _created_at: new Date( ),
+      _updated_at: new Date( ),
+      uuid: uuid.v4( ).toLowerCase( ),
+      projectId,
+      obsFieldId,
+      value,
+    };
+  }
+
+  static schema = {
+    name: "ObservationFieldValue",
+    embedded: true,
+    properties: {
+      // datetime the obsPhoto was created on the device
+      _created_at: "date?",
+      // datetime the obsPhoto was last synced with the server
+      _synced_at: "date?",
+      // datetime the obsPhoto was updated on the device (i.e. edited locally)
+      _updated_at: "date?",
+      uuid: "string",
+      id: "int?",
+      obsFieldId: "int",
+      projectId: "int",
+      value: "string?",
+      // this creates an inverse relationship so observation photos
+      // automatically keep track of which Observation they are assigned to
+      assignee: {
+        type: "linkingObjects",
+        objectType: "Observation",
+        property: "observationFieldValues",
+      },
+    },
+  };
+}
+
+export default ObservationFieldValue;
