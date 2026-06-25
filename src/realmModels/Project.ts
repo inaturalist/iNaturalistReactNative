9b5c52909356cd98620e4038348718f947af3d19
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-06-25 12:30:54 +0200

    Add mapApiToRealm fct to Projects
    Closely Following the code in Observation model

--- src/realmModels/Project.ts
+++ src/realmModels/Project.ts
@@ -1,6 +1,26 @@
 import { Realm } from "@realm/react";
+import type { ApiProjectSummaryWithPOF } from "api/types";
+import ProjectObservationField from "realmModels/ProjectObservationField";
 
 class Project extends Realm.Object {
+  static mapApiToRealm( apiProject: ApiProjectSummaryWithPOF ) {
+    if ( !apiProject ) return apiProject;
+
+    const pofs = ( apiProject.project_observation_fields || [] ).map(
+      pof => ProjectObservationField.mapApiToRealm( pof ),
+    );
+
+    const localProject = {
+      ...apiProject,
+      projectObservationFields: pofs,
+      // Leaving this here explicitly as a reminder to potentially need to refactor project_type
+      // into projectType across the entire codebase for consistency with other data models
+      project_type: apiProject.project_type,
+    };
+
+    return localProject;
+  }
+
   static schema = {
     name: "Project",
     primaryKey: "id",
