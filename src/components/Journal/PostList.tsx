a43e0bd489fc1d3189b631952f693f49cf4ab997
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-06-26 13:19:15 +0200

    Update PostList to have infinite scroll pattern
    Empty and footer component taken as is from ProjectMembers screen

--- src/components/Journal/PostList.tsx
+++ src/components/Journal/PostList.tsx
@@ -1,8 +1,10 @@
+import { useNetInfo } from "@react-native-community/netinfo";
 import type { ListRenderItem } from "@shopify/flash-list";
 import type { ApiPostForUser } from "api/types";
-import { CustomFlashList } from "components/SharedComponents";
+import { CustomFlashList, InfiniteScrollLoadingWheel } from "components/SharedComponents";
 import { View } from "components/styledComponents";
-import React from "react";
+import React, { useMemo } from "react";
+import { ActivityIndicator } from "react-native-paper";
 
 import PostListItem from "./PostListItem";
 
@@ -10,11 +12,25 @@ const ItemSeparator = () => <View className="border-b border-lightGray" />;
 
 interface Props {
   posts: ApiPostForUser[];
+  isFetchingNextPage: boolean;
+  fetchNextPage: ( ) => void;
 }
 
-const PostList = ( {
-  posts,
-}: Props ) => {
+const PostList = ( { posts, isFetchingNextPage, fetchNextPage }: Props ) => {
+  const { isConnected } = useNetInfo();
+  const footerComponent = useMemo(
+    () => (
+      <InfiniteScrollLoadingWheel
+        hideLoadingWheel={!isFetchingNextPage}
+        layout="list"
+        isConnected={isConnected}
+      />
+    ),
+    [isConnected, isFetchingNextPage],
+  );
+
+  const emptyComponent = useMemo( () => <ActivityIndicator size={50} />, [] );
+
   const renderPost: ListRenderItem<ApiPostForUser> = ( { item } ) => (
     <PostListItem item={item} />
   );
@@ -22,8 +38,12 @@ const PostList = ( {
   return (
     <CustomFlashList
       ItemSeparatorComponent={ItemSeparator}
+      ListEmptyComponent={emptyComponent}
+      ListFooterComponent={footerComponent}
       data={posts}
       keyExtractor={item => String( item.id )}
+      onEndReached={fetchNextPage}
+      refreshing={isFetchingNextPage}
       renderItem={renderPost}
       testID="PostList"
     />
