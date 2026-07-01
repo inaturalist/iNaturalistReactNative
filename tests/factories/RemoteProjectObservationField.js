6b4dc3e814142d385da36996f6196936abaf9e59
Johannes Klein <johannes.t.klein@gmail.com>
2026-07-01 20:38:18 +0200

    Use factories (#3788)
    Co-authored-by: Johannes Klein <17345891+jtklein@users.noreply.github.com>

--- tests/factories/RemoteProjectObservationField.js
+++ tests/factories/RemoteProjectObservationField.js
@@ -1,14 +1,10 @@
 import { define } from "factoria";
 
-// TODO: use this instead once MOB-1497 has landed
-// import ofFactory from "./RemoteObservationField";
+import ofFactory from "./RemoteObservationField";
 
 export default define( "RemoteProjectObservationField", faker => ( {
   id: faker.number.int(),
-  // observation_field: ofFactory( "RemoteObservationField" ),
-  observation_field: {
-    id: 1,
-  },
+  observation_field: ofFactory( "RemoteObservationField" ),
   position: faker.number.int(),
   required: faker.datatype.boolean( 0.5 ),
 } ) );
--- tests/unit/models/ObservationField.test.js
+++ tests/unit/models/ObservationField.test.js
@@ -1,21 +1,13 @@
 import ObservationField from "realmModels/ObservationField";
-// import factory from "tests/factory";
+import factory from "tests/factory";
 
-const mockRemoteOf = {
+const mockRemoteOf = factory( "RemoteObservationField", {
   allowed_values: "yes|no",
-  datatype: "text",
-  description: "A field",
-  id: 5,
-  name: "Alive?",
-};
+} );
 
 describe( "ObservationField", ( ) => {
   describe( "mapApiToRealm", ( ) => {
     it( "maps fields, splitting allowed_values", ( ) => {
-      // TODO: use this instead once MOB-1497 has landed
-      // const mockRemoteOf = factory( "RemoteObservationField", {
-      //   allowed_values: "yes|no",
-      // } );
       const mappedOf = ObservationField.mapApiToRealm( mockRemoteOf );
 
       expect( mappedOf.id ).toBe( mockRemoteOf.id );
