032f1a16c50645435cd0215e7e0d9af0f17f7e03
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-06 16:11:07 +0200

    Re-use same PickerSheet as LanguageSettings uses

--- src/components/AddToProjects/FieldInputs/SelectFieldInput.tsx
+++ src/components/AddToProjects/FieldInputs/SelectFieldInput.tsx
@@ -1,4 +1,4 @@
-import { Body3 } from "components/SharedComponents";
+import { Body3, PickerSheet } from "components/SharedComponents";
 import { Pressable } from "components/styledComponents";
 import React, { useState } from "react";
 import type { RealmObservationField } from "realmModels/types";
@@ -17,20 +17,30 @@ const SelectFieldInput = ( { obsField }: Props ) => {
   const [sheetOpen, setSheetOpen] = useState( false );
 
   console.log( "setValue", setValue );
-  console.log( "sheetOpen", sheetOpen );
 
   return (
-    <Pressable
-      accessibilityRole="button"
-      onPress={() => setSheetOpen( true )}
-    >
-      <Body3 className={value
-        ? "pt-1"
-        : "pt-1 color-darkGrayDisabled"}
-      >
-        {value || t( "Select-a-response" )}
-      </Body3>
-    </Pressable>
+    <>
+      {sheetOpen && (
+        <PickerSheet
+          headerText={obsField.name?.toLocaleUpperCase( ) || t( "Select-a-response" )}
+          onPressClose={( ) => setSheetOpen( false )}
+          confirm={( newValue: string ) => {
+            setValue( newValue );
+            setSheetOpen( false );
+          }}
+          selectedValue={value}
+          pickerValues={obsField.allowedValues}
+        />
+      )}
+      <Pressable accessibilityRole="button" onPress={() => setSheetOpen( true )}>
+        <Body3 className={value
+          ? "pt-1"
+          : "pt-1 color-darkGrayDisabled"}
+        >
+          {value || t( "Select-a-response" )}
+        </Body3>
+      </Pressable>
+    </>
   );
 };
 
