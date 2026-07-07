1ab78d1a8772002dfc5456b143238fd602ef6293
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-07 15:37:56 +0200

    Update setupAddToProjects.js

--- tests/unit/components/AddToProjects/helpers/setupAddToProjects.js
+++ tests/unit/components/AddToProjects/helpers/setupAddToProjects.js
@@ -3,23 +3,6 @@ import factory from "tests/factory";
 
 export const initialStoreState = useStore.getState( );
 
-// export const makeObsField = ( overrides = {} ) => ( {
-//   allowedValues: [],
-//   datatype: "text",
-//   description: null,
-//   id: 100,
-//   name: "Test Field",
-//   ...overrides,
-// } );
-
-// export const makePof = ( overrides = {} ) => ( {
-//   id: 1,
-//   obsField: makeObsField( overrides.obsField ),
-//   position: 0,
-//   required: false,
-//   ...overrides,
-// } );
-
 export const mockProjects = [
   factory( "LocalProject" ),
   factory( "LocalProject" ),
