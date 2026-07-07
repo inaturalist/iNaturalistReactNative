21a52625ef3556d6a16f36000451eb01d8d1d345
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-07 11:04:19 +0200

    Get current value for this OF by id from currentObs store

--- src/components/AddToProjects/hooks/useObservationFieldValue.ts
+++ src/components/AddToProjects/hooks/useObservationFieldValue.ts
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import type { RealmObservationPojo } from "realmModels/types";
+import useStore from "stores/useStore";
 
 const useObservationFieldValue = ( obsFieldId: number ) => {
-  const [value, setValue] = useState();
+  const observationFieldValues = useStore(
+    // currentObs is typed as this | null in createObservationFlowSlice.ts but null means the
+    // obs create flow has not been initialized yet. The screen that uses this hook to add obs
+    // to projects should only be reachable after currentObs has been initialized in zustand.
+    // If it isn't I'd prefer us to crash here to notice it.
+    state => ( state.currentObservation as RealmObservationPojo ).observationFieldValues,
+  );
 
-  console.log( obsFieldId );
+  const existingOfv = observationFieldValues?.find(
+    ofv => ofv.obsFieldId === obsFieldId,
+  );
+  const value = existingOfv?.value ?? "";
+  const [value2, setValue] = useState();
+
+  console.log( "value2", value2 );
 
   return { value, setValue };
 };
