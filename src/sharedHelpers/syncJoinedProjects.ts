d8366bf7e9f7936c5d1685eb0feb64a5b25cc065
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-10 13:28:50 +0200

    Fetch users projects in sync fct

--- src/sharedHelpers/syncJoinedProjects.ts
+++ src/sharedHelpers/syncJoinedProjects.ts
@@ -1,3 +1,7 @@
+import { PROJECT_SUMMARY_POF_FIELDS } from "api/fields";
+import type { ApiProjectSummaryWithPOF } from "api/types";
+import fetchUserProjects from "api/usersTyped";
+import { getJWT } from "components/LoginSignUp/AuthenticationService";
 import type Realm from "realm";
 
 async function syncJoinedProjects(
@@ -8,6 +12,20 @@ async function syncJoinedProjects(
     return;
   }
 
+  const params = {
+    id: currentUserId,
+    per_page: 200,
+    fields: PROJECT_SUMMARY_POF_FIELDS,
+    ttl: -1,
+  };
+
+  const apiToken = await getJWT( );
+  const response = await fetchUserProjects<ApiProjectSummaryWithPOF>(
+    params,
+    { api_token: apiToken },
+  );
+
+  console.log( "response", response );
   console.log( "realm", realm );
 }
 
