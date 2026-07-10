b641e3fcf17280a3224d6e1fe1717e86ede1b341
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-10 15:10:43 +0200

    Copy over delete fct from sync obs

--- src/sharedHelpers/syncJoinedProjects.ts
+++ src/sharedHelpers/syncJoinedProjects.ts
@@ -5,6 +5,21 @@ import { getJWT } from "components/LoginSignUp/AuthenticationService";
 import type Realm from "realm";
 import Project from "realmModels/Project";
 
+import safeRealmWrite from "./safeRealmWrite";
+
+const deleteRemotelyDeletedObservations = ( deletedObservations, realm ) => {
+  if ( !deletedObservations ) { return; }
+  if ( deletedObservations?.length > 0 ) {
+    safeRealmWrite( realm, ( ) => {
+      const localObservationsToDelete = realm.objects( "Observation" )
+        .filtered( `id IN { ${deletedObservations} }` );
+      localObservationsToDelete.forEach( observation => {
+        realm.delete( observation );
+      } );
+    }, "removing remotely deleted observations from realm" );
+  }
+};
+
 async function syncJoinedProjects(
   realm: Realm,
   currentUserId: number | undefined,
@@ -32,6 +47,9 @@ async function syncJoinedProjects(
 
   // Update local copy of the current user's joined projects
   Project.upsertRemoteProjects( response.results, realm );
+
+  // Remove projects that are present locally but no longer in server response
+  console.log( "deleteRemotelyDeletedObservations", deleteRemotelyDeletedObservations );
 }
 
 export default syncJoinedProjects;
