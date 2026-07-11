5e10ceead60c8344984bdb2e21667b158458ceeb
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-11 23:13:37 +0200

    numeric fields should return INVALID_NUMERIC for a non-numeric value on an optional field

---
+++ tests/unit/helpers/validateProjectFieldsForObservation.test.js
@@ -0,0 +1,20 @@
+import {
+  INVALID_NUMERIC,
+  validateProjectFieldValue,
+} from "sharedHelpers/validateProjectFieldsForObservation";
+import factory from "tests/factory";
+
+describe( "validateProjectFieldValue", () => {
+  describe( "numeric fields", () => {
+    it( "should return INVALID_NUMERIC for a non-numeric value on an optional field", () => {
+      const mockPOF = factory( "LocalProjectObservationField", {
+        required: false,
+        obsField: factory( "LocalObservationField", {
+          allowedValues: [],
+          datatype: "numeric",
+        } ),
+      } );
+      expect( validateProjectFieldValue( mockPOF, "abc" ) ).toBe( INVALID_NUMERIC );
+    } );
+  } );
+} );
