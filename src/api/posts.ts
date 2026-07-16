b9061706a07980da55660ff9d5da6e1b848d188a
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-16 20:41:15 +0200

    Type opts

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,11 +1,11 @@
 import type { ErrorWithResponse, INatApiError } from "api/error";
 import handleError from "api/error";
-import type { ApiParams } from "api/types";
+import type { ApiOpts, ApiParams } from "api/types";
 import inatjs from "inaturalistjs";
 
 const fetchBlogPosts = async (
   params: Record<string, unknown> = {},
-  opts: Record<string, unknown> = {},
+  opts: ApiOpts = {},
 ): Promise<Record<string, unknown> | null | ErrorWithResponse | INatApiError> => {
   try {
     return await inatjs.posts.for_user( params, opts );
@@ -23,7 +23,7 @@ interface ProjectPostsParams extends ApiParams {
 
 const fetchProjectPosts = async (
   params: ProjectPostsParams,
-  opts: Record<string, unknown> = {},
+  opts: ApiOpts = {},
 ): Promise<Record<string, unknown> | null | ErrorWithResponse | INatApiError> => {
   try {
     return await inatjs.projects.posts( params, opts );
@@ -41,7 +41,7 @@ interface UserPostsParams extends ApiParams {
 
 const fetchUserPosts = async (
   params: UserPostsParams,
-  opts: Record<string, unknown> = {},
+  opts: ApiOpts = {},
 ): Promise<Record<string, unknown> | null | ErrorWithResponse | INatApiError> => {
   try {
     return await inatjs.users.posts( params, opts );
