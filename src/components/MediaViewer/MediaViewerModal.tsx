3c766d13e94a3455963445b8d3963d79382d8559
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-27 13:52:59 +0200

    git mv file, autofix save

--- src/components/MediaViewer/MediaViewerModal.tsx
+++ src/components/MediaViewer/MediaViewerModal.tsx
@@ -5,27 +5,27 @@ import Modal from "components/SharedComponents/Modal";
 import type { Node } from "react";
 import React from "react";
 
-type Props = {
-  autoPlaySound?: boolean, // automatically start playing a sound when it is visible
-  editable?: boolean,
-  deleting?: boolean,
+interface Props {
+  autoPlaySound?: boolean; // automatically start playing a sound when it is visible
+  editable?: boolean;
+  deleting?: boolean;
   // Optional component to use as the header
-  header?: Function,
-  onClose?: Function,
-  onDeletePhoto?: Function,
-  onDeleteSound?: Function,
+  header?: Function;
+  onClose?: Function;
+  onDeletePhoto?: Function;
+  onDeleteSound?: Function;
   photos?: {
-    id?: number,
-    url: string,
-    localFilePath?: string,
-    attribution?: string,
-    licenseCode?: string
-  }[],
+    id?: number;
+    url: string;
+    localFilePath?: string;
+    attribution?: string;
+    licenseCode?: string;
+  }[];
   sounds?: {
-    file_url: string
-  }[],
-  showModal: boolean,
-  uri?: string | null
+    file_url: string;
+  }[];
+  showModal: boolean;
+  uri?: string | null;
 }
 
 const MediaViewerModal = ( {
