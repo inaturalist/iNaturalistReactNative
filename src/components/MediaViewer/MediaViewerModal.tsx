cae4a610b7695fe3b8a5f27c6fb19a34f9a80186
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-07-27 13:56:36 +0200

    Copy prop types from MediaViewer

--- src/components/MediaViewer/MediaViewerModal.tsx
+++ src/components/MediaViewer/MediaViewerModal.tsx
@@ -2,25 +2,31 @@ import MediaViewer from "components/MediaViewer/MediaViewer";
 import Modal from "components/SharedComponents/Modal";
 import React from "react";
 
+interface PhotoItem {
+  attribution?: string;
+  licenseCode?: string;
+  localFilePath?: string;
+  url?: string;
+}
+
+interface SoundItem {
+  file_url: string;
+  hidden: boolean;
+}
+
 interface Props {
   autoPlaySound?: boolean; // automatically start playing a sound when it is visible
   editable?: boolean;
   deleting?: boolean;
   // Optional component to use as the header
-  header?: Function;
-  onClose?: Function;
-  onDeletePhoto?: Function;
-  onDeleteSound?: Function;
-  photos?: {
-    id?: number;
-    url: string;
-    localFilePath?: string;
-    attribution?: string;
-    licenseCode?: string;
-  }[];
-  sounds?: {
-    file_url: string;
-  }[];
+  header?: (
+    { onClose, photoCount }: { onClose: ( ) => void; photoCount: number}
+  ) => React.JSX.Element;
+  onClose?: ( ) => void;
+  onDeletePhoto?: ( uri: string ) => void;
+  onDeleteSound?: ( uri: string ) => void;
+  photos?: PhotoItem[];
+  sounds?: SoundItem[];
   showModal: boolean;
   uri?: string | null;
 }
