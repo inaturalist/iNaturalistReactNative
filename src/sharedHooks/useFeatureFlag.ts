d96d456988af45f3bf8046f0adcaa56ccfab2781
FLGMwt <ryan.stelly@live.com>
2026-07-28 11:24:28 -0500

    consider user admin and installer source in useFeatureFlag

--- src/sharedHooks/useFeatureFlag.ts
+++ src/sharedHooks/useFeatureFlag.ts
@@ -1,13 +1,30 @@
+import { Platform } from "react-native";
+import DeviceInfo from "react-native-device-info";
 import type { FeatureFlag, FeatureFlagSlice } from "stores/createFeatureFlagSlice";
+import { flagsEnabledForAdminsInTestFlight } from "stores/createFeatureFlagSlice";
 import useStore from "stores/useStore";
 
+import useCurrentUser from "./useCurrentUser";
+
+const isTestFlightBuild = Platform.select( {
+  ios: DeviceInfo.getInstallerPackageNameSync() === "TestFlight",
+  default: false,
+} );
+
 const useFeatureFlag = ( featureFlagKey: FeatureFlag ) => {
+  const userIsAdmin = useCurrentUser()?.roles.includes( "admin" );
   const featureFlagConfig = useStore( ( state: FeatureFlagSlice ) => state.featureFlagConfig );
   const featureFlagOverrides
     = useStore( ( state: FeatureFlagSlice ) => state.featureFlagDebugOverrides );
   const override = featureFlagOverrides[featureFlagKey];
   if ( override !== null ) {
     return override;
+  }
+  if ( isTestFlightBuild
+    && userIsAdmin
+    && flagsEnabledForAdminsInTestFlight.includes( featureFlagKey )
+  ) {
+    return true;
   }
   return featureFlagConfig[featureFlagKey];
 };
