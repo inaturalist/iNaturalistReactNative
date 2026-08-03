27833ae4b60b2aa2db2289cd37f6890ea027f9cf
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-04 00:06:05 +0200

    lCreate new ream POs for non-existent POs but selected projects

--- src/sharedHelpers/buildProjectObservationSelection.ts
+++ src/sharedHelpers/buildProjectObservationSelection.ts
@@ -1,3 +1,4 @@
+import ProjectObservation from "realmModels/ProjectObservation";
 import type { RealmProjectObservationPojo } from "realmModels/types";
 
 export interface BuildProjectObservationSelectionResult {
@@ -18,10 +19,21 @@ export default function buildProjectObservationSelection(
   existingProjectObservations: RealmProjectObservationPojo[] | undefined,
   selectedProjectIds: Set<number>,
 ): BuildProjectObservationSelectionResult {
-  console.log( "selectedProjectIds", selectedProjectIds );
   const projectObservations = existingProjectObservations ?? [];
-  console.log( "projectObservations", projectObservations );
+  const nextProjectObservations: RealmProjectObservationPojo[] = [];
+
+  // When pressing Save, for each project that is selected we need to check if there
+  // already exists a PO (e.g. on ObsEdit by not changing this one while adding another)
+  // or if we need to create a new PO
+  selectedProjectIds.forEach( projectId => {
+    const existingPo = projectObservations.find( po => po.projectId === projectId );
+    if ( existingPo ) {
+      nextProjectObservations.push( existingPo );
+    } else {
+      nextProjectObservations.push( ProjectObservation.new( projectId ) );
+    }
+  } );
   return {
-    projectObservations: [],
+    projectObservations: nextProjectObservations,
   };
 }
