9749576bde9573ad479ba9104b9e2a3a7d3b45e2
sepeterson <10458078+sepeterson@users.noreply.github.com>
2026-08-03 22:38:05 -0500

    MOB-1346: header in AdvancedSearch

--- src/components/Explore/ExploreV2/screens/AdvancedSearch.tsx
+++ src/components/Explore/ExploreV2/screens/AdvancedSearch.tsx
@@ -1,14 +1,18 @@
 import { Body2, ViewWrapper } from "components/SharedComponents";
 import { View } from "components/styledComponents";
 import React from "react";
+import { useTranslation } from "sharedHooks";
 
-const AdvancedSearch = ( ) => (
-  <ViewWrapper testID="AdvancedSearch">
-    <View className="flex-1 items-center justify-center p-4">
-      {/* eslint-disable-next-line i18next/no-literal-string */}
-      <Body2>TODO: Advanced Search — MOB-1346</Body2>
-    </View>
-  </ViewWrapper>
-);
+const AdvancedSearch = ( ) => {
+  const { t } = useTranslation( );
+
+  return (
+    <ViewWrapper testID="AdvancedSearch">
+      <View className="flex-1 items-center justify-center p-4">
+        <Body2>{t( "ADVANCED-SEARCH" )}</Body2>
+      </View>
+    </ViewWrapper>
+  );
+};
 
 export default AdvancedSearch;
