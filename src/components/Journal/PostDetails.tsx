d3e8e2d3bbae421ad36b4bd8e08c129b9611590d
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-06 14:54:41 +0200

    Basic title layout

--- src/components/Journal/PostDetails.tsx
+++ src/components/Journal/PostDetails.tsx
@@ -1,14 +1,21 @@
 import {
+  Body1,
   ScrollViewWrapper,
 } from "components/SharedComponents";
 import { ScreenShell } from "components/SharedComponents/ViewWrapper";
+import { View } from "components/styledComponents";
 import React, { useEffect } from "react";
 
 const PostDetails = ( ) => {
   console.log( "useEffect", useEffect );
+  const title = "testgin a very long title looks pretty awesome with this new s component!";
   return (
     <ScreenShell>
-      <ScrollViewWrapper testID="PostDetails" />
+      <ScrollViewWrapper testID="PostDetails">
+        <View className="mx-4 my-5">
+          <Body1>{title}</Body1>
+        </View>
+      </ScrollViewWrapper>
     </ScreenShell>
   );
 };
