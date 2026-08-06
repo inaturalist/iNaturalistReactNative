2095686db62a6d3fcb4373ec0abf640836e8269b
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-06 20:18:54 +0200

    Test that PostListItem calls onPress with the post when pressed

---
+++ tests/unit/components/Journal/PostListItem.test.js
@@ -0,0 +1,18 @@
+import { fireEvent, screen } from "@testing-library/react-native";
+import PostListItem from "components/Journal/PostListItem";
+import React from "react";
+import factory from "tests/factory";
+import { renderComponent } from "tests/helpers/render";
+
+const mockPost = factory( "RemotePost" );
+
+describe( "PostListItem", ( ) => {
+  it( "calls onPress with the post when pressed", ( ) => {
+    const onPress = jest.fn( );
+    renderComponent( <PostListItem item={mockPost} onPress={onPress} /> );
+
+    fireEvent.press( screen.getByTestId( `PostListItem.${mockPost.id}` ) );
+
+    expect( onPress ).toHaveBeenCalledWith( mockPost );
+  } );
+} );
