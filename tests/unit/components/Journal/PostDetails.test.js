a7d9c859a59c8d2d58cacc1719cf3dcebd9fc260
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-06 21:14:05 +0200

    it displays the post title and published date

--- tests/unit/components/Journal/PostDetails.test.js
+++ tests/unit/components/Journal/PostDetails.test.js
@@ -6,6 +6,7 @@ import { renderComponent } from "tests/helpers/render";
 
 const mockPost = factory( "RemotePost", {
   body: "<p>Many birds are on the move.</p>",
+  published_at: "2024-03-15T00:00:00.000Z",
 } );
 
 jest.mock( "@react-navigation/native", ( ) => {
@@ -23,6 +24,13 @@ jest.mock( "@react-navigation/native", ( ) => {
 } );
 
 describe( "PostDetails", ( ) => {
+  it( "displays the post title and published date", ( ) => {
+    renderComponent( <PostDetails /> );
+
+    expect( screen.getByText( mockPost.title ) ).toBeVisible( );
+    expect( screen.getByText( "Mar 15, 2024" ) ).toBeVisible( );
+  } );
+
   it( "displays the post body", ( ) => {
     renderComponent( <PostDetails /> );
 
