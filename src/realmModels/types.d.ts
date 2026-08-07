1d92131eb77b91994cc2f9fcd9c429b16733530a
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-07 14:27:29 +0200

    Add _pendingRemoval to RealmPO interface
    This is not the correct place for this, but I don't want to do the mapRealmToPojo yet.

--- src/realmModels/types.d.ts
+++ src/realmModels/types.d.ts
@@ -85,6 +85,9 @@ export interface RealmProjectObservationPojo extends RealmObject {
   _synced_at?: Date;
   _updated_at?: Date;
   _pending_deletion?: boolean;
+  // TODO: we are conflating types of different purposes: this key is Zustand relevant only,
+  // not added to realm
+  _pendingRemoval?: boolean;
   uuid: string;
   id?: number | null;
   projectId: number;
