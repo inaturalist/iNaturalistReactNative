9d59294168d30ab30b09100c6f124644f9f5600a
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-11 16:35:29 +0200

    Update mock to also have named exports

--- __mocks__/react-native-sensitive-info.ts
+++ __mocks__/react-native-sensitive-info.ts
@@ -15,100 +15,77 @@ try {
   console.warn( "Could not import clearAuthCache, using no-op function", error );
 }
 
-class RNSInfo {
-  static stores = new Map();
+const stores = new Map();
 
-  static getServiceName( o = {} ) {
-    return o.sharedPreferencesName
-        || o.keychainService
+function getServiceName( o = {} ) {
+  return o.service
         || "default";
-  }
-
-  static validateString( s ) {
-    if ( typeof s !== "string" ) { throw new Error( "Invalid string:", s ); }
-  }
-
-  static clearAllStores = jest.fn( () => {
-    RNSInfo.stores.clear();
-    clearAuthCache();
-  } );
-
-  static clearAuthCache = jest.fn( () => {
-    clearAuthCache();
-  } );
-
-  static hasItem = jest.fn( async ( k, o ) => {
-    RNSInfo.validateString( k );
-
-    const serviceName = RNSInfo.getServiceName( o );
-    const service = RNSInfo.stores.get( serviceName );
-
-    if ( service ) { return service.has( k ); }
-    return false;
-  } );
-
-  static getItem = jest.fn( async ( k, o ) => {
-    RNSInfo.validateString( k );
-
-    const serviceName = RNSInfo.getServiceName( o );
-    const service = RNSInfo.stores.get( serviceName );
-
-    if ( service ) { return service.get( k ) || null; }
-    return null;
-  } );
-
-  static getAllItems = jest.fn( async o => {
-    const serviceName = RNSInfo.getServiceName( o );
-    const service = RNSInfo.stores.get( serviceName );
-    let mappedValues = [];
-
-    if ( service?.size ) {
-      mappedValues = Array.from( service.entries() ).map(
-        ( [key, value] ) => ( { key, value, service: serviceName } ),
-      );
-    }
-
-    return mappedValues;
-  } );
-
-  static setItem = jest.fn( async ( k, v, o ) => {
-    RNSInfo.validateString( k );
-    RNSInfo.validateString( v );
-
-    const serviceName = RNSInfo.getServiceName( o );
-    let service = RNSInfo.stores.get( serviceName );
-
-    if ( !service ) {
-      RNSInfo.stores.set( serviceName, new Map() );
-      service = RNSInfo.stores.get( serviceName );
-    }
-
-    service.set( k, v );
-
-    clearAuthCache( );
-
-    return null;
-  } );
-
-  static deleteItem = jest.fn( async ( k, o ) => {
-    RNSInfo.validateString( k );
-
-    const serviceName = RNSInfo.getServiceName( o );
-    const service = RNSInfo.stores.get( serviceName );
-
-    if ( service ) { service.delete( k ); }
-
-    clearAuthCache( );
-
-    return null;
-  } );
-
-  static hasEnrolledFingerprints = jest.fn( async () => true );
-
-  static setInvalidatedByBiometricEnrollment = jest.fn();
-
-  // "Touch ID" | "Face ID" | false
-  static isSensorAvailable = jest.fn( async () => "Face ID" );
 }
 
-module.exports = RNSInfo;
+function validateString( s ) {
+  if ( typeof s !== "string" ) { throw new Error( "Invalid string:", s ); }
+}
+
+const clearAuthCacheInternal = jest.fn( () => {
+  clearAuthCache();
+} );
+
+const hasItem = jest.fn( async ( k, o ) => {
+  validateString( k );
+
+  const serviceName = getServiceName( o );
+  const service = stores.get( serviceName );
+
+  if ( service ) { return service.has( k ); }
+  return false;
+} );
+
+const getItem = jest.fn( async ( k, o ) => {
+  validateString( k );
+
+  const serviceName = getServiceName( o );
+  const service = stores.get( serviceName );
+
+  if ( service ) { return service.get( k ) || null; }
+  return null;
+} );
+
+const setItem = jest.fn( async ( k, v, o ) => {
+  validateString( k );
+  validateString( v );
+
+  const serviceName = getServiceName( o );
+  let service = stores.get( serviceName );
+
+  if ( !service ) {
+    stores.set( serviceName, new Map() );
+    service = stores.get( serviceName );
+  }
+
+  service.set( k, v );
+
+  clearAuthCacheInternal( );
+
+  return null;
+} );
+
+const deleteItem = jest.fn( async ( k, o ) => {
+  validateString( k );
+
+  const serviceName = getServiceName( o );
+  const service = stores.get( serviceName );
+
+  if ( service ) { service.delete( k ); }
+
+  clearAuthCacheInternal( );
+
+  return null;
+} );
+
+module.exports = {
+  deleteItem,
+  setItem,
+  getItem,
+  hasItem,
+  stores,
+};
