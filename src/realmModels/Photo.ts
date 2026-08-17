1bae6c36543cd99a43165b17d60bb996ec9ffcbe
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-17 13:11:34 +0200

    That was the wrong approach

--- src/realmModels/Photo.ts
+++ src/realmModels/Photo.ts
@@ -5,7 +5,6 @@ import { photoUploadPath } from "appConstants/paths";
 import { Platform } from "react-native";
 import type { RealmPhoto } from "realmModels/types";
 import resizeImage from "sharedHelpers/resizeImage";
-import { unlinkShareExtensionSourceIfNeeded } from "sharedHelpers/shareExtensionFiles";
 import { unlink } from "sharedHelpers/util";
 
 class Photo extends Realm.Object {
@@ -62,8 +61,6 @@ class Photo extends Realm.Object {
         onlyScaleDown: true,
       },
     } );
-
-    await unlinkShareExtensionSourceIfNeeded( pathOrUri );
 
     return uri;
   }
