3333495f9084de53aaf11cc31485d70cfbd6453d
Seth Peterson <10458078+sepeterson@users.noreply.github.com>
2026-08-23 21:32:17 -0500

    MOB-1121: fix tests

--- tests/integration/SuggestionsWithUnsyncedObs.test.js
+++ tests/integration/SuggestionsWithUnsyncedObs.test.js
@@ -89,8 +89,7 @@ const mockModelResultWithHuman = {
 jest.mock( "react-native/Libraries/Utilities/Platform", () => ( {
   __esModule: true,
   default: {
-    OS: "ios",
-    select: jest.fn(),
+    ...jest.requireActual( "react-native/Libraries/Utilities/Platform" ).default,
     Version: 11,
   },
 } ) );
--- tests/integration/navigation/Suggestions.test.js
+++ tests/integration/navigation/Suggestions.test.js
@@ -44,8 +44,7 @@ afterEach( () => {
 jest.mock( "react-native/Libraries/Utilities/Platform", () => ( {
   __esModule: true,
   default: {
-    OS: "ios",
-    select: jest.fn(),
+    ...jest.requireActual( "react-native/Libraries/Utilities/Platform" ).default,
     Version: 11,
   },
 } ) );
