63da9cbcfb1dc8303fc8b47057ba427f81f57139
Ryan Stelly <ryan@inaturalist.org>
2026-08-24 20:23:03 -0500

    upgrade to nativewind@4 (#3826)
    * nativewind 4 spike
    
    * themed and cssInterop fixes
    
    * clearify comment on inline Animated.View styles
    
    * remaining style fixes and supplementary agent docs
    
    * fix pill button on media screens
    
    ---------
    
    Co-authored-by: Johannes Klein <17345891+jtklein@users.noreply.github.com>

--- src/components/SharedComponents/Typography/Heading6.tsx
+++ src/components/SharedComponents/Typography/Heading6.tsx
@@ -1,12 +1,16 @@
 import { tailwindFontBold } from "appConstants/fontFamilies";
+import classnames from "classnames";
 import React from "react";
 import type { TextProps } from "react-native";
 
 import InatText from "./InatText";
 
 const Heading6 = ( props: TextProps ) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <InatText className={`text-3xs tracking-wide ${tailwindFontBold}`} {...props} />
+  <InatText
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
+    className={classnames( "text-3xs tracking-wide", tailwindFontBold, props.className )}
+  />
 );
 
 export default Heading6;
