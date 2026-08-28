fe46b7f5b7695b7f389e1ca50b50fe2c1b5d3159
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-08-28 12:33:09 +0200

    Get realm obs by uuid

--- src/uploaders/utils/persistUploadError.ts
+++ src/uploaders/utils/persistUploadError.ts
@@ -2,18 +2,22 @@ import type Realm from "realm";
 
 function persistObservationUploadError(
   realm: Realm,
+  obsUuid: string,
 ): void {
   if ( !realm || realm.isClosed ) {
     return;
   }
+  const observation = realm.objectForPrimaryKey( "Observation", obsUuid );
 }
 
 function clearObservationUploadError(
   realm: Realm,
+  obsUuid: string,
 ): void {
   if ( !realm || realm.isClosed ) {
     return;
   }
+  const observation = realm.objectForPrimaryKey( "Observation", obsUuid );
 }
 
 export {
