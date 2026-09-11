f255e5bef9bb2d8ab163592ca1bbfc0b4dd0d192
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-09-11 21:44:36 +0200

    Remove app-wide setup

--- tests/integration/AddToProjects.test.js
+++ tests/integration/AddToProjects.test.js
@@ -7,34 +7,7 @@ import {
 } from "tests/helpers/addObsBottomSheet";
 import faker from "tests/helpers/faker";
 import setStoreStateLayout from "tests/helpers/setStoreStateLayout";
-import setupUniqueRealm from "tests/helpers/uniqueRealm";
 import { signIn, signOut } from "tests/helpers/user";
-
-// We're explicitly testing navigation here so we want react-navigation
-// working normally
-jest.unmock( "@react-navigation/native" );
-
-// // UNIQUE REALM SETUP
-const mockRealmIdentifier = __filename;
-const { mockRealmModelsIndex, uniqueRealmBeforeAll, uniqueRealmAfterAll } = setupUniqueRealm(
-  mockRealmIdentifier,
-);
-jest.mock( "realmModels/index", ( ) => mockRealmModelsIndex );
-jest.mock( "providers/contexts", ( ) => {
-  const originalModule = jest.requireActual( "providers/contexts" );
-  const { makeRealmHooks } = jest.requireActual( "tests/helpers/uniqueRealm" );
-  return {
-    __esModule: true,
-    ...originalModule,
-    RealmContext: {
-      ...originalModule.RealmContext,
-      ...makeRealmHooks( __filename ),
-    },
-  };
-} );
-beforeAll( uniqueRealmBeforeAll );
-afterAll( uniqueRealmAfterAll );
-// // /UNIQUE REALM SETUP
 
 const mockUser = factory( "LocalUser", {
   login: faker.internet.username(),
