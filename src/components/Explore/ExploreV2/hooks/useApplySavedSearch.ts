343ce481882ee109f2f89b25d37359e968c51cc7
FLGMwt <ryan.stelly@live.com>
2026-09-17 14:36:12 -0500

    simplify zustand types for useApplySavedSearch.ts

--- src/components/Explore/ExploreV2/hooks/useApplySavedSearch.ts
+++ src/components/Explore/ExploreV2/hooks/useApplySavedSearch.ts
@@ -3,7 +3,6 @@ import countFilters from "components/Explore/ExploreV2/helpers/countFilters";
 import type { ExploreStackScreenProps } from "navigation/types";
 import { EXPLORE_V2_ACTION, useExploreV2 } from "providers/ExploreV2Context";
 import { useCallback } from "react";
-import type { ExploreV2AdvancedSearchSlice } from "stores/createExploreV2AdvancedSearchSlice";
 import type { SavedSearch } from "stores/createExploreV2SearchesSlice";
 import useStore from "stores/useStore";
 
@@ -11,7 +10,7 @@ const useApplySavedSearch = ( ) => {
   const { dispatch } = useExploreV2( );
   const navigation = useNavigation<ExploreStackScreenProps<"ExploreResults">["navigation"]>( );
   const setAdvancedSearchMode = useStore(
-    ( state: ExploreV2AdvancedSearchSlice ) => state.exploreV2AdvancedSearch.setAdvancedSearchMode,
+    state => state.exploreV2AdvancedSearch.setAdvancedSearchMode,
   );
 
   return useCallback( ( search: SavedSearch ) => {
