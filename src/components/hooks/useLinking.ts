8cf1cb900cc922946df1f9d757377d3c5975d28e
Seth Peterson <10458078+sepeterson@users.noreply.github.com>
2026-09-24 10:46:11 -0500

    Revert "MOB-1675: no deep linking until after onboarding"
    This reverts commit 4acff0e7868fa90f2cc776248be210905433dfc3.

--- src/components/hooks/useLinking.ts
+++ src/components/hooks/useLinking.ts
@@ -6,14 +6,12 @@ import { getJWT } from "components/LoginSignUp/AuthenticationService";
 import navigateToObsDetails from "components/ObsDetails/helpers/navigateToObsDetails";
 import { useCallback, useEffect, useState } from "react";
 import { Linking } from "react-native";
-import { useOnboardingShown } from "sharedHelpers/installData";
 
 const newAccountConfirmedUrl = "https://www.inaturalist.org/users/sign_in?confirmed=true";
 const existingAccountConfirmedUrl = "https://www.inaturalist.org/home?confirmed=true";
 
 const useLinking = ( currentUser?: object ) => {
   const navigation = useNavigation( );
-  const [onboardingShown] = useOnboardingShown( );
   const [observationId, setObservationId] = useState<string | null>( null );
 
   const navigateConfirmedUser = useCallback( ( ) => {
@@ -80,10 +78,10 @@ const useLinking = ( currentUser?: object ) => {
   }, [handleUrl] );
 
   useEffect( ( ) => {
-    if ( observationId && onboardingShown ) {
+    if ( observationId ) {
       navigateToObservations( );
     }
-  }, [observationId, onboardingShown, navigateToObservations] );
+  }, [observationId, navigateToObservations] );
 };
 
 export default useLinking;
